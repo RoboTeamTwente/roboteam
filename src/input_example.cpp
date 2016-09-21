3970bd1fc396205757f46c824bb380773debff85
jim3101 <jimhoekstra2@gmail.com>
2016-09-21 14:30:44 +0200

    added documentation

--- src/input_example.cpp
+++ src/input_example.cpp
@@ -5,22 +5,24 @@
 #include <vector>
 #include <iostream>
 
+int id;
+bool active;
+float x_vel;
+float y_vel;
+float w_vel;
+bool dribbler;
+float kick_vel;
+float chip_vel;
+
 int main(int argc, char **argv)
 {
+    // Create ros node 'input_example' and advertise on ropic 'robotcommands'
     ros::init(argc, argv, "input_example");
     ros::NodeHandle n;
     ros::Publisher chatter_pub = n.advertise<roboteam_msgs::RobotCommand>("robotcommands", 1000);
     ros::Rate loop_rate(60);
-
-    int id;
-    bool active;
-    float x_vel;
-    float y_vel;
-    float w_vel;
-    bool dribbler;
-    float kick_vel;
-    float chip_vel;
     
+    // Keyboard inputs
     std::cout << "Enter a robot ID: ";
     std::cin >> id;
     std::cout << "Enter an x-velocity: ";
@@ -35,8 +37,10 @@ int main(int argc, char **argv)
     kick_vel = 0.0;
     chip_vel = 0.0;
 
+    // Initialize RobotCommand message;
     roboteam_msgs::RobotCommand command;
 
+    // Fill the RobotCommand message with the keyboard inputs
     command.id = id;
     command.active = active;
     command.x_vel = x_vel;
@@ -46,9 +50,11 @@ int main(int argc, char **argv)
     command.kick_vel = kick_vel;
     command.chip_vel = chip_vel;
 
+    // Publish the command
+    chatter_pub.publish(command);
+
     ROS_INFO_STREAM("command sent for robot: " << command.id);
 
-    chatter_pub.publish(command);
     loop_rate.sleep();
     ros::spin();
 
--- src/sender.cpp
+++ src/sender.cpp
@@ -4,8 +4,6 @@
 #include "grSim_Packet.pb.h"
 #include "grSim_Commands.pb.h"
 
-// #include "std_msgs/Float32MultiArray.h"
-// #include "beginner_tutorials/robocup_command.h"
 #include "roboteam_msgs/RobotCommand.h"
 
 #include <iostream>
@@ -18,9 +16,12 @@ void sendCommands(const roboteam_msgs::RobotCommand::ConstPtr &_msg)
     bool yellow = true;
     packet.mutable_commands()->set_isteamyellow(yellow);
     packet.mutable_commands()->set_timestamp(0.0);
+
+    // Initialize a grSim command (grSim is the robocup SSL simulator created by Parsians)
     grSim_Robot_Command* command = packet.mutable_commands()->add_robot_commands();
     command->set_id(_msg->id);
 
+    // Fill the grSim command with the received values. Set either wheelspeed or robotspeed
     command->set_wheelsspeed(false);
     // command->set_wheel1(0.0);
     // command->set_wheel2(0.0);
@@ -42,6 +43,7 @@ void sendCommands(const roboteam_msgs::RobotCommand::ConstPtr &_msg)
     QHostAddress _addr;
     quint16 _port;
 
+    // Send to IP address and port specified in grSim
     _addr = "127.0.0.1";
     _port = 20011;
     udpsocket.writeDatagram(dgram, _addr, _port);
@@ -49,7 +51,7 @@ void sendCommands(const roboteam_msgs::RobotCommand::ConstPtr &_msg)
 
 int main(int argc, char *argv[])
 {
-    
+    // Create ROS node called robothub and subscribe to topic 'robotcommands'
     ros::init(argc, argv, "robothub");
     ros::NodeHandle n;
     ros::Rate loop_rate(60);

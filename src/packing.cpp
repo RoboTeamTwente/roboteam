af7fa77717862e7b3c030bf95852f857c8ff8c9a
bob rubbens <bobrubbens@gmail.com>
2016-09-22 10:48:27 +0200

    Added message packer for robothub and a test for the packer

---
+++ src/packing.cpp
@@ -0,0 +1,93 @@
+#include "packing.h"
+
+#include <array>
+#include <cassert>
+#include <cstdint>
+#include <iostream>
+#include <memory>
+#include <boost/optional.hpp>
+
+namespace rtt {
+
+/**
+ * Packet format, inspired by the (old?) RoboJackets protocol.
+ *
+ * Byte     Config      Description
+ * 1        aaaabbbb    aaaa: Robot ID, bbbb: Robot velocity
+ * 2        bbbbbbbb    bbbbbbbb: Robot velocity, 0 - 4095 (mm/s)
+ * 3        cccccccc    cccccccc: Moving direction, resolution 2 * pi / 512
+ * 4        000cdeee    c: Moving direction, d: Rotation direction,
+ * 5        eeeeeeee    eeeeeeeeeee: Angular velocity, 0-2047 (deg/s)
+ * 6        ffffffff    ffffffff: Kick force, 0 - 255
+ * 7        0ghijkkk    g: whether or not to kick
+ *                      h: whether or not to chip
+ *                      i: forced or not
+ *                      j: counterclockwise dribbler
+ *                      kkk: dribbler speed, 0 - 7
+ */
+
+/**
+ * All mentioned ranges are inclusive. If the specified ranges are violated,
+ * the function returns boost::none.
+ *
+ * \param id Must be between 0 and 15
+ * \param robot_vel Must be between 0 and 4095 (mm/s)
+ * \param w Moving direction. Must be between 0 and 511. Resolution
+ *          is 2*pi/512
+ * \param rot_cclockwise True if rotation is counter clockwise
+ * \param w_vel Angular velocity. Must be between 0 and 2047
+ * \param kick_force Naturally between 0 and 255
+ * \param do_kick Whether or not a chip or kick should be done
+ * \param chip True if the robot should chip, otherwise a kick will be done
+ * \param forced If true robot will not wait until it's close to the ball
+ * \param dribble_cclockwise If true dribbler will spin counter clockwise
+ * \param dribble_vel Must be between 0 and 7.
+ */
+boost::optional<std::array<uint8_t, 7>> createRobotPacket(int id, int robot_vel, int w,
+                                                         bool rot_cclockwise, int w_vel, uint8_t kick_force,
+                                                         bool do_kick, bool chip, bool forced,
+                                                         bool dribble_cclockwise, uint8_t dribble_vel) {
+    // TODO: Maybe replace this with if's and then a ROS_ERROR?
+    // And the function will just plug zero or a default value
+    // Or maybe return an optional?
+    // Make sure every value is within range
+    if (!((id >= 0 && id < 16)
+            && (robot_vel >= 0 && robot_vel < 4096)
+            && (w >= 0 && w < 512)
+            && (w_vel >= 0 && w_vel < 2048)
+            && (dribble_vel >= 0 && dribble_vel < 8))) {
+        return boost::none;
+    }
+
+    std::array<uint8_t, 7> byteArr;
+
+    // First nibble are the robot id
+    // Second nibble are the third nibble of robot velocity
+    byteArr[0] = static_cast<uint8_t>(((id & 15) << 4) | ((robot_vel >> 8) & 15));
+    // First and second nibble of robot velocity
+    byteArr[1] = static_cast<uint8_t>(robot_vel);
+    // Second to ninth bit of moving direction
+    byteArr[2] = uint8_t{w >> 1};
+    // First bit of moving direction
+    // Then bit that designates clockwise rotation or not
+    // Last three bits of angular velocity
+    byteArr[3] = static_cast<uint8_t>((w & 1) << 4) 
+                | static_cast<uint8_t>(rot_cclockwise << 3)
+                | static_cast<uint8_t>((w_vel >> 8) & 7);
+    // First two nibbles of angular velocity
+    byteArr[4] = static_cast<uint8_t>(w_vel);
+    // Just plug in the byte of kick-force
+    byteArr[5] = kick_force;
+    // gggg = 0, 0, chip = 1 kick = 0, forced = 1 normal = 0
+    // First the chip and forced bools, then a bool that designates
+    // a clockwise dribbler, and then three bits to designate dribble velocity
+    byteArr[6] = static_cast<uint8_t>(do_kick << 6)
+                    | static_cast<uint8_t>(chip << 5)
+                    | static_cast<uint8_t>(forced << 4)
+                    | static_cast<uint8_t>(dribble_cclockwise << 3)
+                    | static_cast<uint8_t>(dribble_vel & 7);
+
+    return boost::optional<std::array<uint8_t, 7>>(byteArr);
+}
+
+}
---
+++ test/packing_test.cpp
@@ -0,0 +1,127 @@
+#include "packing.h"
+
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+TEST(PackingTestSuite, simplePacking) {
+    // Positive bounds checking
+    {
+        auto result = rtt::createRobotPacket(16, 4095, 511, true, 2047, 255, true, true, true, true, 7);
+        ASSERT_FALSE(!!result);
+    }
+
+    {
+        auto result = rtt::createRobotPacket(15, 4096, 511, true, 2047, 255, true, true, true, true, 7);
+        ASSERT_FALSE(!!result);
+    }
+
+    {
+        auto result = rtt::createRobotPacket(15, 4095, 512, true, 2047, 255, true, true, true, true, 7);
+        ASSERT_FALSE(!!result);
+    }
+
+    {
+        auto result = rtt::createRobotPacket(15, 4095, 511, true, 2048, 255, true, true, true, true, 7);
+        ASSERT_FALSE(!!result);
+    }
+
+    {
+        auto result = rtt::createRobotPacket(15, 4095, 511, true, 2047, 255, true, true, true, true, 8);
+        ASSERT_FALSE(!!result);
+    }
+
+    // Negative bounds
+    {
+        auto result = rtt::createRobotPacket(-1, 4095, 511, true, 2047, 255, true, true, true, true, 7);
+        ASSERT_FALSE(!!result);
+    }
+
+    {
+        auto result = rtt::createRobotPacket(15, -1, 511, true, 2047, 255, true, true, true, true, 7);
+        ASSERT_FALSE(!!result);
+    }
+
+    {
+        auto result = rtt::createRobotPacket(15, 4095, -1, true, 2047, 255, true, true, true, true, 7);
+        ASSERT_FALSE(!!result);
+    }
+
+    {
+        auto result = rtt::createRobotPacket(15, 4095, 511, true, -1, 255, true, true, true, true, 7);
+        ASSERT_FALSE(!!result);
+    }
+
+    {
+        auto result = rtt::createRobotPacket(15, 4095, 511, true, 2047, 255, true, true, true, true, -1);
+        ASSERT_FALSE(!!result);
+    }
+
+    // Handcrafted result checking
+    {
+        auto result = rtt::createRobotPacket(15, 4095, 511, true, 2047, 255, true, true, true, true, 7);
+        std::string correctResult = "11111111\n" // Everything at the highest value
+                                    "11111111\n"
+                                    "11111111\n"
+                                    "00011111\n"
+                                    "11111111\n"
+                                    "11111111\n"
+                                    "01111111\n";
+        ASSERT_TRUE(!!result);
+        if (result) {
+            ASSERT_EQ(correctResult, rtt::byteArrayToString<7>(*result));
+        }
+    }
+
+    {
+        auto result = rtt::createRobotPacket(0, 0, 0, false, 0, 0, false, false, false, false, 0);
+        std::string correctResult = "00000000\n" // Everything at the lowest value
+                                    "00000000\n"
+                                    "00000000\n"
+                                    "00000000\n"
+                                    "00000000\n"
+                                    "00000000\n"
+                                    "00000000\n";
+        ASSERT_TRUE(!!result);
+        if (result) {
+            ASSERT_EQ(correctResult, rtt::byteArrayToString<7>(*result));
+        }
+    }
+
+    {
+        std::string correctResult = "10100101\n"  // a: 12
+                                    "11001100\n"  // b: 1484
+                                    "11100010\n"  // 
+                                    "00010101\n"  // c: 453, d: false
+                                    "10010111\n"  // e: 1431
+                                    "00001100\n"  // f: 12
+                                    "01010110\n"; // g: true, h: false, i: true, j: false, k: 6
+
+        auto result = rtt::createRobotPacket(10, 1484, 453, false, 1431, 12, true, false, true, false, 6);
+        ASSERT_TRUE(!!result);
+        if (result) {
+            ASSERT_EQ(correctResult, rtt::byteArrayToString<7>(*result));
+        }
+    }
+
+    {
+        std::string correctResult = "11100010\n"  // a: 14
+                                    "00101001\n"  // b: 553
+                                    "00110101\n"  // 
+                                    "00001011\n"  // c: 106, d: true
+                                    "11001110\n"  // e: 974
+                                    "01001110\n"  // f: 78
+                                    "00110111\n"; // g: false, h: true, i: true, j: false, k: 7
+
+        auto result = rtt::createRobotPacket(14, 553, 106, true, 974, 78, false, true, true, false, 7);
+        ASSERT_TRUE(!!result);
+        if (result) {
+            ASSERT_EQ(correctResult, rtt::byteArrayToString<7>(*result));
+        }
+    }
+}
+
+int main(int argc, char **argv){
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}

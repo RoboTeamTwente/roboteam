0b4809dd695af67083a545ad813e210fb1a9eaed
Wybe <wybe@ruurdwestra.nl>
2017-01-26 13:42:51 +0100

    * Changed Draw so it uses if statements instead of defines.

--- src/Draw.cpp
+++ src/Draw.cpp
@@ -2,15 +2,17 @@
 
 namespace rtt {
 
+const bool RTT_ENABLE_DEBUG_GRAPHICS = RTT_CMAKE_ENABLE_DEBUG_GRAPHICS;
+
 Draw::Draw() {
-#ifdef RTT_ENABLE_DEBUG_GRAPHICS
+if (RTT_ENABLE_DEBUG_GRAPHICS) {
     debugPub = n.advertise<roboteam_msgs::DebugLine>("view_debug_lines", 1000);
     debugPubPoint = n.advertise<roboteam_msgs::DebugPoint>("view_debug_points", 1000);
-#endif
+}
 }
 
 void Draw::DrawLine(std::string name, roboteam_utils::Vector2 start, roboteam_utils::Vector2 line) {
-#ifdef RTT_ENABLE_DEBUG_GRAPHICS
+if (RTT_ENABLE_DEBUG_GRAPHICS) {
     roboteam_msgs::DebugLine Line;
     Line.name = name;
     Line.remove = false;
@@ -24,44 +26,44 @@ void Draw::DrawLine(std::string name, roboteam_utils::Vector2 start, roboteam_ut
     Line.points.push_back(endLine1);
     Line.color = color;
     debugPub.publish(Line);
-#endif
+}
 }
 
 void Draw::RemoveLine(std::string name) {
-#ifdef RTT_ENABLE_DEBUG_GRAPHICS
+if (RTT_ENABLE_DEBUG_GRAPHICS) {
     roboteam_msgs::DebugLine Line;
     Line.name = name;
     Line.remove = true;
     debugPub.publish(Line);
-#endif
+}
 }
 
 void Draw::DrawPoint(std::string name, roboteam_utils::Vector2 point) {
-#ifdef RTT_ENABLE_DEBUG_GRAPHICS
-    roboteam_msgs::DebugPoint position;
-    position.name = name;
-    position.pos.x = point.x;
-    position.pos.y = point.y;
-    position.color = color;
-    debugPubPoint.publish(position);
-#endif
+    if (RTT_ENABLE_DEBUG_GRAPHICS) {
+        roboteam_msgs::DebugPoint position;
+        position.name = name;
+        position.pos.x = point.x;
+        position.pos.y = point.y;
+        position.color = color;
+        debugPubPoint.publish(position);
+    }
 }
 
 void Draw::RemovePoint(std::string name) {
-#ifdef RTT_ENABLE_DEBUG_GRAPHICS
+if (RTT_ENABLE_DEBUG_GRAPHICS) {
     roboteam_msgs::DebugPoint position;
     position.name = name;
     position.remove = true;
     debugPubPoint.publish(position);
-#endif
+}
 }
 
 void Draw::SetColor(int r, int g, int b) {
-#ifdef RTT_ENABLE_DEBUG_GRAPHICS
+if (RTT_ENABLE_DEBUG_GRAPHICS) {
     color.r = r;
     color.g = g;
     color.b = b;
-#endif
+}
 }
 
 } // rtt

1dd2dd2cb30c5f34344e320c422217201869d606
DennisdeW <dennisdw@outlook.com>
2017-07-03 13:37:52 +0200

    Bit of documentation, minor tweak to Section

--- src/Section.cpp
+++ src/Section.cpp
@@ -15,11 +15,15 @@ Vector2 Section::intersection(const Section& other) const {
            
     //https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection#Given_two_points_on_each_line
     double divisor = (x1-x2) * (y3-y4) - (y1 - y2) * (x3 - x4);
+    if (divisor < .0001) {
+    	// Parallel or coincident
+    	return {NAN, NAN};
+    }
     double t1 = (x1 * y2 - y1 * x2);
     double t2 = (x3 * y4 - y3 * x4);
     double x = (t1 * (x3 - x4) - (x1 - x2) * t2) / divisor;
     double y = (t1 * (y3 - y4) - (y1 - y2) * t2) / divisor;
-    return Vector2(x, y);
+    return {x, y};
 }    
     
 bool Section::pointOnLine(const Vector2& point) const {

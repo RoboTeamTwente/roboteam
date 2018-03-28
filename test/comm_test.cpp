8f70cf9dd16546814869bd49c3403a3e828f3ced
Emiel Steerneman <emielsteerneman@gmail.com>
2018-03-29 00:35:22 +0200

    Halfway through

--- test/comm_test.cpp
+++
@@ -1,9 +0,0 @@
-#include "roboteam_robothub/RobotComm.h"
-
-#include <iostream>
-
-#include <gtest/gtest.h>
-
-TEST(CommTestSuite, simpleComm) {
-    ASSERT_TRUE(true); // Until further complexity I guess we can leave this empty
-}

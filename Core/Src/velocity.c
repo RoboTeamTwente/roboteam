e5307d198b05576e3ea652e4f43a3b19eafe913e
simenbootsma <s.t.bootsma@student.utwente.nl>
2019-04-23 11:16:32 +0200

    Clean up

--- Core/Src/velocity.c
+++ Core/Src/velocity.c
@@ -5,20 +5,20 @@
 
 static PID_states velState = off;
 static PIDvariables velK[3];
-static float ref[3] = {0.0f};
+static float velRef[3] = {0.0f};
 static float wheelRef[4] = {0.0f};
 static float state[3] = {0.0f};
 
 ///////////////////////////////////////////////////// PRIVATE FUNCTION DECLARATIONS
 
 //Transforms body velocity to wheel speed
-static void body2Wheels(float wheelspeed[4], float velocity[3]);
+static void body2Wheels(float wheelSpeed[4], float velocity[3]);
 
 //Transfer global coordinate frame to local coordinate frame
-static void global2Local(float input[3], float output[2], float  yaw);
+static void global2Local(float input[3], float output[2], float yaw);
 
 //Determine the desired wheel speed given the desired velocities
-static void translationVelControl(float State[3], float vel_ref[3], float translationalRef[4]);
+static void translationVelControl(float state[3], float velRef[3], float translationalRef[4]);
 
 //Determine the desired wheel speed given the desired angle
 static float angleControl(float angleRef, float angle);
@@ -42,9 +42,9 @@ int velocity_DeInit(){
 void velocity_Update(){
 	if (velState == on){
 		float translationalRef[4] = {0.0f};
-		translationVelControl(state, ref, translationalRef);
+		translationVelControl(state, velRef, translationalRef);
 
-		float angularRef = angleControl(ref[body_w], state[body_w]);
+		float angularRef = angleControl(velRef[body_w], state[body_w]);
 
 		for (wheel_names wheel=wheels_RF; wheel<=wheels_LF; wheel++){
 			wheelRef[wheel] = translationalRef[wheel] + angularRef;
@@ -53,9 +53,9 @@ void velocity_Update(){
 }
 
 void velocity_SetRef(float input[3]){
-	ref[body_x] = input[body_x];
-	ref[body_y] = input[body_y];
-	ref[body_w] = input[body_w];
+	velRef[body_x] = input[body_x];
+	velRef[body_y] = input[body_y];
+	velRef[body_w] = input[body_w];
 }
 
 void velocity_GetWheelRef(float output[4]){
@@ -72,15 +72,15 @@ void velocity_SetState(float input[3]){
 
 ///////////////////////////////////////////////////// PRIVATE FUNCTION IMPLEMENTATIONS
 
-static void body2Wheels(float wheelspeed[4], float velocity[3]){
+static void body2Wheels(float wheelSpeed[4], float velocity[3]){
 	//mixing matrix
 	float velx2wheel = (velocity[body_x]*sin60/rad_wheel);
 	float vely2wheel = (velocity[body_y]*cos60/rad_wheel);
 	//float rot2wheel =  (rad_robot*velocity[body_w]/rad_wheel);
-	wheelspeed[wheels_RF] = (velx2wheel + vely2wheel);
-	wheelspeed[wheels_RB] = (velx2wheel - vely2wheel);
-	wheelspeed[wheels_LB] = (-velx2wheel - vely2wheel);
-	wheelspeed[wheels_LF] = (-velx2wheel + vely2wheel);
+	wheelSpeed[wheels_RF] = (velx2wheel + vely2wheel);
+	wheelSpeed[wheels_RB] = (velx2wheel - vely2wheel);
+	wheelSpeed[wheels_LB] = (-velx2wheel - vely2wheel);
+	wheelSpeed[wheels_LF] = (-velx2wheel + vely2wheel);
 }
 
 static void global2Local(float global[3], float local[3], float  yaw){
@@ -90,9 +90,9 @@ static void global2Local(float global[3], float local[3], float  yaw){
 	local[body_w] = global[body_w];
 }
 
-static void translationVelControl(float State[3], float vel_ref[3], float translationalRef[4]){
+static void translationVelControl(float state[3], float velRef[3], float translationalRef[4]){
 	float velLocalRef[3] = {0, 0, 0};
-	global2Local(vel_ref, velLocalRef, State[body_w]); //transfer global to local
+	global2Local(velRef, velLocalRef, state[body_w]); //transfer global to local
 
 	// Manually adjusting velocity command
 	//     Explanation: see Velocity Difference file on drive (https://docs.google.com/document/d/1pGKysiwpu19DKLpAZ4GpluMV7UBhBQZ65YMTtI7bd_8/)
@@ -100,8 +100,8 @@ static void translationVelControl(float State[3], float vel_ref[3], float transl
 	velLocalRef[body_y] = 1.308 * velLocalRef[body_y];
 
 	// Local control
-	float velxErr = (velLocalRef[body_x] - State[body_x]);
-	float velyErr = (velLocalRef[body_y] - State[body_y]);
+	float velxErr = (velLocalRef[body_x] - state[body_x]);
+	float velyErr = (velLocalRef[body_y] - state[body_y]);
 	velLocalRef[body_x] += PID(velxErr, &velK[body_x]);
 	velLocalRef[body_y] += PID(velyErr, &velK[body_y]);
 

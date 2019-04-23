97bc74a356f0945243143ea00e2128c036910e14
Cas Doornkamp <cjdoornkamp@gmail.com>
2019-04-23 21:04:12 +0200

    remove Debug folder from commits

--- Debug/Lib/MTi/xbus/xbusutility.d
+++
@@ -1,4 +0,0 @@
-Lib/MTi/xbus/xbusutility.o: ../Lib/MTi/xbus/xbusutility.c \
- ../Lib/MTi/xbus/xbusutility.h
-
-../Lib/MTi/xbus/xbusutility.h:
--- Debug/Lib/MTi/xbus/xsdeviceid.d
+++
@@ -1,4 +0,0 @@
-Lib/MTi/xbus/xsdeviceid.o: ../Lib/MTi/xbus/xsdeviceid.c \
- ../Lib/MTi/xbus/xsdeviceid.h
-
-../Lib/MTi/xbus/xsdeviceid.h:

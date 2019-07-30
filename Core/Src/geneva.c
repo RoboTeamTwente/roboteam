f49cbad022dc8f33c907ed678f94d6a1f005aa87
simenbootsma <s.t.bootsma@student.utwente.nl>
2019-07-30 12:02:34 +0200

    Remove unused functions

--- Core/Src/geneva.c
+++ Core/Src/geneva.c
@@ -58,7 +58,7 @@ void geneva_Update(){
 	static int last_time = 10000;
 	static int time = 0;
 
-	bool moving = fabs(geneva_Encodervalue() - prev_enc) > ENCODER_DEVIATION_MARGIN || pwm < 200;
+	bool moving = fabs(geneva_Encodervalue() - prev_enc) > ENCODER_DEVIATION_MARGIN || pwm < PWM_CUTOFF;
 	time = HAL_GetTick();
 	if(moving){
 		prev_enc = geneva_Encodervalue();
@@ -83,7 +83,7 @@ void geneva_Update(){
 
 		break;
 	case turning:
-		if(fabs(genevaRef - geneva_Encodervalue()) < 3*ENCODER_DEVIATION_MARGIN){
+		if(fabs(genevaRef - geneva_Encodervalue()) < 3 * ENCODER_DEVIATION_MARGIN){
 			genevaState = idle;
 			pwm = 0;
 		} else {
@@ -144,45 +144,9 @@ geneva_positions geneva_GetState() {
 
 bool geneva_IsWorking() {
 	return genevaState != off;
-
-
-
-
-
-//	static int timer = 0xffff;
-//	static bool isWorking = true;
-//
-//	static bool isResponding = true;
-//	static int prevEncoder = 0;
-//
-//	if (fabs(pwm) > 0) {
-//		isResponding = (fabs(geneva_Encodervalue() - prevEncoder) > ENCODER_DEVIATION_MARGIN); // if geneva moves, it is responding
-//		prevEncoder = geneva_Encodervalue();
-//	}
-//
-//	if (isResponding) {
-//		timer = HAL_GetTick();
-//		isWorking = true;
-//	} else if (HAL_GetTick() - timer > 2000) {
-//		isWorking = false;
-//	}
-//	return isWorking;
 }
 
 ///////////////////////////////////////////////////// PRIVATE FUNCTION IMPLEMENTATIONS
-
-static void CheckIfStuck(){
-	static uint tick = 0xFFFF;			//
-	static int enc = 0;
-	if(fabs(geneva_Encodervalue() - enc) > ENCODER_DEVIATION_MARGIN){
-		enc = geneva_Encodervalue();
-		tick = HAL_GetTick();
-	}else if(tick + 1000 < HAL_GetTick()){
-		__HAL_TIM_SET_COUNTER(ENC_GENEVA, GENEVA_CAL_EDGE_CNT);
-		geneva_SetRef(geneva_middle);
-		genevaState = turning;
-	}
-}
 
 static int geneva_Encodervalue(){
 	return (int32_t)__HAL_TIM_GetCounter(ENC_GENEVA);
@@ -207,24 +171,6 @@ static void limitScale(){
 	} else if(pwm > MAX_PWM){
 		pwm = MAX_PWM;
 	}
-}
-
-static bool isResponding() {
-	static bool result = true;
-	static int cnt = 0;
-	static int prevEncoder = 0;
-	if ((pwm > 0) && (fabs(geneva_Encodervalue() - prevEncoder)) < ENCODER_DEVIATION_MARGIN)  {
-		cnt++;
-	} else {
-		cnt = 0;
-		result = true;
-		prevEncoder = geneva_Encodervalue();
-	}
-	if (cnt >= 500) {
-		cnt = 0;
-		result = !result;
-	}
-	return result;
 }
 
 static void setDir(){

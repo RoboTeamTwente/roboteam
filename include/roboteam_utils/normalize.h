3152045d2959142173b53a7b65f224250ebe57c2
mrlukasbos <mrlukasbos@gmail.com>
2019-09-03 21:10:00 +0200

    some work on converting field and world

--- include/roboteam_utils/normalize.h
+++ include/roboteam_utils/normalize.h
@@ -3,45 +3,120 @@
 #include <roboteam_proto/RobotCommand.pb.h>
 #include <roboteam_proto/GeometryData.pb.h>
 #include <roboteam_proto/Referee.pb.h>
+#include <roboteam_proto/WorldBall.pb.h>
+#include <roboteam_proto/WorldRobot.pb.h>
+#include <roboteam_proto/World.pb.h>
+#include <roboteam_proto/messages_robocup_ssl_referee.pb.h>
+#include <roboteam_proto/messages_robocup_ssl_geometry.pb.h>
+#include "roboteam_proto/FieldLineSegment.pb.h"
+#include "roboteam_proto/FieldCircularArc.pb.h"
+#include "Mathematics.h"
 
-namespace rtt {
-
-using namespace roboteam_proto;
-
-
-///**
-// * Rotates a detectionframe message 180 degrees.
-// */
-//void rotateDetectionFrame(DetectionFrame * world);
-//
-///**
-// * Rotates a detectionball message 180 degrees.
-// */
-//void rotateBall(DetectionBall * ball);
-//
-///**
-// * Rotates a detectionrobot message 180 degrees.
-// */
-//void rotateRobot(DetectionRobot * bot);
-
-//void rotateGeometryData(GeometryData * data);
-//
-//void rotateGeometryFieldSize(GeometryFieldSize * size);
-//
-//void rotateGeometryCameraCalibration(GeometryCameraCalibration * calib);
-//
-//void rotateLine(FieldLineSegment * line);
-//
-//void rotateArc(FieldCircularArc * arc);
-//
-//void rotateRefereeData(RefereeData * data);
+namespace roboteam_utils {
 
 
-/**
- * "Rotates" the robot command. I.e., if you would rotate the field, and then rotate the robot command,
- * the robot would head in the same direction as it would have if you did not do those two rotations.
- * Effectively it only negates the x_vel and y_vel properties of the message.
- */
-//void rotateRobotCommand(RobotCommand const & command);
+float mm_to_m(float scalar) {
+  return scalar/1000;
+}
 
-} // rtt
+// rotate a ball
+void rotate(roboteam_proto::WorldBall *ball) {
+  ball->mutable_pos()->set_x(ball->pos().x()*-1);
+  ball->mutable_pos()->set_y(ball->pos().y()*-1);
+}
+
+// rotate a single robot
+void rotate(roboteam_proto::WorldRobot *robot) {
+  robot->mutable_pos()->set_x(robot->pos().x()*-1);
+  robot->mutable_pos()->set_y(robot->pos().y()*-1);
+  robot->mutable_vel()->set_x(robot->vel().x()*-1);
+  robot->mutable_vel()->set_y(robot->vel().y()*-1);
+  robot->set_w(robot->w()*-1);
+  robot->set_angle(rtt::cleanAngle(robot->angle() + M_PI));
+}
+
+// rotate the ball and robots
+void rotate(roboteam_proto::World *world) {
+  rotate(world->mutable_ball());
+
+  // rotate all blue robots
+  for (int i = 0; i < world->mutable_blue()->size(); i++) {
+    rotate(world->mutable_blue(i));
+  }
+
+  // rotate all yellow robots
+  for (int i = 0; i < world->mutable_yellow()->size(); i++) {
+    rotate(world->mutable_yellow(i));
+  }
+}
+
+// rotate the designated position given by the referee
+// this position is used for example for ball placement.
+void rotate(roboteam_proto::SSL_Referee *refereeData) {
+  refereeData->mutable_designated_position()->set_x(refereeData->designated_position().x()*-1);
+  refereeData->mutable_designated_position()->set_y(refereeData->designated_position().y()*-1);
+}
+
+// rotate a single field arc
+void rotate(roboteam_proto::SSL_FieldCicularArc *arc) {
+  arc->mutable_center()->set_x(arc->center().x()*-1);
+  arc->mutable_center()->set_y(arc->center().y()*-1);
+  arc->set_a1(arc->a1()*-1);
+  arc->set_a2(arc->a2()*-1);
+}
+
+// convert an arc from millimeters to meters
+void toMeters(roboteam_proto::SSL_FieldCicularArc *arc) {
+  arc->mutable_center()->set_x(mm_to_m(arc->center().x()));
+  arc->mutable_center()->set_y(mm_to_m(arc->center().y()));
+  arc->set_a1(mm_to_m(arc->a1()));
+  arc->set_a2(mm_to_m(arc->a2()));
+  arc->set_radius(mm_to_m(arc->radius()));
+  arc->set_thickness(mm_to_m(arc->thickness()));
+}
+
+// rotate a single field line
+void rotate(roboteam_proto::SSL_FieldLineSegment *line) {
+  line->mutable_p1()->set_x(line->p1().x()*-1);
+  line->mutable_p1()->set_y(line->p1().y()*-1);
+  line->mutable_p2()->set_x(line->p2().x()*-1);
+  line->mutable_p2()->set_y(line->p2().y()*-1);
+}
+
+// convert a line from millimeters to meters
+void toMeters(roboteam_proto::SSL_FieldLineSegment * line) {
+  line->mutable_p1()->set_x(mm_to_m(line->p1().x()));
+  line->mutable_p1()->set_y(mm_to_m(line->p1().y()));
+  line->mutable_p2()->set_x(mm_to_m(line->p2().x()));
+  line->mutable_p2()->set_y(mm_to_m(line->p2().y()));
+  line->set_thickness(mm_to_m(line->thickness()));
+}
+
+// rotate the lines and arcs of a field
+void rotate(roboteam_proto::SSL_GeometryFieldSize * field) {
+
+  // rotate all field lines
+  for (int i = 0; i < field->mutable_field_lines()->size(); i++) {
+    rotate(field->mutable_field_lines(i));
+  }
+
+  // rotate all field arcs
+  for (int i = 0; i < field->mutable_field_arcs()->size(); i++) {
+    rotate(field->mutable_field_arcs(i));
+  }
+}
+
+// convert all units from the field from millimeters to meters.
+void toMeters(roboteam_proto::SSL_GeometryFieldSize * field) {
+  // convert all field lines
+  for (int i = 0; i < field->mutable_field_lines()->size(); i++) {
+    toMeters(field->mutable_field_lines(i));
+  }
+
+  // convert all field arcs
+  for (int i = 0; i < field->mutable_field_arcs()->size(); i++) {
+    toMeters(field->mutable_field_arcs(i));
+  }
+}
+
+}

4e1598d8e5a72775f4720f02b210ee4de1035c7f
mrlukasbos <mrlukasbos@gmail.com>
2019-10-27 11:17:34 +0100

    use map and enum

--- include/roboteam_utils/constants.h
+++ include/roboteam_utils/constants.h
@@ -2,18 +2,25 @@
 
 #include <array>
 #include <string>
+#include <map>
+
 #include <roboteam_proto/Channel.h>
 
 namespace roboteam_utils {
 
-extern const roboteam_proto::Channel GEOMETRY_CHANNEL;
-extern const roboteam_proto::Channel REFEREE_CHANNEL;
-extern const roboteam_proto::Channel WORLD_CHANNEL;
-extern const roboteam_proto::Channel ROBOT_COMMANDS_PRIMARY_CHANNEL;
-extern const roboteam_proto::Channel ROBOT_COMMANDS_SECONDARY_CHANNEL;
-extern const roboteam_proto::Channel FEEDBACK_PRIMARY_CHANNEL;
-extern const roboteam_proto::Channel FEEDBACK_SECONDARY_CHANNEL;
-extern const roboteam_proto::Channel SETTINGS_PRIMARY_CHANNEL;
-extern const roboteam_proto::Channel SETTINGS_SECONDARY_CHANNEL;
+enum ChannelType {
+  GEOMETRY_CHANNEL,
+  REFEREE_CHANNEL,
+  WORLD_CHANNEL,
+  ROBOT_COMMANDS_PRIMARY_CHANNEL,
+  ROBOT_COMMANDS_SECONDARY_CHANNEL,
+  FEEDBACK_PRIMARY_CHANNEL,
+  FEEDBACK_SECONDARY_CHANNEL,
+  SETTINGS_PRIMARY_CHANNEL,
+  SETTINGS_SECONDARY_CHANNEL
+};
+
+extern const std::map<ChannelType, roboteam_proto::Channel> CHANNELS;
+
 
 } // namespace rtt
--- src/constants.cpp
+++ src/constants.cpp
@@ -12,14 +12,16 @@ static bool has(std::array<T, N> arr, T val) {
   return false;
 }
 
-const roboteam_proto::Channel GEOMETRY_CHANNEL =                  {"geometry", "tcp://127.0.0.1:5556"};
-const roboteam_proto::Channel REFEREE_CHANNEL =                   {"referee", "tcp://127.0.0.1:5557"};
-const roboteam_proto::Channel WORLD_CHANNEL =                     {"world", "tcp://127.0.0.1:5558"};
-const roboteam_proto::Channel ROBOT_COMMANDS_PRIMARY_CHANNEL =    {"commands_primary", "tcp://127.0.0.1:5559"};
-const roboteam_proto::Channel ROBOT_COMMANDS_SECONDARY_CHANNEL =  {"commands_secondary", "tcp://127.0.0.1:5560"};
-const roboteam_proto::Channel FEEDBACK_PRIMARY_CHANNEL =          {"feedback_primary", "tcp://127.0.0.1:5561"};
-const roboteam_proto::Channel FEEDBACK_SECONDARY_CHANNEL =        {"feedback_yellow", "tcp://127.0.0.1:5562"};
-const roboteam_proto::Channel SETTINGS_PRIMARY_CHANNEL=           {"settings_primary", "tcp://127.0.0.1:5563"};
-const roboteam_proto::Channel SETTINGS_SECONDARY_CHANNEL=         {"settings_secondary", "tcp://127.0.0.1:5564"};
+const std::map<ChannelType, roboteam_proto::Channel> CHANNELS = {
+    {GEOMETRY_CHANNEL, {"geometry", "tcp://127.0.0.1:5556"}},
+    {REFEREE_CHANNEL, {"referee", "tcp://127.0.0.1:5557"}},
+    {WORLD_CHANNEL, {"world", "tcp://127.0.0.1:5558"}},
+    {ROBOT_COMMANDS_PRIMARY_CHANNEL, {"commands_primary", "tcp://127.0.0.1:5559"}},
+    {ROBOT_COMMANDS_SECONDARY_CHANNEL, {"commands_secondary", "tcp://127.0.0.1:5560"}},
+    {FEEDBACK_PRIMARY_CHANNEL, {"feedback_primary", "tcp://127.0.0.1:5561"}},
+    {FEEDBACK_SECONDARY_CHANNEL, {"feedback_yellow", "tcp://127.0.0.1:5562"}},
+    {SETTINGS_PRIMARY_CHANNEL, {"settings_primary", "tcp://127.0.0.1:5563"}},
+    {SETTINGS_SECONDARY_CHANNEL, {"settings_secondary", "tcp://127.0.0.1:5564"}}
+};
 
 } // namespace rtt

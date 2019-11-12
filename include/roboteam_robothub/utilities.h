503002f6250d3e9cf535fa098ef824fd404bbf8b
Yuhanun <y.citgez@student.utwente.nl>
2019-11-12 16:05:39 +0100

    General improvement to utilities

--- include/roboteam_robothub/utilities.h
+++ include/roboteam_robothub/utilities.h
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <string>
+#include <bitset>
 #include "packing.h"
 
 namespace rtt {
@@ -29,16 +30,26 @@ static int char2int(char input) {
 }
 
 // This function assumes src to be a zero terminated sanitized string with an even number of [0-9a-f] characters, and target to be sufficiently large
-static void hex2bin(const char* src, packed_robot_feedback& target) {
-    int i;
-    while (*src && src[1]) {
+[[maybe_unused]] static void hex2bin(const char* src, packed_robot_feedback& target) {
+    int i{ 0 }; // don't leave this uninitialized, the value is UB, incrementing it could either be 1, 0, -500, 273613 or whatever else
+    /**
+     * be consistent
+     * operator[] for pointers is essentiall *(ptr + i), so *src == src[0]
+     */
+    while (src[0] && src[1]) {
         target.at(i) = char2int(*src)*16+char2int(src[1]);
         src += 2;
         i++;
     }
 }
 
-static Mode stringToMode(const std::string& type) {
+/**
+ * Inline implies inline, it does not guarantee it
+ * However if a function marked constexpr then it's guaranteed to be inlined
+ * Things that can be calculated compiletime **should** be actually calculated compiletime
+ * Try stick to using constexpr as much as possible, you're using C++17 so might aswel use modern features
+ */
+[[maybe_unused]] /* Why static?... inline instead */ static Mode stringToMode(const std::string& type) {
     if (type=="serial") {
         return Mode::SERIAL;
     }
@@ -50,52 +61,66 @@ static Mode stringToMode(const std::string& type) {
     }
 }
 
-static std::string modeToString(Mode mode) {
+[[maybe_unused]] static std::string modeToString(Mode mode) {
     switch (mode) {
     case Mode::SERIAL:return "Serial";
     case Mode::GRSIM:return "Grsim";
     case Mode::UNDEFINED:return "Undefined";
     }
+    // should never hit this:
+    // Just to surpress compiler warnings
+    return "";
 }
 
 // Copy of getWorldBot() because I don't want to pull in tactics as a dependency.
 // If this function is moved to utils, we can use that
-static std::shared_ptr<proto::WorldRobot> getWorldBot(unsigned int id, bool ourTeam, const proto::World &world) {
+[[maybe_unused]] static std::shared_ptr<roboteam_proto::WorldRobot> getWorldBot(unsigned int id, bool ourTeam, const roboteam_proto::World &world) {
+    /** Heavily inefficient, copying over all the robots :(
+     * If this was C++20 I would've picked std::span, but for now just use yellow() / blue()
+     */
+    // if (ourTeam) {
+    //     robots = std::vector<roboteam_proto::WorldRobot>( world.yellow().begin(),  world.yellow().end());
+    // } else {
+    //     robots = std::vector<roboteam_proto::WorldRobot>(world.blue().begin(), world.blue().end());
+    // }
 
-    std::vector<proto::WorldRobot> robots;
+    // Prevent a copy.
+    auto& robots = ourTeam ? world.yellow() : world.blue();
 
-    if (ourTeam) {
-        robots = std::vector<proto::WorldRobot>( world.yellow().begin(),  world.yellow().end());
-    } else {
-        robots = std::vector<proto::WorldRobot>(world.blue().begin(), world.blue().end());
-
-    }
-
+    // https://en.cppreference.com/w/cpp/algorithm/find
+    // Should do that instead, but whatever, doesn't really matter in terms of performance
     for (const auto &bot : robots) {
         if (bot.id() == id) {
 
-            return std::make_shared<proto::WorldRobot>(bot);
+            return std::make_shared<roboteam_proto::WorldRobot>(bot);
         }
     }
     return nullptr;
 }
 
-static void printbits(packed_protocol_message byteArr) {
-    for (int b = 0; b<12; b++) {
+/**
+ * Maybe unused surpresses compiler warnings, nothing major it's just better to 
+ * explicitly mark them unused
+ */
+[[maybe_unused]] static void printbits(packed_protocol_message const& byteArr) {
+    // for (int b = 0; b<12; b++) {
 
-        std::cout << "    ";
-        uint8_t byte = byteArr[b];
-//            for (int i = 0; i < 8; i++) {
-        for (int i = 7; i>=0; i--) {
-            if ((byte & (1 << i))==(1 << i)) {
-                std::cout << "1";
-            } else {
-                std::cout << "0";
-            }
-        }
-        std::cout << std::endl;
+    for (auto const bit : byteArr) { // Litearlly def'd to std::array
+    /**
+     * std::array implements begin() and end(), hence this is easier
+     */
+        std::cout << "    " << std::bitset<8>(bit) << std::endl;
     }
-}
+        // for (int i = 0; i < 8; i++) {
+        // for (int i = 7; i>=0; i--) {
+        //     if ((byte & (1 << i))==(1 << i)) {
+        //         std::cout << "1";
+        //     } else {
+        //         std::cout << "0";
+        //     }
+        // }
+        // std::cout << std::endl;
+    }
 
 } // utils
 } // robothub

624200fe8e085a185f9fa162300903a41dfb50b0
rolfvdhulst <rolfvdhulst@gmail.com>
2019-12-15 22:01:40 +0100

    move files and add camera based filtering

---
+++ src/kalman/CameraFilter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rolf on 15-12-19.
+//
+
+#include "CameraFilter.h"
---
+++ src/kalman/CameraFilter.h
@@ -0,0 +1,14 @@
+//
+// Created by rolf on 15-12-19.
+//
+
+#ifndef RTT_CAMERAFILTER_H
+#define RTT_CAMERAFILTER_H
+
+
+class CameraFilter {
+
+};
+
+
+#endif //RTT_CAMERAFILTER_H

8b9367174ce5a1b296c8bd5bd79778d6a59f88cc
mrlukasbos <mrlukasbos@gmail.com>
2020-01-23 20:20:50 +0100

    move pid to rtt namespace

--- include/roboteam_utils/pid.h
+++ include/roboteam_utils/pid.h
@@ -6,67 +6,67 @@
 #define ROBOTEAM_AI_PID_H
 
 #include <tuple>
-
+namespace rtt {
 class PID {
-   public:
-    PID(double, double, double);
-    PID(double, double, double, double);
-    PID(std::tuple<double, double, double>);
-    PID(std::tuple<double, double, double, double>);
+ public:
+  PID(double, double, double);
+  PID(double, double, double, double);
+  PID(std::tuple<double, double, double>);
+  PID(std::tuple<double, double, double, double>);
 
-    void setP(double);
-    void setI(double);
-    void setD(double);
-    void setF(double);
-    void setPID(double, double, double);
-    void setPID(std::tuple<double, double, double>);
-    void setPID(std::tuple<double, double, double>, double);
+  void setP(double);
+  void setI(double);
+  void setD(double);
+  void setF(double);
+  void setPID(double, double, double);
+  void setPID(std::tuple<double, double, double>);
+  void setPID(std::tuple<double, double, double>, double);
 
-    void setPID(double, double, double, double);
-    void setMaxIOutput(double);
-    void setOutputLimits(double);
-    void setOutputLimits(double, double);
-    void setDirection(bool);
-    void setSetpoint(double);
-    void reset();
-    void setOutputRampRate(double);
-    void setSetpointRange(double);
-    void setOutputFilter(double);
-    double getOutput();
-    double getOutput(double);
-    double getOutput(double, double);
+  void setPID(double, double, double, double);
+  void setMaxIOutput(double);
+  void setOutputLimits(double);
+  void setOutputLimits(double, double);
+  void setDirection(bool);
+  void setSetpoint(double);
+  void reset();
+  void setOutputRampRate(double);
+  void setSetpointRange(double);
+  void setOutputFilter(double);
+  double getOutput();
+  double getOutput(double);
+  double getOutput(double, double);
 
-   private:
-    static double clamp(double, double, double);
-    static bool bounded(double, double, double);
-    void checkSigns();
-    void init();
-    static constexpr int TICK_RATE = 60;
-    double P;
-    double I;
-    double D;
-    double F;
+ private:
+  static double clamp(double, double, double);
+  static bool bounded(double, double, double);
+  void checkSigns();
+  void init();
+  static constexpr int TICK_RATE = 60;
+  double P;
+  double I;
+  double D;
+  double F;
 
-    double maxIOutput;
-    double maxError;
-    double errorSum;
+  double maxIOutput;
+  double maxError;
+  double errorSum;
 
-    double maxOutput;
-    double minOutput;
+  double maxOutput;
+  double minOutput;
 
-    double setpoint;
+  double setpoint;
 
-    double lastActual;
+  double lastActual;
 
-    bool firstRun;
-    bool reversed;
+  bool firstRun;
+  bool reversed;
 
-    double outputRampRate;
-    double lastOutput;
+  double outputRampRate;
+  double lastOutput;
 
-    double outputFilter;
+  double outputFilter;
 
-    double setpointRange;
+  double setpointRange;
 };
-
+} // rtt
 #endif  // ROBOTEAM_AI_PID_H

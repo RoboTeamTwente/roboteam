80abfc2215034f6b540450c731e1697816f06ced
rolfvdhulst <37153373+rolfvdhulst@users.noreply.github.com>
2020-01-24 13:54:33 +0100

    Feature/geometry classes cleanup (#33)
    * fix epsilon bullshit
    
    * Fix cmake indent
    
    * remove explicit specifiers again
    
    * Remove LineBase and polymorphism
    
    * Add copy constructors Line-LineSegment
    
    * optional > shared_ptr
    
    * some spelling
    
    * fix codefactor
    
    * fix tests
    
    * Fixed travis
    
    * fix requested changes
    
    Co-authored-by: Yuhanun Citgez <y.citgez@student.utwente.nl>
    Co-authored-by: Lukas Bos <mrlukasbos@gmail.com>

--- include/roboteam_utils/Line.h
+++ include/roboteam_utils/Line.h
@@ -5,40 +5,129 @@
 #ifndef ROBOTEAM_UTILS_LINE_H
 #define ROBOTEAM_UTILS_LINE_H
 
-#include "LineBase.h"
+#include "Vector2.h"
 
 namespace rtt {
-    /**
-     * @brief Line class, inherits from Base
-     * 
-     */
-    class Line : public LineBase {
+class LineSegment;
+/**
+ * @brief Line class, inherits from Base
+ *
+ */
+class Line {
     public:
+        /**
+        * @brief Start of the line
+        *
+        */
+        Vector2 start;
+        /**
+         * @brief End of the line
+         *
+         */
+        Vector2 end;
         /**
          * @brief Construct a new Line object
          * 
          */
-        Line() = default;
+        constexpr Line() = default;
 
         /**
          * @brief Constructs a new Line object
-         * 
-         * Calls the LineBase constructor aswel
          * 
          * @param _start Start of the Line
          * @param _end End of the Line
          * 
          */
-        constexpr Line(const Vector2 &_start, const Vector2 &_end)
-                : LineBase(_start, _end) {};
+        constexpr Line(const Vector2 &_start, const Vector2 &_end) noexcept
+                :start{_start}, end{_end} { } ;
+        /**
+         * @brief Constructs a new Line from a LineSegment.
+         * @param other LineSegment to use for construction
+         */
+         explicit Line(const LineSegment &other) noexcept;
+        /**
+         * @brief Gets the length of the vector representation of this line.
+         *        Note in this case it does not really make sense as this class represents lines of infinite length
+         * Literally:
+         *      (end - start).length()
+         * @return double Gets the length of the line
+         */
+        [[nodiscard]] double length() const;
 
+        /**
+         * @brief Gets the length of the vector representation of this line
+         *
+         * @return double Length of this Line
+         */
+        [[nodiscard]] double length2() const;
+
+        /**
+         * @brief Gets the slope of this line
+         *
+         * @return double Slope of the line
+         */
+        [[nodiscard]] double slope() const;
+
+        /**
+         * @brief Gets the intercept of this line
+         * Literally:
+         *      start.y - slope() * start.x;
+         * @return double Intercept of this line
+         */
+        [[nodiscard]] double intercept() const;
+
+        /**
+         * @brief Gets the direction of the Line
+         *
+         * @return Vector2 Vector representation of the direction of this vector
+         */
+        [[nodiscard]] Vector2 direction() const;
+
+        /**
+         * @brief Gets a pair of coefficients
+         *
+         * @return std::pair<double, double> Pair of doubles where .first == slope() and .second == intercept()
+         */
+        [[nodiscard]] std::pair<double, double> coefficients() const;
+
+        /**
+         * @brief Checks whether line is vertical
+         *
+         * @return true True if line is vertical, will be true if isPoint is true
+         * @return false False if line is not vertical
+         */
+        [[nodiscard]] bool isVertical() const;
+
+        /**
+         * @brief Checks whether 2 lines are parralel
+         *
+         * @param line Other line to check against
+         * @return true True if this->slope() == line.slope()
+         * @return false False if this->slope() != line.slope()
+         */
+        [[nodiscard]] bool isParallel(const Line &line) const;
+        /**
+         * @brief Checks whether 2 lines are parralel
+         *
+         * @param line Other line to check against
+         * @return true True if this->slope() == line.slope()
+         * @return false False if this->slope() != line.slope()
+         */
+        [[nodiscard]] bool isParallel(const LineSegment &line) const;
+        /**
+         * @brief Checks whether line is a single point
+         *
+         * @return true True if start == end
+         * @return false False if start != end
+         */
+        [[nodiscard]] bool isPoint() const;
         /**
          * @brief Gets the distance from \ref point to the line
          * 
          * @param point Point to get distance to
          * @return double Distance to line
          */
-        [[nodiscard]] double distanceToLine(const Vector2 &point) const override;
+        [[nodiscard]] double distanceToLine(const Vector2 &point) const;
 
         /**
          * @brief Checks whether a \ref point is on the line
@@ -47,7 +136,7 @@ namespace rtt {
          * @return true If \ref point is on `this`
          * @return false If \ref point is not on `this`
          */
-        [[nodiscard]] bool isOnLine(const Vector2 &point) const override;
+        [[nodiscard]] bool isOnLine(const Vector2 &point) const;
 
         /**
          * @brief Projects the point onto the line
@@ -55,7 +144,7 @@ namespace rtt {
          * @param point Point to get the projection to
          * @return Vector2 Vector projection from the line to the point
          */
-        [[nodiscard]] Vector2 project(const Vector2 &point) const override;
+        [[nodiscard]] Vector2 project(const Vector2 &point) const;
 
         /**
          * @brief Gets the intersection of the lines
@@ -64,7 +153,7 @@ namespace rtt {
          * @param line Line to get an intersection from
          * @return std::shared_ptr<Vector2> Vector representation of this intersection
          */
-        [[nodiscard]] std::shared_ptr<Vector2> intersects(const Line &line) const override;
+        [[nodiscard]] std::optional<Vector2> intersects(const Line &line) const;
 
         /**
          * @brief Gets the intersection of the lines
@@ -73,7 +162,7 @@ namespace rtt {
          * @param line LineSegment to get an intersection from
          * @return std::shared_ptr<Vector2> Vector representation of this intersection
          */
-        [[nodiscard]] std::shared_ptr<Vector2> intersects(const LineSegment &line) const override;
+        [[nodiscard]] std::optional<Vector2> intersects(const LineSegment &line) const ;
 
         /**
          * @brief Checks whether \ref line intersects `this`
@@ -82,7 +171,7 @@ namespace rtt {
          * @return true if \ref line intersects `this`
          * @return false False if \ref line does not intersect `this`
          */
-        [[nodiscard]] bool doesIntersect(const Line &line) const override;
+        [[nodiscard]] bool doesIntersect(const Line &line) const;
 
         /**
          * @brief Checks whether \ref line intersects `this`
@@ -91,9 +180,9 @@ namespace rtt {
          * @return true True if \ref line intersects `this`
          * @return false False if \ref line does not intersect `this`
          */
-        [[nodiscard]] bool doesIntersect(const LineSegment &line) const override;
+        [[nodiscard]] bool doesIntersect(const LineSegment &line) const;
 
-    };
+};
 
 }
 
--- src/Vector2.cpp
+++ src/Vector2.cpp
@@ -1,218 +1,217 @@
 #include "../include/roboteam_utils/Vector2.h"
 #include "../include/roboteam_utils/Angle.h"
-
+#include "../include/roboteam_utils/Definitions.h"
 #include <cmath>
 
 namespace rtt {
 
-    Vector2::Vector2(rtt::Angle &angle, const double &length)
-        : x{ cos(angle.getAngle()) * length}, y{ sin(angle.getAngle()) * length }, epsilon{ 0.00001 }
-    {}
+Vector2::Vector2(rtt::Angle &angle, const double &length)
+        :x{cos(angle.getAngle())*length}, y{sin(angle.getAngle())*length} { }
 
-    double Vector2::dot(const Vector2 &other) const {
-        return this->x * other.x + this->y * other.y;
+double Vector2::dot(const Vector2 &other) const {
+    return this->x*other.x + this->y*other.y;
+}
+
+double Vector2::dist(const Vector2 &other) const {
+    return sqrt(dist2(other));
+}
+
+double Vector2::dist2(const Vector2 &other) const {
+    return (*this - other).length2();
+}
+
+Vector2 Vector2::scale(double scalar) const {
+    return {x*scalar, y*scalar};
+}
+
+Vector2 Vector2::normalize() const {
+    if (this->length() == 0.0)
+        return {0.0, 0.0};
+
+    double d = 1.0/length();
+    return {x*d, y*d};
+}
+
+double Vector2::length() const {
+    return sqrt(this->length2());
+}
+
+double Vector2::length2() const {
+    return (x*x + y*y);
+}
+
+double Vector2::angle() const {
+    return this->toAngle().getAngle();
+}
+
+rtt::Angle Vector2::toAngle() const {
+    return Angle(atan2(y, x));
+}
+
+Vector2 Vector2::lerp(const Vector2 &other, double factor) const {
+    return this->scale(factor) + other.scale(1 - factor);
+}
+
+Vector2 Vector2::rotate(double radians) const {
+    double c = cos(radians);
+    double s = sin(radians);
+    return Vector2(x*c - y*s, x*s + y*c);
+}
+
+Vector2 Vector2::project(const Vector2 &lineA, const Vector2 &lineB) const {
+    Vector2 ab = lineB - lineA;
+    Vector2 ap = *this - lineA;
+    double t = ap.dot(ab)/ab.dot(ab);
+    if (t < 0) {
+        return lineA;
     }
-
-    double Vector2::dist(const Vector2 &other) const {
-        return sqrt(dist2(other));
+    else if (t > 1) {
+        return lineB;
     }
+    return lineA + ab.scale(t);
+}
 
-    double Vector2::dist2(const Vector2 &other) const {
-        return (*this - other).length2();
+Vector2 Vector2::project2(const Vector2 &ab) const {
+    return ab.scale(
+            this->dot(ab)/ab.dot(ab));
+}
+
+bool Vector2::isNotNaN() const {
+    /**
+     * NaN is defined as not being equal to itself
+     */
+    return x == x && y == y;
+}
+
+Vector2 Vector2::closestPointOnVector(const Vector2 &startPoint, const Vector2 &point) const {
+    Vector2 vectorToPoint = point - startPoint;
+    Angle me = this->toAngle();
+    Angle vtp = vectorToPoint.toAngle();
+    Angle a = me - vtp;
+    double angle = a.getAngle();
+    double projectionLength = vectorToPoint.length()*cos(angle);
+
+    Vector2 closestPoint;
+    if (projectionLength > this->length()) {
+        closestPoint = *this + startPoint;
     }
-
-    Vector2 Vector2::scale(double scalar) const {
-        return { x * scalar, y * scalar };
+    else if (projectionLength < 0) {
+        closestPoint = startPoint;
     }
-
-    Vector2 Vector2::normalize() const {
-        if (this->length() == 0.0)
-            return { 0.0, 0.0 };
-
-        double d = 1.0 / length();
-        return {x * d, y * d};
+    else {
+        closestPoint = this->scale(projectionLength/this->length()) + startPoint;
     }
+    return closestPoint;
+}
 
-    double Vector2::length() const {
-        return sqrt(this->length2());
+Vector2 Vector2::stretchToLength(double desiredLength) const {
+    if (length() == 0.0) {
+        return {desiredLength, 0};
     }
+    double frac = desiredLength/length();
+    return {x*frac, y*frac};
+}
 
-    double Vector2::length2() const {
-        return (x * x + y * y);
-    }
+double Vector2::cross(const Vector2 &other) const {
+    return this->x*other.y - this->y*other.x;
+}
 
-    double Vector2::angle() const {
-        return this->toAngle().getAngle();
-    }
+bool Vector2::operator==(const Vector2 &other) const {
+    return fabs(this->x - other.x) < VECTOR_PRECISION && fabs(this->y - other.y) < VECTOR_PRECISION;
+}
 
-    rtt::Angle Vector2::toAngle() const {
-        if (this->length() < epsilon)
-            return { 0 };
+bool Vector2::operator!=(const Vector2 &other) const {
+    return ! (*this == other);
+}
 
-        return { atan2(y, x) };
-    }
+bool Vector2::operator<(const Vector2 &other) const {
+    return this->length() < other.length();
+}
 
-    Vector2 Vector2::lerp(const Vector2 &other, double factor) const {
-        return this->scale(factor) + other.scale(1 - factor);
-    }
+Vector2 Vector2::operator+=(const Vector2 &other) {
+    return {this->x += other.x, this->y += other.y};
+}
 
-    Vector2 Vector2::rotate(double radians) const {
-        double c = cos(radians);
-        double s = sin(radians);
-        return Vector2(x * c - y * s, x * s + y * c);
-    }
+Vector2 Vector2::operator+=(const double &scalar) {
+    return {x += scalar, y += scalar};
+}
 
-    Vector2 Vector2::project(const Vector2 &lineA, const Vector2 &lineB) const {
-        Vector2 ab = lineB - lineA;
-        Vector2 ap = *this - lineA;
-        double t = ap.dot(ab) / ab.dot(ab);
-        if (t < 0) {
-            return lineA;
-        } else if (t > 1) {
-            return lineB;
-        }
-        return lineA + ab.scale(t);
-    }
+Vector2 Vector2::operator-=(const Vector2 &other) {
+    return {this->x -= other.x, this->y -= other.y};
+}
 
-    Vector2 Vector2::project2(const Vector2 &ab) const {
-        return ab.scale(
-            this->dot(ab) / ab.dot(ab));
-    }
+Vector2 Vector2::operator-=(const double &scalar) {
+    return {x -= scalar, y -= scalar};
+}
 
-    bool Vector2::isNotNaN() const {
-        /**
-         * NaN is defined as not being equal to itself
-         */
-        return x == x && y == y; 
-    }
+Vector2 Vector2::operator*=(const Vector2 &other) {
+    return {this->x *= other.x, this->y *= other.y};
+}
 
-    Vector2 Vector2::closestPointOnVector(const Vector2 &startPoint, const Vector2 &point) const {
-        Vector2 vectorToPoint = point - startPoint;
-        Angle me = this->toAngle();
-        Angle vtp = vectorToPoint.toAngle();
-        Angle a = me - vtp;
-        double angle = a.getAngle();
-        double projectionLength = vectorToPoint.length() * cos(angle);
+Vector2 Vector2::operator*=(const double &scalar) {
+    return {x *= scalar, y *= scalar};
+}
 
-        Vector2 closestPoint;
-        if (projectionLength > this->length()) {
-            closestPoint = *this + startPoint;
-        } else if (projectionLength < 0) {
-            closestPoint = startPoint;
-        } else {
-            closestPoint = this->scale(projectionLength / this->length()) + startPoint;
-        }
-        return closestPoint;
-    }
+Vector2 Vector2::operator/=(const Vector2 &other) {
+    assert(other == Vector2() && "Division by zero");
+    return {this->x /= other.x, this->y /= other.y};
+}
 
-    Vector2 Vector2::stretchToLength(double desiredLength) const {
-        if (length() == 0.0) {
-            return {desiredLength, 0};
-        }
-        double frac = desiredLength / length();
-        return {x * frac, y * frac};
-    }
+Vector2 Vector2::operator/=(const double &scalar) {
+    return {x /= scalar, y /= scalar};
+}
 
-    double Vector2::cross(const Vector2 &other) const {
-        return this->x * other.y - this->y * other.x;
-    }
+Vector2 Vector2::operator+(const Vector2 &other) const {
+    return {x + other.x, y + other.y};
+}
 
-    bool Vector2::operator==(const Vector2 &other) const {
-        return fabs(this->x - other.x) < epsilon && fabs(this->y - other.y) < epsilon;
-    }
+Vector2 Vector2::operator+(const double &scalar) const {
+    return {x + scalar, y + scalar};
+}
 
-    bool Vector2::operator!=(const Vector2 &other) const {
-        return !(*this == other);
-    }
+Vector2 Vector2::operator-(const Vector2 &other) const {
+    return {this->x - other.x, this->y - other.y};
+}
 
-    bool Vector2::operator<(const Vector2 &other) const {
-        return this->length() < other.length();
-    }
+Vector2 Vector2::operator-(const double &scalar) const {
+    return {x - scalar, y - scalar};
+}
 
-    Vector2 Vector2::operator+=(const Vector2 &other) {
-        return {this->x += other.x, this->y += other.y};
-    }
+Vector2 Vector2::operator*(const Vector2 &other) const {
+    return {this->x*other.x, this->y*other.y};
+}
 
-    Vector2 Vector2::operator+=(const double &scalar) {
-        return {x += scalar, y += scalar};
-    }
+Vector2 Vector2::operator*(const double &scalar) const {
+    return {x*scalar, y*scalar};
+}
 
-    Vector2 Vector2::operator-=(const Vector2 &other) {
-        return {this->x -= other.x, this->y -= other.y};
-    }
+Vector2 Vector2::operator/(const Vector2 &other) const {
+    return {this->x/other.x, this->y/other.y};
+}
 
-    Vector2 Vector2::operator-=(const double &scalar) {
-        return {x -= scalar, y -= scalar};
-    }
+Vector2 Vector2::operator/(const double &scalar) const {
+    return {x/scalar, y/scalar};
+}
 
-    Vector2 Vector2::operator*=(const Vector2 &other) {
-        return {this->x *= other.x, this->y *= other.y};
-    }
+void Vector2::operator=(const proto::Vector2f &msg) {
+    x = msg.x();
+    y = msg.y();
+}
 
-    Vector2 Vector2::operator*=(const double &scalar) {
-        return {x *= scalar, y *= scalar};
-    }
+Vector2::operator proto::Vector2f() const {
+    proto::Vector2f msg;
+    msg.set_x(x);
+    msg.set_y(y);
+    return msg;
+}
 
-    Vector2 Vector2::operator/=(const Vector2 &other) {
-        assert(other == Vector2() && "Division by zero");
-        return {this->x /= other.x, this->y /= other.y};
-    }
+std::ostream &Vector2::write(std::ostream &os) const {
+    return os << "{ x = " << x << ", y = " << y << " }";
+}
 
-    Vector2 Vector2::operator/=(const double &scalar) {
-        return {x /= scalar, y /= scalar};
-    }
-
-    Vector2 Vector2::operator+(const Vector2 &other) const {
-        return {x + other.x, y + other.y};
-    }
-
-    Vector2 Vector2::operator+(const double &scalar) const {
-        return {x + scalar, y + scalar};
-    }
-
-    Vector2 Vector2::operator-(const Vector2 &other) const {
-        return {this->x - other.x, this->y - other.y};
-    }
-
-    Vector2 Vector2::operator-(const double &scalar) const {
-        return {x - scalar, y - scalar};
-    }
-
-    Vector2 Vector2::operator*(const Vector2 &other) const {
-        return {this->x * other.x, this->y * other.y};
-    }
-
-    Vector2 Vector2::operator*(const double &scalar) const {
-        return {x * scalar, y * scalar};
-    }
-
-    Vector2 Vector2::operator/(const Vector2 &other) const {
-        return {this->x / other.x, this->y / other.y};
-    }
-
-    Vector2 Vector2::operator/(const double &scalar) const {
-        return {x / scalar, y / scalar};
-    }
-
-    void Vector2::operator=(const proto::Vector2f &msg) {
-        x = msg.x();
-        y = msg.y();
-    }
-
-    Vector2::operator proto::Vector2f() const {
-        proto::Vector2f msg;
-        msg.set_x(x);
-        msg.set_y(y);
-        return msg;
-    }
-
-    std::ostream &Vector2::write(std::ostream &os) const {
-        return os << "{ x = " << x << ", y = " << y << " }";
-    }
-
-    std::ostream &operator<<(std::ostream &os, rtt::Vector2 const& vec) {
-        return vec.write(os);
-    }
+std::ostream &operator<<(std::ostream &os, rtt::Vector2 const &vec) {
+    return vec.write(os);
+}
 
 }
 

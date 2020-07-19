b84a88a0c9353588d5bb89b00e4998d3628ccc62
Haico Dorenbos <HaicoDorenbos@gmail.com>
2020-07-19 20:27:13 +0200

    Removed dead code and tag from Line and LineSegment classes

--- include/roboteam_utils/Line.h
+++ include/roboteam_utils/Line.h
@@ -50,7 +50,7 @@ class Line {
     [[nodiscard]] Vector2 project(const Vector2 &point) const;
 
     /**
-     * Get the intersection point between two (infinite) Lines. In case there is no intersection (Lines are parallel and distinct), return std
+     * Get the intersection point between two (infinite) Lines.
      *
      * @param line The other (infinite) Line.
      * @return In case there is no intersection (i.e. the Lines are parallel and distinct), return std::nullopt. In case there is a single intersection, return that point (Lines
@@ -88,96 +88,6 @@ class Line {
      * @return The value t such that p1 + (p2 - p1) * t = pointOnLine
      */
     static float relativePosition(const Vector2 p1, const Vector2 p2, const Vector2 pointOnLine);
-
-    /* 	 ______   _______  _______  ______     _______  _______  ______   _______
-     *	(  __  \ (  ____ \(  ___  )(  __  \   (  ____ \(  ___  )(  __  \ (  ____ \
-     *	| (  \  )| (    \/| (   ) || (  \  )  | (    \/| (   ) || (  \  )| (    \/
-     *	| |   ) || (__    | (___) || |   ) |  | |      | |   | || |   ) || (__
-     *	| |   | ||  __)   |  ___  || |   | |  | |      | |   | || |   | ||  __)
-     *	| |   ) || (      | (   ) || |   ) |  | |      | |   | || |   ) || (
-     *	| (__/  )| (____/\| )   ( || (__/  )  | (____/\| (___) || (__/  )| (____/\
-     *	(______/ (_______/|/     \|(______/   (_______/(_______)(______/ (_______/
-     *
-     * The functions below are dead. Remove this tag if you use any of the functions and make sure to remove this tag at other places as well that will become alive by using any of
-     *the function below. Do not read/document/redesign/analyse/test/optimize/etc. any of this code, because it is a waste of your time! This code was not removed or placed at
-     *another branch, because other software developers are very attached to this code and are afraid that this code might be used at some day (but I think it won't be used at all
-     *and should be removed).
-     */
-
-    /**
-     * @brief Gets the intersection of the lines
-     * See https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection for help
-     *
-     * @param line LineSegment to get an intersection from
-     * @return std::shared_ptr<Vector2> Vector representation of this intersection
-     */
-    // [[nodiscard]] std::optional<Vector2> intersects(const LineSegment &line) const;
-
-    /**
-     * @brief Checks whether \ref line intersects `this`
-     *
-     * @param line Line to check against
-     * @return true True if \ref line intersects `this`
-     * @return false False if \ref line does not intersect `this`
-     */
-    // [[nodiscard]] bool doesIntersect(const LineSegment &line) const;
-
-    /**
-     * @brief Checks whether 2 lines are parralel
-     *
-     * @param line Other line to check against
-     * @return true True if this->slope() == line.slope()
-     * @return false False if this->slope() != line.slope()
-     */
-    // [[nodiscard]] bool isParallel(const Line &line) const;
-
-    /**
-     * @brief Checks whether 2 lines are parralel
-     *
-     * @param line Other line to check against
-     * @return true True if this->slope() == line.slope()
-     * @return false False if this->slope() != line.slope()
-     */
-    // [[nodiscard]] bool isParallel(const LineSegment &line) const;
-
-    /**
-     * @brief Gets the intercept of this line
-     * Literally:
-     *      start.y - slope() * start.x;
-     * @return double Intercept of this line
-     */
-    // [[nodiscard]] double intercept() const;
-
-    /**
-     * @brief Gets a pair of coefficients
-     *
-     * @return std::pair<double, double> Pair of doubles where .first == slope() and .second == intercept()
-     */
-    // [[nodiscard]] std::pair<double, double> coefficients() const;
-
-    /**
-     * @brief Gets the slope of this line
-     *
-     * @return double Slope of the line
-     */
-    // [[nodiscard]] double slope() const;
-
-    /**
-     * @brief Checks whether line is vertical
-     *
-     * @return true True if line is vertical, will be true if isPoint is true
-     * @return false False if line is not vertical
-     */
-    // [[nodiscard]] bool isVertical() const;
-
-    /**
-     * @brief Checks whether \ref line intersects `this`
-     *
-     * @param line Line to check against
-     * @return true if \ref line intersects `this`
-     * @return false False if \ref line does not intersect `this`
-     */
-    // [[nodiscard]] bool doesIntersect(const Line &line) const;
 };
 }  // namespace rtt
 

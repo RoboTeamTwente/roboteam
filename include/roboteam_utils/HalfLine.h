eab6aa5e65712aa442ace99f0d7d4a154f39d3c4
Haico Dorenbos <HaicoDorenbos@gmail.com>
2020-07-19 23:21:24 +0200

    Undo Johan fix

--- include/roboteam_utils/HalfLine.h
+++ include/roboteam_utils/HalfLine.h
@@ -22,7 +22,7 @@ class HalfLine {
          * @param start The location where the HalfLine starts.
          * @param goesThrough An arbitrary other vector through the HalfLine goes (make sure that it is different than the start point).
          */
-        contexpr HalfLine(const Vector2 &start, const Vector2 &goesThrough);
+        explicit HalfLine(const Vector2 &start, const Vector2 &goesThrough);
 
         /**
          * Compute the intersection point between this HalfLine and the given Line.

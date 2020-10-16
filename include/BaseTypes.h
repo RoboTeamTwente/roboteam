7b92e7b5eb53b4217858ad1b3c8eaec81a3f4d4c
Emiel Steerneman <emielsteerneman@gmail.com>
2020-10-16 16:29:36 +0200

    Changed last two reserved packets to LOG for basestation and robot; Note: robot doensn't yet support log, because SX1280 can't send variable length messages;

--- include/BaseTypes.h
+++ include/BaseTypes.h
@@ -1,12 +1,19 @@
 #ifndef __BASETYPES_H
 #define __BASETYPES_H
 
+/** Warning : Check the unicode table before assigning a byte, to make 
+ *  sure that the byte isn't used for anything special : https://unicode-table.com/
+ * For example, don't use the following bytes
+ *      0b00000000 : The null-terminator, used to signal the end of strings / arrays / etc.
+ *      0b00001010 : The byte for newline, used for line termination.
+ * */
 typedef enum _PACKET_TYPE { 
 	PACKET_TYPE_ROBOT_COMMAND                  = 0b01100110,
     PACKET_TYPE_ROBOT_FEEDBACK                 = 0b00001111,
     PACKET_TYPE_ROBOT_SET_SETTINGS             = 0b00110011,
     PACKET_TYPE_ROBOT_GET_SETTINGS             = 0b00111100,
     PACKET_TYPE_ROBOT_SETTINGS                 = 0b01010101,
+    // PACKET_TYPE_ROBOT_LOG                      = 0b01101001,
     PACKET_TYPE_ROBOT_ALERT                    = 0b01011010,
 
     PACKET_TYPE_BASESTATION_SET_SETTINGS       = 0b10010110,
@@ -14,10 +21,8 @@ typedef enum _PACKET_TYPE {
     PACKET_TYPE_BASESTATION_SETTINGS           = 0b10100101,
     PACKET_TYPE_BASESTATION_GET_STATISTICS     = 0b10101010,
     PACKET_TYPE_BASESTATION_STATISTICS         = 0b11000011,
-    PACKET_TYPE_BASESTATION_ALERT              = 0b11001100,
-
-    PACKET_TYPE_RESERVED_1                     = 0b01101001,
-    PACKET_TYPE_RESERVED_2                     = 0b11110000
+    PACKET_TYPE_BASESTATION_LOG                = 0b11110000,
+    PACKET_TYPE_BASESTATION_ALERT              = 0b11001100
 } PACKET_TYPE;
 
 typedef enum _PACKET_SIZE { 
@@ -26,6 +31,7 @@ typedef enum _PACKET_SIZE {
     PACKET_SIZE_ROBOT_SET_SETTINGS             = 0,
     PACKET_SIZE_ROBOT_GET_SETTINGS             = 0,
     PACKET_SIZE_ROBOT_SETTINGS                 = 0,
+    // PACKET_TYPE_ROBOT_LOG                      = 0,
     PACKET_SIZE_ROBOT_ALERT                    = 0,
 
     PACKET_SIZE_BASESTATION_SET_SETTINGS       = 0,
@@ -33,10 +39,8 @@ typedef enum _PACKET_SIZE {
     PACKET_SIZE_BASESTATION_SETTINGS           = 0,
     PACKET_SIZE_BASESTATION_GET_STATISTICS     = 0,
     PACKET_SIZE_BASESTATION_STATISTICS         = 33,
-    PACKET_SIZE_BASESTATION_ALERT              = 0,
-
-    PACKET_SIZE_RESERVED_1                     = 0,
-    PACKET_SIZE_RESERVED_2                     = 0
+    PACKET_SIZE_BASESTATION_LOG                = 0,
+    PACKET_SIZE_BASESTATION_ALERT              = 0
 } PACKET_SIZE;
 
 # endif /*__BASETYPES_H*/

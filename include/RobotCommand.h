b7d9724c9c98846e5d2e000b9dd6011c58da54f0
Cas Doornkamp <cjdoornkamp@gmail.com>
2020-10-18 16:31:18 +0200

    added documentation; added setters

--- include/RobotCommand.h
+++ include/RobotCommand.h
@@ -10,71 +10,149 @@ typedef struct _RobotCommandPayload {
     uint8_t payload[PACKET_SIZE_ROBOT_COMMAND];
 } RobotCommandPayload;
 
+// Conversion constants
+#define CONVERT_RHO 			0.004f
+#define CONVERT_THETA 			0.00307f
+#define CONVERT_YAW_REF 		0.00614f
+#define CONVERT_SHOOTING_POWER 	0.39f
+#define CONVERT_DRIBBLE_SPEED 	3.125f
+#define CONVERT_VISION_YAW 		0.00307f
+
+                                //Description                 Values            Units     	  Represented values    Bits
 typedef struct _RobotCommand{
-    uint8_t header;
-    uint8_t id;
-    uint16_t rho;
-    int16_t theta;
-    int16_t angularVelocity;
-    uint8_t power;
-    bool doKick;
-    bool doChip;
-    bool force;
-    bool debugInfo;
-    bool useCamInfo;
-    uint8_t geneva;
-    uint8_t dribblerSpeed;
-    int16_t camRotation;
+    PACKET_TYPE header;         //Packet type (RobotCommand)  -                 -             -                        8
+    uint8_t id:8;               //Robot ID                    [0, 15]           -             [0, 15]                  4
+    uint16_t rho:11;            //Velocity magnitude          [0, 2047]         0.004m/s      [0, 8.188]              11
+    int16_t theta:11;           //Velocity angle              [-1024, 1023]     0.00307rad    [-pi, pi>               11
+    int16_t angularVelocity:10; //Reference angular velocity  [-512, 511]       0.098rad/s    [-8*2pi, 8*2pi]         10
+    uint8_t power:8;            //Kick/chip power             [0, 255]          0.39%         [0, 100]%                8
+    bool doKick:1;              //Kick                        [0, 1]            -             {true, false}            1
+    bool doChip:1;              //Chip                        [0, 1]            -             {true, false}            1
+    bool force:1;               //Kick/chip immediately       [0, 1]            -             {true, false}            1
+    bool debugInfo:1;           //Debug information           [0, 1]            -             {true, false}            1
+    bool useCamInfo:1;          //Use camera information      [0, 1]            -             {true, false}            1
+    uint8_t geneva:3;           //Geneva drive state          [0, 7]            -             [-2, 2]                  3
+    uint8_t dribblerSpeed:5;    //Reference dribbler speed    [0, 31]          3.125%         [0, 100]%                5
+    int16_t camRotation:11;     //Orientation (seen bycamera) [-1024, 1023]     0.00307rad    [-pi, pi]               11
 } RobotCommand;
 
-inline uint8_t RobotCommand_getHeader(RobotCommandPayload *rcp){
-    return rcp->payload[0];
-};
+// Packet structure
+// [---0--] [---1--] [---2--] [---3--] [---4--] [---5--] [---6--] [---7--] [---8--] [---9--]
+// 11111111 -------- -------- -------- -------- -------- -------- -------- -------- -------- // header
+// -------- 11111111 -------- -------- -------- -------- -------- -------- -------- -------- // id
+// -------- -------- 11111111 111----- -------- -------- -------- -------- -------- -------- // rho
+// -------- -------- -------- ---11111 111111-- -------- -------- -------- -------- -------- // theta
+// -------- -------- -------- -------- ------11 11111111 -------- -------- -------- -------- // angularVelocity
+// -------- -------- -------- -------- -------- -------- 11111111 -------- -------- -------- // power
+// -------- -------- -------- -------- -------- -------- -------- 1------- -------- -------- // Kick
+// -------- -------- -------- -------- -------- -------- -------- -1------ -------- -------- // Chip
+// -------- -------- -------- -------- -------- -------- -------- --1----- -------- -------- // Force
+// -------- -------- -------- -------- -------- -------- -------- ---1---- -------- -------- // debug info
+// -------- -------- -------- -------- -------- -------- -------- ----1--- -------- -------- // use cam info
+// -------- -------- -------- -------- -------- -------- -------- -----111 -------- -------- // geneva
+// -------- -------- -------- -------- -------- -------- -------- -------- 11111--- -------- // dribbler speed
+// -------- -------- -------- -------- -------- -------- -------- -------- -----111 11111111 // cam rotation
+
+
+// -------------------------------------- GETTERS --------------------------------------
+inline PACKET_TYPE RobotCommand_getHeader(RobotCommandPayload *rcp){
+    return (PACKET_TYPE)rcp->payload[0];
+}
 inline uint8_t RobotCommand_getId(RobotCommandPayload *rcp){
     return rcp->payload[1];
-};
+}
 inline uint16_t RobotCommand_getRho(RobotCommandPayload *rcp){
     return ((rcp->payload[2] & 0b11111111) << 3)
          | ((rcp->payload[3] & 0b11100000) >> 5);
-};
+}
 inline int16_t RobotCommand_getTheta(RobotCommandPayload *rcp){
     return ((rcp->payload[3] & 0b00011111) << 6)
          | ((rcp->payload[4] & 0b11111100) >> 2);
-};
+}
 inline int16_t RobotCommand_getAngularVelocity(RobotCommandPayload *rcp){
     return ((rcp->payload[4] & 0b00000011) << 8)
          | ((rcp->payload[5] & 0b11111111) >> 0);
-};
+}
 inline uint8_t RobotCommand_getPower(RobotCommandPayload *rcp){
     return rcp->payload[6];
-};
+}
 inline bool RobotCommand_getDoKick(RobotCommandPayload *rcp){
-    return rcp->payload[7] & 0b10000000;
-};
+    return rcp->payload[7] & 0b10000000 ? true : false;
+}
 inline bool RobotCommand_getDoChip(RobotCommandPayload *rcp){
-    return rcp->payload[7] & 0b01000000;
-};
+    return rcp->payload[7] & 0b01000000 ? true : false;
+}
 inline bool RobotCommand_getForce(RobotCommandPayload *rcp){
-    return rcp->payload[7] & 0b00100000;
-};
+    return rcp->payload[7] & 0b00100000 ? true : false;
+}
 inline bool RobotCommand_getDebugInfo(RobotCommandPayload *rcp){
-    return rcp->payload[7] & 0b00010000;
-};
+    return rcp->payload[7] & 0b00010000 ? true : false;
+}
 inline bool RobotCommand_getUseCamInfo(RobotCommandPayload *rcp){
-    return rcp->payload[7] & 0b00001000;
-};
+    return rcp->payload[7] & 0b00001000 ? true : false;
+}
 inline uint8_t RobotCommand_getGeneva(RobotCommandPayload *rcp){
     return rcp->payload[7] & 0b00000111;
-};
+}
 inline uint8_t RobotCommand_getDribblerSpeed(RobotCommandPayload *rcp){
     return (rcp->payload[8] & 0b11111000) >> 3;
-};
+}
 inline int16_t RobotCommand_getCamRotation(RobotCommandPayload *rcp){
     return ((rcp->payload[8] & 0b00000111) << 8)
          | ((rcp->payload[9] & 0b11111111) >> 0);
-};
+}
 
-static inline void fillRobotCommand(RobotCommand *command, RobotCommandPayload *rcp){
+// -------------------------------------- SETTERS --------------------------------------
+// Note: functions assume an empty {0} packet to write to
+inline void RobotCommand_setHeader(RobotCommandPayload *rcp, PACKET_TYPE header){
+    rcp->payload[0] = header;
+}
+inline void RobotCommand_setId(RobotCommandPayload *rcp, uint8_t id){
+    rcp->payload[1] = id;
+}
+inline void RobotCommand_setRho(RobotCommandPayload *rcp, uint16_t rho){
+    rcp->payload[2]  = (rho >> 3) & 0b11111111;
+    rcp->payload[3] |= (rho << 5) & 0b11100000;
+}
+inline void RobotCommand_setTheta(RobotCommandPayload *rcp, int16_t theta){
+    rcp->payload[3] |= (theta >> 6) & 0b00011111;
+    rcp->payload[4] |= (theta << 2) & 0b11111100;
+}
+inline void RobotCommand_setAngularVelocity(RobotCommandPayload *rcp, int16_t angularVelocity){
+    rcp->payload[4] |= (angularVelocity >> 8) & 0b00000011;
+    rcp->payload[5] |= (angularVelocity >> 0) & 0b11111111;
+}
+inline void RobotCommand_setPower(RobotCommandPayload *rcp, uint8_t power){
+    rcp->payload[6]  = power;
+}
+inline void RobotCommand_setDoKick(RobotCommandPayload *rcp, bool kick){
+    rcp->payload[7] |= (kick << 7) & 0b10000000;
+}
+inline void RobotCommand_setDoChip(RobotCommandPayload *rcp, bool chip){
+    rcp->payload[7] |= (chip << 6) & 0b01000000;
+}
+inline void RobotCommand_setForce(RobotCommandPayload *rcp, bool force){
+    rcp->payload[7] |= (force << 5) & 0b00100000;
+}
+inline void RobotCommand_setDebugInfo(RobotCommandPayload *rcp, bool debugInfo){
+    rcp->payload[7] |= (debugInfo << 4) & 0b00010000;
+}
+inline void RobotCommand_setUseCamInfo(RobotCommandPayload *rcp, bool useCamInfo){
+    rcp->payload[7] |= (useCamInfo << 3) & 0b00001000;
+}
+inline void RobotCommand_setGeneva(RobotCommandPayload *rcp, uint8_t geneva){
+    rcp->payload[7] |= (geneva) & 0b00000111;
+}
+inline void RobotCommand_setDribblerSpeed(RobotCommandPayload *rcp, uint8_t dribblerSpeed){
+    rcp->payload[8] |= (dribblerSpeed << 3) & 0b11111000;
+}
+inline void RobotCommand_setCamRotation(RobotCommandPayload *rcp, int16_t camRotation){
+    rcp->payload[8] |= (camRotation << 8) & 0b00000111;
+    rcp->payload[9]  = (camRotation << 0) & 0b11111111;
+}
+
+// -------------------------------------- ENCODE/DECODE --------------------------------------
+static inline void decodeRobotCommand(RobotCommand *command, RobotCommandPayload *rcp){
     command->header = RobotCommand_getHeader(rcp);
     command->id = RobotCommand_getId(rcp);
     command->rho = RobotCommand_getRho(rcp);
@@ -89,6 +167,23 @@ static inline void fillRobotCommand(RobotCommand *command, RobotCommandPayload *
     command->geneva = RobotCommand_getGeneva(rcp);
     command->dribblerSpeed = RobotCommand_getDribblerSpeed(rcp);
     command->camRotation = RobotCommand_getCamRotation(rcp);
+}
+
+static inline void encodeRobotCommand(RobotCommandPayload *rcp, RobotCommand *command){
+    RobotCommand_setHeader(rcp, command->header);
+    RobotCommand_setId(rcp, command->id);
+    RobotCommand_setRho(rcp, command->rho);
+    RobotCommand_setTheta(rcp, command->theta);
+    RobotCommand_setAngularVelocity(rcp, command->angularVelocity);
+    RobotCommand_setPower(rcp, command->power);
+    RobotCommand_setDoKick(rcp, command->doKick);
+    RobotCommand_setDoChip(rcp, command->doChip);
+    RobotCommand_setForce(rcp, command->force);
+    RobotCommand_setDebugInfo(rcp, command->debugInfo);
+    RobotCommand_setUseCamInfo(rcp, command->useCamInfo);
+    RobotCommand_setGeneva(rcp, command->geneva);
+    RobotCommand_setDribblerSpeed(rcp, command->dribblerSpeed);
+    RobotCommand_setCamRotation(rcp, command->camRotation);
 }
 
 #endif /*__ROBOT_COMMAND_H*/
--- include/RobotFeedback.h
+++ include/RobotFeedback.h
@@ -10,46 +10,65 @@ typedef struct _RobotFeedbackPayload {
     uint8_t payload[PACKET_SIZE_ROBOT_FEEDBACK];
 } RobotFeedbackPayload;
 
+                                //Description                           Values          Units     	Represented values      Bits
 typedef struct _RobotFeedback {
-    uint8_t header;
-    uint8_t id;
-    bool XsensCalibrated;
-    bool battery;
-    bool ballSensorWorking;
-    bool hasBall;
-    uint8_t ballPos;
-    bool genevaWorking;
-    uint8_t genevaState;
-    int16_t rho;
-    int16_t angle;
-    int16_t theta;
-    bool wheelBraking;
-    uint8_t rssi;
+    PACKET_TYPE header;         //Packet type (RobotFeedback)           -
+    uint8_t id:8;               //Robot ID                              [0, 15]         -           [0, 15]                  8
+    bool XsensCalibrated:1;     //XsensCalibrated                       [0, 1]          -           {false, true}            1
+    bool battery:1;             //Battery empty                         [0, 1]          -           {false, true}            1
+    bool ballSensorWorking:1;   //Ballsensor active and working         [0, 1]          -           {false, true}            1
+    bool hasBall:1;             //Robot has ball                        [0, 1]          -           {false, true}            1
+    uint8_t ballPos:4;          //Ballpos seen by sensor                [0, 15]                     {left, .., right}        4
+    bool genevaWorking:1;       //Geneva active and working             [0, 1]          -           {false, true}            1
+    uint8_t genevaState:7;      //Geneva oriëntation                    [0, 5]          -           {none, left, .. right}   7
+    int16_t rho:11;             //Robot driving velocity                [0, 2047]       0.004m/s    [0, 8.188]              11
+    int16_t angle:10;           //Robot driving direction               [-512, 511]     0.00614f    [-pi, pi>               10
+    int16_t theta:11;           //Robot oriëntation                     [-1024, 1023]   0.00307rad  [-pi, pi]               11
+    bool wheelBraking:1;        //Robot has a wheel braking             [0, 1]          -           {false, true}            1
+    uint8_t rssi:7;             //Last received packet RSSI strength    [0, 127]        -1dBm       [0dBm, -127dBm]          7
 } RobotFeedback;
 
-inline uint8_t RobotFeedback_getHeader(RobotFeedbackPayload *rfp){
-    return rfp->payload[0];
+// Packet structure
+// [---0--] [---1--] [---2--] [---3--] [---4--] [---5--] [---6--] [---7--] [---8--]
+// 11111111 -------- -------- -------- -------- -------- -------- -------- -------- // header
+// -------- 11111111 -------- -------- -------- -------- -------- -------- -------- // id
+// -------- -------- 1------- -------- -------- -------- -------- -------- -------- // XsensCalibrated
+// -------- -------- -1------ -------- -------- -------- -------- -------- -------- // battery
+// -------- -------- --1----- -------- -------- -------- -------- -------- -------- // ballSensorWorking
+// -------- -------- ---1---- -------- -------- -------- -------- -------- -------- // hasBall
+// -------- -------- ----1111 -------- -------- -------- -------- -------- -------- // ballPos
+// -------- -------- -------- 1------- -------- -------- -------- -------- -------- // genevaWorking
+// -------- -------- -------- -1111111 -------- -------- -------- -------- -------- // genevaState
+// -------- -------- -------- -------- 11111111 111----- -------- -------- -------- // rho
+// -------- -------- -------- -------- -------- ---11111 11111--- -------- -------- // angle
+// -------- -------- -------- -------- -------- -------- -----111 11111111 -------- // theta
+// -------- -------- -------- -------- -------- -------- -------- -------- 1------- // wheelBraking
+// -------- -------- -------- -------- -------- -------- -------- -------- -1111111 // rssi
+
+// -------------------------------------- GETTERS --------------------------------------
+inline PACKET_TYPE RobotFeedback_getHeader(RobotFeedbackPayload *rfp){
+    return (PACKET_TYPE)rfp->payload[0];
 }
 inline uint8_t RobotFeedback_getRobotId(RobotFeedbackPayload *rfp){
     return rfp->payload[1];
 }
 inline bool RobotFeedback_getXsensCalibrated(RobotFeedbackPayload *rfp){
-    return rfp->payload[2] & 0b10000000;
+    return rfp->payload[2] & 0b10000000 ? true : false;
 }
 inline bool RobotFeedback_getBattery(RobotFeedbackPayload *rfp){
-    return rfp->payload[2] & 0b01000000;
+    return rfp->payload[2] & 0b01000000 ? true : false;
 }
 inline bool RobotFeedback_getBallSensorWorking(RobotFeedbackPayload *rfp){
-    return rfp->payload[2] & 0b00100000;
+    return rfp->payload[2] & 0b00100000 ? true : false;
 }
 inline bool RobotFeedback_getHasBall(RobotFeedbackPayload *rfp){
-    return rfp->payload[2] & 0b00010000;
+    return rfp->payload[2] & 0b00010000 ? true : false;
 }
 inline uint8_t RobotFeedback_getBallPos(RobotFeedbackPayload *rfp){
     return rfp->payload[2] & 0b00001111;
 }
 inline bool RobotFeedback_getGenevaWorking(RobotFeedbackPayload *rfp){
-    return rfp->payload[3] & 0b10000000;
+    return rfp->payload[3] & 0b10000000 ? true : false;
 }
 inline uint8_t RobotFeedback_getGenevaState(RobotFeedbackPayload *rfp){
     return rfp->payload[3] & 0b01111111;
@@ -67,18 +86,69 @@ inline int16_t RobotFeedback_getTheta(RobotFeedbackPayload *rfp){
          | ((rfp->payload[7] & 0b11111111) >> 0);
 }
 inline bool RobotFeedback_getWheelBraking(RobotFeedbackPayload *rfp){
-    return rfp->payload[8] & 0b10000000;
+    return rfp->payload[8] & 0b10000000 ? true : false;
 }
 inline uint8_t RobotFeedback_getRSSI(RobotFeedbackPayload *rfp){
     return rfp->payload[8] & 0b01111111;
 }
 
-static inline void fillRobotFeedback(RobotFeedback *feedback, RobotFeedbackPayload *rfp){
+// -------------------------------------- SETTERS --------------------------------------
+// Note: functions assume an empty {0} packet to write to
+inline void RobotFeedback_setHeader(RobotFeedbackPayload *rcp, PACKET_TYPE header){
+    rcp->payload[0] = header;
+}
+inline void RobotFeedback_setId(RobotFeedbackPayload *rcp, uint8_t id){
+    rcp->payload[1] = id;
+}
+inline void RobotFeedback_setXsensCalibrated(RobotFeedbackPayload *rfp, bool XsensCalibrated){
+    rfp->payload[2] |= (XsensCalibrated << 7) & 0b10000000;
+}
+inline void RobotFeedback_setBattery(RobotFeedbackPayload *rfp, bool battery){
+    rfp->payload[2] |= (battery << 6) & 0b01000000;
+}
+inline void RobotFeedback_setBallSensorWorking(RobotFeedbackPayload *rfp, bool ballSensorWorking){
+    rfp->payload[2] |= (ballSensorWorking << 6) & 0b00100000;
+}
+inline void RobotFeedback_setHasBall(RobotFeedbackPayload *rfp, bool hasBall){
+    rfp->payload[2] |= (hasBall << 5) & 0b00010000;
+}
+inline void RobotFeedback_setBallPos(RobotFeedbackPayload *rfp, uint8_t ballPos){
+    rfp->payload[2] |= (ballPos) & 0b00001111;
+}
+inline void RobotFeedback_setGenevaWorking(RobotFeedbackPayload *rfp, bool genevaWorking){
+    rfp->payload[3] |= (genevaWorking << 7) & 0b10000000;
+}
+inline void RobotFeedback_setGenevaState(RobotFeedbackPayload *rfp, uint8_t genevaState){
+    rfp->payload[3] |= (genevaState) & 0b01111111;
+}
+inline void RobotFeedback_setRho(RobotFeedbackPayload *rfp, int16_t rho){
+    rfp->payload[4]  = (rho >> 3) & 0b11111111;
+    rfp->payload[5] |= (rho << 5) & 0b11100000;
+}
+inline void RobotFeedback_setAngle(RobotFeedbackPayload *rfp, int16_t angle){
+    rfp->payload[5] |= (angle >> 5) & 0b00011111;
+    rfp->payload[6] |= (angle << 3) & 0b11111000;
+}
+inline void RobotFeedback_setTheta(RobotFeedbackPayload *rfp, int16_t theta){
+    rfp->payload[6] |= (theta >> 8) & 0b00000111;
+    rfp->payload[7] |= (theta << 0) & 0b11111111;
+}
+inline void RobotFeedback_setWheelBraking(RobotFeedbackPayload *rfp, bool wheelBraking){
+    rfp->payload[8] |= (wheelBraking << 7) & 0b10000000;
+}
+inline void RobotFeedback_setRSSI(RobotFeedbackPayload *rfp, uint8_t rssi){
+    rfp->payload[8] |= (rssi) & 0b01111111;
+}
+
+// -------------------------------------- ENCODE/DECODE --------------------------------------
+static inline void decodeRobotFeedback(RobotFeedback *feedback, RobotFeedbackPayload *rfp){
     feedback->header = RobotFeedback_getHeader(rfp);
     feedback->id = RobotFeedback_getRobotId(rfp);
     feedback->XsensCalibrated = RobotFeedback_getXsensCalibrated(rfp);
     feedback->battery = RobotFeedback_getBattery(rfp);
     feedback->ballSensorWorking = RobotFeedback_getBallSensorWorking(rfp);
+    feedback->hasBall = RobotFeedback_getHasBall(rfp);
+    feedback->ballPos = RobotFeedback_getBallPos(rfp);
     feedback->genevaWorking = RobotFeedback_getGenevaWorking(rfp);
     feedback->genevaState = RobotFeedback_getGenevaState(rfp);
     feedback->rho = RobotFeedback_getRho(rfp);
@@ -88,4 +158,20 @@ static inline void fillRobotFeedback(RobotFeedback *feedback, RobotFeedbackPaylo
     feedback->rssi = RobotFeedback_getRSSI(rfp);
 }
 
+static inline void encodeRobotFeedback(RobotFeedbackPayload *rfp, RobotFeedback *feedback){
+    RobotFeedback_setHeader(rfp, feedback->header);
+    RobotFeedback_setId(rfp, feedback->id);
+    RobotFeedback_setXsensCalibrated(rfp, feedback->XsensCalibrated);
+    RobotFeedback_setBattery(rfp, feedback->battery);
+    RobotFeedback_setBallSensorWorking(rfp, feedback->ballSensorWorking);
+    RobotFeedback_setHasBall(rfp, feedback->hasBall);
+    RobotFeedback_setBallPos(rfp, feedback->ballPos);
+    RobotFeedback_setGenevaWorking(rfp, feedback->genevaWorking);
+    RobotFeedback_setGenevaState(rfp, feedback->genevaState);
+    RobotFeedback_setRho(rfp, feedback->rho);
+    RobotFeedback_setAngle(rfp, feedback->angle);
+    RobotFeedback_setTheta(rfp, feedback->theta);
+    RobotFeedback_setWheelBraking(rfp, feedback->wheelBraking);
+    RobotFeedback_setRSSI(rfp, feedback->rssi);
+}
 #endif /*__ROBOTFEEDBACK_H*/

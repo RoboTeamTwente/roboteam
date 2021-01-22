7008232b1b8d132083072f2c3696eac2db28a0a2
Emiel Steerneman <emielsteerneman@gmail.com>
2021-01-22 01:03:40 +0100

    Removed unused roboData struct. Fixed object passing to pointer passing

--- Core/Inc/packing.h
+++ Core/Inc/packing.h
@@ -40,40 +40,10 @@ typedef struct ReceivedData {
 	bool do_chip;
 } ReceivedData;
 
-/*
- * A data struct which is easy to work with
- * when accessing variables.
- * It needs to be converted before it can be
- * transmitted, though.
- */
-
-									//Description                 Values            Units     	  Represented values    Bits
-typedef struct roboData{
-   uint8_t id:5;					//Robot ID                    [0, 15]           -             [0, 15]                  4
-   uint16_t rho:11;					//Velocity length             [0, 2047]         0.004m/s      [0, 8.188]              11
-   int16_t theta:11;				//Velocity angle              [-1024, 1023]     0.00307rad    [-pi, pi>               11
-   uint8_t driving_reference:1;		//Driving reference           [0, 1]            -             {true, false}            1
-   uint8_t use_cam_info:1;			//Use camera information      [0, 1]            -             {true, false}            1
-   uint8_t use_angle:1;				//Go to the newly set angle	  [0, 1]			-			  {true, false}			   1
-   int16_t velocity_angular:10;		//Reference angular velocity  [-512, 511]       0.098rad/s    [-8*2pi, 8*2pi]         10
-   uint8_t debug_info:1;			//Debug information           [0, 1]            -             {true, false}            1
-   uint8_t do_kick:1;				//Kick                        [0, 1]            -             {true, false}            1
-   uint8_t do_chip:1;				//Chip                        [0, 1]            -             {true, false}            1
-   uint8_t kick_chip_forced:1;		//Kick/chip immediately       [0, 1]            -             {true, false}            1
-   uint8_t kick_chip_power:8;		//Kick/chip power             [0, 255]          0.39%         [0, 100]%                8
-   uint8_t velocity_dribbler:5;		//Reference dribbler speed    [0, 31]          3.125%         [0, 100]%                5
-   uint8_t geneva_drive_state:3;	//Geneva drive state          [0, 7]            -             [-2, 2]                  3
-   //int16_t cam_position_x:13;		//x position robot (camera)   [-4096, 4095]     0.0025m       [-10.24, 10.23]         13
-   //int16_t cam_position_y:13;		//y position robot (camera)   [-4096, 4095]     0.0025m       [-10.24, 10.23]         13
-   int16_t cam_rotation:11;			//Orientation (camera)        [-1024, 1023]     0.00307rad    [-pi, pi]               11
-} roboData;
-
-
 //for debugging
 float uint32tofloat(uint32_t raw);
-void printRoboData(roboData *input, uint8_t dataArray[ROBOPKTLEN]);
 
-void packetToRoboData(RobotCommandPayload input, ReceivedData* receivedData);
+void packetToRoboData(RobotCommandPayload* input, ReceivedData* receivedData);
 
 #endif /* PACKING_H_ */
 
--- Core/Src/packing.c
+++ Core/Src/packing.c
@@ -23,35 +23,18 @@
 
 #include "RobotCommand.h"
 #include "RobotFeedback.h"
-
-void printRoboData(roboData *input, uint8_t dataArray[ROBOPKTLEN]) {
-	Putty_printf("----->FROM BASESTATION----->\n\r");
-	for(int i=0; i<ROBOPKTLEN; i++) {
-		Putty_printf("%02x ", dataArray[i]);
-	}
-	Putty_printf("\n\r");
-
-	Putty_printf("\tRoboID: %i \n\r", input->id);
-	Putty_printf("\tDebug info: %i \n\r", input->debug_info);
-	Putty_printf("\tRho: %i \n\r\tTheta: %i \n\r", input->rho, input->theta);
-	Putty_printf("\tKICKCHIP\n\r");
-	Putty_printf("\tKick %i \n\r\t Chip: %i \n\r\t Forced: %i \n\r\t Power: %i \n\r", input->do_kick, input->do_chip ,input->kick_chip_forced, input->kick_chip_power);
-	Putty_printf("\tDribbler velocity: %i \n\r", input->velocity_dribbler);
-	Putty_printf("\tGeneva drive: %i \n\r", input->geneva_drive_state);
-	Putty_printf("\tDriving reference: %i \n\r", input->driving_reference);
-	Putty_printf("\tAngular velocity: %i \n\r", input->velocity_angular);
-	Putty_printf("\tCAMERA \n\r\t use cam info: %i \n\r\t rotation: %i \n\r\n\r", input->use_cam_info, input->cam_rotation);
-}
-
+#include "peripheral_util.h"
 
 /*
  * Create a roboData structure from a given Bytearray.
  * This is used by the robot to convert a received nRF packet into a struct with named variables.
  */
-void packetToRoboData(RobotCommandPayload input, ReceivedData* receivedData) {
-	RobotCommand rc = {0};
 
-	decodeRobotCommand(&rc, &input);
+void packetToRoboData(RobotCommandPayload* input, ReceivedData* receivedData) {
+	RobotCommand rc = {0};
+	
+
+	decodeRobotCommand(&rc, input);
 
 	/*
 	 * Convert data to useful units

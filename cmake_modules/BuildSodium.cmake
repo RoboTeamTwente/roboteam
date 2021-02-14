fb8114f1a1c2857f6cd27ca908a05d92a5b9d5ab
rolf <rolfvdhulst@gmail.com>
2021-02-14 14:26:51 +0100

    fixed libsodium problems

--- cmake_modules/BuildSodium.cmake
+++ cmake_modules/BuildSodium.cmake
@@ -1,23 +1,19 @@
 include(ExternalProject)
-set(sodium_src_dir   ${CMAKE_CURRENT_BINARY_DIR}/libsodium)
+set(sodium_src_dir   ${CMAKE_CURRENT_SOURCE_DIR}/libsodium)
 set(sodium_build_dir ${CMAKE_CURRENT_BINARY_DIR}/libsodium)
 set(LIBSODIUM_LIB_DIR     ${sodium_build_dir}/lib)
 set(LIBSODIUM_INCLUDE_DIR ${sodium_src_dir}/src/libsodium/include)
 
 ExternalProject_Add(project_libsodium
-        GIT_REPOSITORY https://github.com/jedisct1/libsodium.git
-        GIT_TAG a5e2122177fa88f09b72047030f3a23f5b651c49
-        PREFIX     ${CMAKE_CURRENT_BINARY_DIR}/libsodium
-        CONFIGURE_COMMAND echo "test echo?!?!??!?!?"
-        BUILD_COMMAND
-        ${CMAKE_CURRENT_BINARY_DIR}/libsodium/autogen.sh
-        ${CMAKE_CURRENT_BINARY_DIR}/libsodium/configure
+        URL https://github.com/jedisct1/libsodium/archive/1.0.18.tar.gz
+        PREFIX     ${sodium_build_dir}
+        SOURCE_DIR ${sodium_src_dir}
+        BINARY_DIR ${sodium_src_dir}
+        CONFIGURE_COMMAND ${sodium_src_dir}/autogen.sh -S
+        BUILD_COMMAND ${sodium_src_dir}/configure --prefix=${sodium_build_dir}
         INSTALL_COMMAND make -j8 install
         BUILD_BYPRODUCTS ${LIBSODIUM_LIB_DIR}/libsodium.a
         )
 add_library(lib::sodium STATIC IMPORTED)
 add_dependencies(lib::sodium project_libsodium)
-#set_target_properties(lib::sodium PROPERTIES IMPORTED_LOCATION ${CMAKE_CURRENT_BINARY_DIR}/libsodium/lib/libsodium.a)
-#include_directories(${CMAKE_CURRENT_BINARY_DIR}/libzmq/include)
-#target_include_directories(lib::zmq
-#        INTERFACE ${CMAKE_CURRENT_BINARY_DIR})
+set_target_properties(lib::sodium PROPERTIES IMPORTED_LOCATION ${CMAKE_CURRENT_BINARY_DIR}/libsodium/lib/libsodium.a)

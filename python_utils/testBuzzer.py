fb1d5e63e97c6c91e4a9fc2fe6cad9c5daedd982
Emiel Steerneman <emielsteerneman@gmail.com>
2021-06-06 17:13:19 +0200

    cleanup

--- python_utils/testBuzzer.py
+++ python_utils/testBuzzer.py
@@ -1,6 +1,5 @@
 import time
 import math
-from inspect import getmembers
 import utils
 import serial
 import numpy as np
@@ -9,6 +8,7 @@ import argparse
 import shutil
 import multiprocessing
 import getch
+import sys
 
 try:
 	from rem import rem
@@ -18,70 +18,59 @@ except ImportError:
 	print("[Error] $ python generate.py --includes ../include/*  --name rem --output ../../rem")
 	exit()
 
-def printPacket(rc):
-	maxLength = max([len(k) for k, v in getmembers(rc)])
-	title = re.findall(r"_(\w+) ", str(rc))[0]
-	
-	lines = [("┌─ %s "%title) + ("─"*100)[:maxLength*2+2-len(title)] + "┐" ]	
-	lines += [ "│ %s : %s │" % ( k.rjust(maxLength) , str(v).ljust(maxLength) ) for k, v in getmembers(rc) ]
-	lines += [ "└" + ("─"*(maxLength*2+5)) + "┘"]
-	print("\n".join(lines))
-
-def drawProgressBar(progress):
-	cols = min(40, shutil.get_terminal_size((80, 20)).columns)
-	filled = int(cols*progress)
-	string = "["
-	string += "*" * filled
-	string += " " * (cols - filled)
-	string += "]"
-	return string
-
 basestation = None
-cmdPayload = rem.ffi.new("RobotCommandPayload*")
+buzzPacket = rem.ffi.new("RobotBuzzer*")
 buzzPayload = rem.ffi.new("RobotBuzzerPayload*")
 
 lastWritten = time.time()
-tickCounter = 0
-periodLength = 100
 packetHz = 60
 
-totalCommandsSent = 0
-totalFeedbackReceived = 0
-robotConnected = True
-
-lastBasestationLog = ""
-
-counter = 0
-
-
-
-
-
-    # {NF3,0,NG3,0,NF3,0,NF3,NA3,0,NF3,0,ND3,0,NF3,0,NC4,0,NF3,0,NF3,NC4,0,NC4,0,NG3,0,NF3,0,NC4,0,NF4,0,NF3,ND3,0,ND3,NC3,0,NG3,0,NF3},
 notes = {
-	"d" : 294,
-	"f" : 349,
-	"g" : 392,
-	"c" : 262,
-	"a" : 440,
-	"5" : 587,
-	"3" : 220,
-	"e" : 330,
-	"s" : 466
+	"q" : 220, # A3
+	"a" : 233, # AS3
+	"z" : 247, # B3
+	"w" : 262, # C4
+	"s" : 277, # CS4
+	"x" : 294, # D4
+	"e" : 311, # DS4
+	"d" : 330, # E4
+	"c" : 349, # F4
+	"r" : 370, # FS4
+	"f" : 392, # G4
+	"v" : 415, # GS4
+	"t" : 440, # A4
+	"g" : 466, # AS4
+	"b" : 494, # B4
+	"y" : 523, # C5
+	"h" : 554, # CS5
+	"n" : 587, # D5
+	"u" : 622, # DS5
+	"j" : 659, # E5
+	"m" : 698, # F5
+	"i" : 740, # FS5
+	"k" : 784, # G5
+	"o" : 831, # GS5
+	"l" : 880, # A5
+	"p" : 932, # AS5
 }
 
 
+try:
+	if len(sys.argv) != 2:
+		raise Exception("Error : Invalid number of arguments. Expected id")
+	
+	robotId = int(sys.argv[1])
+	if robotId < 0 or 15 < robotId:
+		raise Exception("Error : Invalid robot id %d. Robot id should be between 0 and 15" % robotId)
+except Exception as e:
+	print(e)
+	print("Error : Run script with \"python testBuzzer.py id\"")
+	exit()
 
-# notes = {
-# 	"c" : 523,
-# 	"d" : 587,
-# 	"e" : 659,
-# 	"f" : 698,
-# 	"g" : 784,
-# 	"a" : 880,
-# 	"b" : 988,
-# 	"x" : 1047
-# }
+
+buzzPacket.header = rem.lib.PACKET_TYPE_ROBOT_BUZZER
+buzzPacket.id = robotId
+
 
 def read_input(note):
 	while True:
@@ -90,10 +79,8 @@ def read_input(note):
 			note.value = notes[char]
 
 note = multiprocessing.Value('i', 0)
-
 inputThread = multiprocessing.Process(target=read_input, args=(note,))
 inputThread.start()
-
 
 
 while True:
@@ -109,41 +96,12 @@ while True:
 			if 1./packetHz <= time.time() - lastWritten:
 				lastWritten = time.time()
 
-				cmd = rem.ffi.new("RobotCommand*")
-				cmd.header = rem.lib.PACKET_TYPE_ROBOT_COMMAND
-				cmd.id = 3
-				rem.lib.encodeRobotCommand(cmdPayload, cmd)
-				basestation.write(cmdPayload.payload)
-
 				if note.value != 0:
-					print("note", note.value)
-					cmd = rem.ffi.new("RobotBuzzer*")
-					cmd.header = rem.lib.PACKET_TYPE_ROBOT_BUZZER
-					cmd.id = 3
-					cmd.period = note.value
-					cmd.duration = 1
-					rem.lib.encodeRobotBuzzer(buzzPayload, cmd)
+					buzzPacket.period = note.value
+					buzzPacket.duration = 1
+					rem.lib.encodeRobotBuzzer(buzzPayload, buzzPacket)
 					basestation.write(buzzPayload.payload)
 					note.value = 0
-				# if counter % 10 == 0:
-				# 	for i in cmdPayload.payload:
-				# 		print(i, end=" ")
-				# 	print()
-				# for i in buzzPayload.payload:
-				# 	print(i, end=" ")
-				# print()
-
-
-				# if counter % 10 == 0:
-				# 	for i in cmdPayload.payload:
-				# 		print(hex(i), end=" ")
-				# 	print()
-				# for i in buzzPayload.payload:
-				# 	print(hex(i), end=" ")
-				# print()
-
-
-				counter += 1
 
 			# Read feedback packets coming from the robot
 			packet_type = basestation.read(1)
@@ -151,12 +109,6 @@ while True:
 				continue
 
 			packetType = packet_type[0]
-
-			# if packetType == rem.lib.PACKET_TYPE_ROBOT_FEEDBACK:
-			# 	print("Feedback")
-
-			# if packetType == rem.lib.PACKET_TYPE_BASESTATION_STATISTICS:
-			# 	print("Statistics")
 
 			if packetType == rem.lib.PACKET_TYPE_BASESTATION_LOG:
 				msg = basestation.readline().decode()
@@ -171,5 +123,3 @@ while True:
 		print("[Error] KeyError", e, "{0:b}".format(int(str(e))))
 	except Exception as e:
 		print("[Error]", e)
-		# Reset the connection to the basestation
-		# ser = None

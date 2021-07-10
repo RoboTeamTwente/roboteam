1ae0d43388bfa0cecfa4f12e7d220bb32edd8d53
rolfvdhulst <rolfvdhulst@gmail.com>
2021-07-10 13:15:24 +0200

    add documentation

--- observer/include/observer/filters/vision/GeometryFilter.h
+++ observer/include/observer/filters/vision/GeometryFilter.h
@@ -7,10 +7,24 @@
 
 
 #include <roboteam_proto/messages_robocup_ssl_geometry.pb.h>
+/**
+ * @brief A class which filters the geometry from multiple camera's/sources and merges it to a single geometry correctly.
+ */
 class GeometryFilter {
 public:
+    /**
+     * Updates the filter, replacing the field geometry data and adding any camera's to the list of known camera calibrations
+     * @param geometryData data to update the filter with
+     * @return true if the geometry changed/was updated, false if this geometry was already known
+     */
     bool process(const proto::SSL_GeometryData& geometryData);
+    /**
+     * @return true if a geometry message was received, false otherwise
+     */
     bool receivedFirstGeometry() const;
+    /**
+     * @return The most recent filtered geometry
+     */
     proto::SSL_GeometryData getGeometry() const;
 private:
     std::string lastGeometryString;
--- observer/include/observer/filters/vision/robot/RobotOrientationFilter.h
+++ observer/include/observer/filters/vision/robot/RobotOrientationFilter.h
@@ -6,18 +6,37 @@
 #define RTT_ROBOTEAM_WORLD_OBSERVER_SRC_FILTERS_VISION_ROBOT_ROBOTORIENTATIONFILTER_H_
 
 #include "observer/filters/vision/PosVelFilter1D.h"
-//Note the getState() function returns invalid results for this class!
+
+/**
+ * @brief A class which tracks robot angle; this is a 1 dimensional posvelFilter which applies mod 2pi, essentially
+ */
 class RobotOrientationFilter : public PosVelFilter1D {
  public:
-  RobotOrientationFilter() :PosVelFilter1D(){}
+  RobotOrientationFilter() : PosVelFilter1D(){}
   RobotOrientationFilter(const Eigen::Vector2d &initialState,
                          const Eigen::Matrix2d &initialCovariance,
                          double modelError,
                          double measurementError,
                          const Time &timeStamp);
+  /**
+   * @param kalman update, making sure the angle is safe
+   */
   void update(const double& position) override;
+  /**
+   * @return get's the current robot angle
+   */
   [[nodiscard]] double getPosition() const override;
+  /**
+   * @brief Gets the angle estimate at the given time
+   * @param time
+   * @return the estimate of the angle at the given timm
+   */
   [[nodiscard]] double getPositionEstimate(const Time &time) const override;
+  /**
+   * @brief projects the angle from any value x to the range [-pi,pi)
+   * @param angle
+   * @return the limited angle
+   */
   [[nodiscard]] static double limitAngle(double angle);
 };
 

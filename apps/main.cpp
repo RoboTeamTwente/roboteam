f0a7c339f4e0ea06182812739bcec3be649e9d09
Dawid Kulikowski <git@dawidkulikowski.pl>
2021-08-18 09:45:22 +0200

    Move shared interface components to roboteam_interface and test json declaration parsing

--- apps/main.cpp
+++ apps/main.cpp
@@ -2,8 +2,21 @@
 // Created by Dawid Kulikowski on 05/08/2021.
 //
 
-#import <iostream>
+#include "roboteam_interface/InterfaceDeclarations.h"
+#include "nlohmann/json.hpp"
+
+#include <fstream>
+#include <iostream>
+
 
 int main() {
+
+    std::ifstream jsonFile("interface_declarations.json");
+    nlohmann::json decls;
+    jsonFile >> decls;
+
+    rtt::Interface::InterfaceDeclarations ifaceDecls(decls);
+
+    std::cout << ifaceDecls.getDeclarations()[0].path << std::endl;
 
 }
---
+++ include/roboteam_interface/InterfaceDeclarations.h
@@ -0,0 +1,47 @@
+//
+// Created by Dawid Kulikowski on 09/08/2021.
+//
+
+#ifndef RTT_INTERFACEDECLARATIONS_H
+#define RTT_INTERFACEDECLARATIONS_H
+
+#include <roboteam_proto/UiOptions.pb.h>
+
+#include <nlohmann/json.hpp>
+
+#include "InterfaceDeclaration.h"
+#include "InterfaceStateHandler.h"
+
+namespace rtt::Interface {
+class InterfaceDeclarations {
+   private:
+       std::vector<InterfaceDeclaration> decls = {};
+
+       mutable std::mutex mtx;
+
+       std::weak_ptr<InterfaceStateHandler> stateHandler;
+
+       void _unsafeAddDeclaration(InterfaceDeclaration);
+       void _unsafeRemoveDeclaration(std::string);
+
+       std::optional<InterfaceDeclaration> _unsafeGetDeclaration(std::string) const;
+
+   public:
+       InterfaceDeclarations(const nlohmann::json&);
+       InterfaceDeclarations(std::weak_ptr<InterfaceStateHandler> sts): stateHandler(sts) {}
+
+       explicit InterfaceDeclarations(const std::vector<InterfaceDeclaration> vec, std::weak_ptr<InterfaceStateHandler> sts) : decls(vec), stateHandler(sts) {};
+
+       void addDeclaration(InterfaceDeclaration);
+       void removeDeclaration(std::string);
+
+       std::vector<InterfaceDeclaration> getDeclarations() const;
+       std::optional<InterfaceDeclaration> getDeclaration(std::string) const;
+
+       void handleData(const proto::UiOptionDeclarations&);
+
+       proto::UiOptionDeclarations toProto() const;
+};
+
+}
+#endif  // RTT_INTERFACEDECLARATIONS_H

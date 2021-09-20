f86090ba458e1f6211b034239c32fe259afc2b36
Emiel Steerneman <emielsteerneman@gmail.com>
2021-09-20 17:03:28 +0200

    Updated to latest REM version; wip;

--- Core/Inc/logging.h
+++ Core/Inc/logging.h
@@ -1,6 +1,7 @@
 #ifndef LOGGING_H_
 #define LOGGING_H_
 
+void LOG_printf(char *format, ...);
 void LOG(char *message);
 
 #endif /* LOGGING_H_ */
--- Core/Src/logging.c
+++ Core/Src/logging.c
@@ -1,12 +1,16 @@
 #include "logging.h"
 #include "BaseTypes.h"
+#include "RobotLog.h"
 #include "peripheral_util.h"
+#include "robot.h"
 
 #include <stdarg.h>  // LOG_printf formatting in putty_printf
 
 // Static to make it private to this file
 // Buffer used by vsprintf
 static char printf_buffer[1024]; 
+static char log_bugger[1024];
+static RobotLogPayload robotLogPayload;
 
 void LOG_printf(char *format, ...)
 {
@@ -21,26 +25,33 @@ void LOG_printf(char *format, ...)
 
 
 /**
+ * TODO Ensure that messages are not "too" long, whatever that may be.
+ * 
  * @brief Sends a log message over USB using the PACKET_TYPE_ROBOT_LOG header.
  * The log must always end with \n, which this function enforces.
  * 
  * @param message The message to send over the USB
  */
 void LOG(char *message){
-    // Add +1 to the length of the string to account for the extra header bytes
-    int length = strlen(message) + 1;
-    // Free up space for header + message
-    uint8_t* buffer = malloc(length);
-    // Set the header
-    buffer[0] = PACKET_TYPE_ROBOT_LOG;
-    // Enforce newline
-    buffer[length] = '\n';
-    // Copy the message into the buffer, next to the header
-    memcpy(buffer+1, message, length-1);
+    
+    // Get message length
+    uint32_t message_length = strlen(message);  
+    // Ensure newline at the end of the message (Can be removed if all software everywhere properly used the RobotLog_message_length field)
+    message[message_length-1] = '\n';
+
+    RobotLog_set_header(&robotLogPayload, PACKET_TYPE_ROBOT_LOG); // 8 bits
+    RobotLog_set_remVersion(&robotLogPayload, LOCAL_REM_VERSION); // 4 bits
+    RobotLog_set_id(&robotLogPayload, ROBOT_ID);                  // 4 bits
+    RobotLog_set_message_length(&robotLogPayload, message_length);// 8 bits
+                                                                  // = 3 bytes
+    uint8_t robotlog_length = 3; // TODO Replace '3' with something dynamic, so that its always correct even if the RobotLog packet changes                                                                  
+    
+    // Copy the RobotLog packet into the buffer
+    memcpy(log_bugger, &robotLogPayload, robotlog_length);
+    // Copy the message into the buffer, next to the RobotLog packet
+    memcpy(log_bugger + robotlog_length, message, message_length);
     // Wait until the UART interface is free to use
     while(UART_PC->gState != HAL_UART_STATE_READY);
     // Transmit the data to the computer
-    HAL_UART_Transmit_DMA(UART_PC, buffer, length);
-    // Free up the memory
-    free(buffer);
+    HAL_UART_Transmit_DMA(UART_PC, log_bugger, robotlog_length + message_length);
 }

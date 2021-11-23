b57117821064161125959f12d8aa7df61a679ed7
Floris Heinen <Floris.heinen1@gmail.com>
2021-11-23 10:46:54 +0100

    Copied over files from utils/networking

---
+++ include/utils/Channel.hpp
@@ -0,0 +1,30 @@
+#ifndef RTT_CHANNEL_H
+#define RTT_CHANNEL_H
+
+#include <string>
+
+namespace proto {
+
+struct Channel {
+    std::string name;
+    std::string ip;
+    std::string port;
+
+    Channel() = default;
+    Channel(std::string name, std::string ip, std::string port);
+    Channel(const Channel& other);
+    Channel& operator=(Channel const&) = default;
+
+    std::string getAddress(const std::string& _ip, const std::string& _port);
+    std::string getSubscribeAddress();
+    std::string getPublishAddress();
+
+    bool operator==(const Channel& other);
+    bool operator!=(const Channel& other);
+
+    std::string toInfoString(bool isPublisher);
+};
+
+}  // namespace proto
+
+#endif  // RTT_CHANNEL_H

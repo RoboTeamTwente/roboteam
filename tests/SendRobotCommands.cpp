47cf5a432932838b4a2bb3505996436f8d85e787
Floris Heinen <Floris.heinen1@gmail.com>
2022-01-28 21:02:04 +0100

    Make commands test more general
    This test offers more functionality, like sending a single robot command to robothub with a specific robot ID, or a command to all robots. And it simultaneously can send a command to both blue and yellow team

--- tests/SendRobotCommands.cpp
+++ tests/SendRobotCommands.cpp
@@ -9,19 +9,14 @@
 
 using namespace rtt::net;
 
-void sendRobotHubBasestationMode(std::unique_ptr<SettingsPublisher>& pub) {
-    proto::Setting settings;
+constexpr int ID = 0;
+constexpr int MAX_AMOUNT_OF_ROBOTS = 16;
 
-    settings.set_serialmode(true);
-
-    pub->publish(settings);
-}
-
-void sendEmptyRobotCommand(std::unique_ptr<RobotCommandsBluePublisher>& pub) {
+proto::AICommand getEmptyRobotCommand(int id) {
     proto::AICommand commands;
 
     auto* command = commands.add_commands();
-    command->set_id(0);
+    command->set_id(id);
     command->mutable_vel()->set_x(0);
     command->mutable_vel()->set_y(0);
     command->set_w(0);
@@ -32,7 +27,35 @@ void sendEmptyRobotCommand(std::unique_ptr<RobotCommandsBluePublisher>& pub) {
     command->set_chip_kick_forced(false);
     command->set_chip_kick_vel(false);
 
-    pub->publish(commands);
+    return commands;
+}
+
+proto::AICommand getEmptyRobotCommandToAllRobots() {
+    proto::AICommand commands;
+
+    for (int i = 0; i < 16; ++i) {
+        auto* command = commands.add_commands();
+        command->set_id(i);
+        command->mutable_vel()->set_x(0);
+        command->mutable_vel()->set_y(0);
+        command->set_w(0);
+        command->set_use_angle(false);
+        command->set_dribbler(false);
+        command->set_kicker(false);
+        command->set_chipper(false);
+        command->set_chip_kick_forced(false);
+        command->set_chip_kick_vel(false);
+    }
+
+    return commands;
+}
+
+proto::Setting getRobotHubBasestationModePacket() {
+    proto::Setting settings;
+
+    settings.set_serialmode(true);
+
+    return settings;
 }
 
 void onFeedback(const proto::RobotData& robotFeedback) {
@@ -55,37 +78,37 @@ void onFeedback(const proto::RobotData& robotFeedback) {
 void onWorld(const proto::State& world) {
 
     for (const auto& robot : world.last_seen_world().yellowfeedback()) {
-        std::cout << "Robot " << robot.id() << " of team yellow has " << (robot.hasball() ? "" : "not ") << "the ball" << std::endl;
+        //std::cout << "Robot " << robot.id() << " of team yellow has " << (robot.hasball() ? "" : "not ") << "the ball" << std::endl;
     }
 
     for (const auto& robot : world.last_seen_world().bluefeedback()) {
-        std::cout << "Robot " << robot.id() << " of team blue has " << (robot.hasball() ? "" : "not ") << "the ball" << std::endl;
+        //std::cout << "Robot " << robot.id() << " of team blue has " << (robot.hasball() ? "" : "not ") << "the ball" << std::endl;
     }
 }
 
 int main() {
-
-    std::unique_ptr<SettingsPublisher> settingsPub = std::make_unique<SettingsPublisher>();
-    std::unique_ptr<RobotCommandsBluePublisher> commandsPub = std::make_unique<RobotCommandsBluePublisher>();
+    auto settingsPub = std::make_unique<SettingsPublisher>();
+    auto commandsBluePub = std::make_unique<RobotCommandsBluePublisher>();
+    auto commandsYellowPub = std::make_unique<RobotCommandsYellowPublisher>();
     
-    auto callback = [](const proto::RobotData& data){
-        onFeedback(data);
-    };
+    auto feedbackSub = std::make_unique<RobotFeedbackSubscriber>(onFeedback);
+    auto worldSub = std::make_unique<WorldSubscriber>(onWorld);
 
-    std::unique_ptr<RobotFeedbackSubscriber> feedbackSub = std::make_unique<RobotFeedbackSubscriber>(callback);
-    std::unique_ptr<WorldSubscriber> worldSub = std::make_unique<WorldSubscriber>(onWorld);
+    auto singleCommand = getEmptyRobotCommand(0);
+    auto teamCommand = getEmptyRobotCommandToAllRobots();
 
     // First, send settings message to robothub that it needs to forward messages to the basestation
+    std::cout << "Started test..." << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(2));
-    
     std::cout << "Sending robothub mode settings message" << std::endl;
-    sendRobotHubBasestationMode(settingsPub);
-
+    settingsPub->publish(getRobotHubBasestationModePacket());
     std::this_thread::sleep_for(std::chrono::seconds(2));
 
-
+    std::cout << "Starting to send commands: " << std::endl << std::endl;
+    auto command = getEmptyRobotCommandToAllRobots();
     while (true) {
-        sendEmptyRobotCommand(commandsPub);
+        commandsYellowPub->publish(command);
+        commandsBluePub->publish(command);
         std::cout << "Sent robot command..." << std::endl;
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }

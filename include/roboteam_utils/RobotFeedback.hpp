ac10adb62a0a29945c2aed9ea0307f67a9744f4a
Floris Heinen <Floris.heinen1@gmail.com>
2022-03-10 14:19:45 +0100

    Add shared RobotFeedback data type

---
+++ include/roboteam_utils/RobotFeedback.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <vector>
+
+#include <roboteam_utils/Vector2.h>
+#include <roboteam_utils/Angle.h>
+#include <roboteam_utils/Teams.hpp>
+
+namespace rtt {
+
+/* The sources that can provide robot feedback */
+enum class RobotFeedbackSource {
+    SIMULATOR, BASESTATION
+};
+
+/* This type represents the feedback that a robot can give */
+typedef struct RobotFeedback {
+    int id;                     // ID of the robot
+    bool hasBall;               // The robot has the ball
+    float ballPosition;         // [-0.5, 0.5] Position of the ball relative to the sensor
+    bool ballSensorIsWorking;   // The ball sensor is working
+    Vector2 velocity;           // Estimated velocity according to the robot
+    Angle angle;                // Estimated angle according to the robot
+    bool xSensIsCalibrated;     // The xSens is calibrated
+    bool capacitorIsCharged;    // The capacitor is charged
+    int wheelLocked;            // Indicates if a wheel is locked. One bit per wheel
+    int wheelBraking;           // Indicates if a wheel is slipping. One bit per wheel
+    float batteryLevel;         // Battery level // TODO: Decide if this is in volts or percentage
+    int signalStrength;         // The signal strength of the robot
+} RobotFeedback;
+
+/* This is the data object that is published by RobotHub */
+typedef struct RobotsFeedback {
+    Team team;
+    RobotFeedbackSource source;
+    std::vector<RobotFeedback> feedback;
+} RobotsFeedback;
+
+} // namespace rtt
---
+++ include/roboteam_utils/Teams.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace rtt {
+
+// The colors of the two teams. Yellow is our default
+enum class Team {
+    YELLOW, BLUE
+};
+
+// Teams expressed relatively.
+enum class RelativeTeam {
+    US, THEM
+};
+
+} // namespace rtt

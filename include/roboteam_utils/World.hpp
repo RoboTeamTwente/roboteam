b8cbc528ba3c14a9406b413f7116dd475982a889
Floris Heinen <Floris.heinen1@gmail.com>
2022-04-04 00:11:51 +0200

    Fix default values, add comments

--- include/roboteam_utils/World.hpp
+++ include/roboteam_utils/World.hpp
@@ -13,20 +13,20 @@ namespace rtt {
 
 /* This structure contains the locations of all moving elements from a certain time stamp. */
 typedef struct World {
-    unsigned long timePoint;
-    unsigned int id;
-    std::optional<Ball> ball;
-    std::vector<Robot> yellowRobots;
-    std::vector<Robot> blueRobots;
+    unsigned long timePoint = 0;            // The timestamp of the world
+    unsigned int id = 0;                    // The ID of the world
+    std::optional<Ball> ball = std::nullopt;// The ball
+    std::vector<Robot> yellowRobots;        // All yellow robots
+    std::vector<Robot> blueRobots;          // All blue robots
 } World;
 
 /* This represents the complete world (SoonTM also past and guestimated future) */
 typedef struct WorldStates {
-    World currentWorld;
-    // World futureWorld;  // TODO: Implement this
+    World currentWorld;         // The current world
+    // World futureWorld;       // TODO: Implement this
 
-    rtt::Field field;
-    proto::SSL_Referee referee;
+    rtt::Field field;           // The field of the world
+    proto::SSL_Referee referee; // The latest referee command
 } WorldStates;
 
 } // namespace rtt

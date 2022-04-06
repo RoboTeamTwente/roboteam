cace0e9e1e67fa5dedb759a0dc87382f74c1b2c0
Floris Heinen <Floris.heinen1@gmail.com>
2022-04-06 17:19:14 +0200

    Fix channel of client controller

--- libs/interface/include/InterfaceControllerClient.h
+++ libs/interface/include/InterfaceControllerClient.h
@@ -15,7 +15,7 @@ namespace rtt::Interface {
     class InterfaceControllerClient: public InterfaceController<proto::UiValues, proto::ModuleState> {
     public:
         std::weak_ptr<InterfaceFieldStateStore> getFieldState() const;
-        InterfaceControllerClient(): InterfaceController<proto::UiValues, proto::ModuleState>(rtt::net::utils::ChannelType::INTERFACE_TO_AI_CHANNEL, rtt::net::utils::ChannelType::INTERFACE_TO_AI_CHANNEL, 20, 20), fieldState(std::make_shared<InterfaceFieldStateStore>()) {}
+        InterfaceControllerClient(): InterfaceController<proto::UiValues, proto::ModuleState>(rtt::net::utils::ChannelType::INTERFACE_TO_AI_CHANNEL, rtt::net::utils::ChannelType::AI_TO_INTERFACE_CHANNEL, 20, 20), fieldState(std::make_shared<InterfaceFieldStateStore>()) {}
     private:
         std::shared_ptr<InterfaceFieldStateStore> fieldState;
         proto::UiValues getDataForRemote(bool) const noexcept override;

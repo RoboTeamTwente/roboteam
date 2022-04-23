8cec900247dabc27a535b54043adddcba127761e
RoboPC <robopc@roboteamtwente.nl>
2022-04-23 22:07:42 +0200

    Update docker image

---
+++ docker/clone.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git clone https://github.com/RoboTeamTwente/roboteam_suite /home/default/roboteam_suite
+git clone https://github.com/RoboTeamTwente/roboteam_ai /home/default/roboteam_suite/roboteam_ai
+git clone https://github.com/RoboTeamTwente/roboteam_world /home/default/roboteam_suite/roboteam_world
+git clone https://github.com/RoboTeamTwente/roboteam_robothub /home/default/roboteam_suite/roboteam_robothub
+git clone https://github.com/RoboTeamTwente/roboteam_networking /home/default/roboteam_suite/roboteam_networking
+git clone https://github.com/RoboTeamTwente/roboteam_utils /home/default/roboteam_suite/roboteam_utils
+git clone https://github.com/RoboTeamTwente/roboteam_embedded_messages /home/default/roboteam_suite/roboteam_robothub/roboteam_embedded_messages
+git clone https://github.com/RoboTeamTwente/roboteam_interface /home/default/roboteam_suite/roboteam_interface
--- docker/tests/roboteam_world.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Cmake build
-cmake --build . --target world_tests -- -j $(nproc)
-# Run tests, xvfb-run simulates an xserver
-cd roboteam_world
-xvfb-run ./world_tests

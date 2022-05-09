3e5c899252cded8a07afb025e36cdfa2c2cd58a3
Emiel Steerneman <emielsteerneman@gmail.com>
2022-05-09 19:45:21 +0200

    added remversion and messagelength to basestation log; added remversion to basestation_get_configuration

--- generator/packets.py
+++ generator/packets.py
@@ -2,97 +2,97 @@ import math
 
 packets = {
     "REM_RobotCommand" : [
-        ["header",             8,  None, "Header byte indicating the type of packet"],
-        ["remVersion",         4,  None, "Version of roboteam_embedded_messages"],
-        ["id",                 4,  None, "Id of the robot"],
-        ["messageId",          4,  None, "Id of the message"],
-        ["doKick",             1,  None, "Do a kick if ballsensor"],
-        ["doChip",             1,  None, "Do a chip if ballsensor"],
-        ["doForce",            1,  None, "Do regardless of ballsensor"],
-        ["useCameraAngle",     1,  None, "Use the info in 'cameraAngle'"],
+        ["header",              8,  None, "Header byte indicating the type of packet"],
+        ["remVersion",          4,  None, "Version of roboteam_embedded_messages"],
+        ["id",                  4,  None, "Id of the robot"],
+        ["messageId",           4,  None, "Id of the message"],
+        ["doKick",              1,  None, "Do a kick if ballsensor"],
+        ["doChip",              1,  None, "Do a chip if ballsensor"],
+        ["doForce",             1,  None, "Do regardless of ballsensor"],
+        ["useCameraAngle",      1,  None, "Use the info in 'cameraAngle'"],
         ["rho",                16, [0, 8], "Magnitude of movement (m/s)"],
         ["theta",              16, [-math.pi, math.pi], "Direction of movement (radians)"],
         ["angle",              16, [-math.pi, math.pi], "Absolute angle (rad) / angular velocity (rad/s)"],
         ["cameraAngle",        16, [-math.pi, math.pi], "Angle of the robot as seen by camera (rad)"],
-        ["dribbler",           3,  [0, 1], "Dribbler speed"],
-        ["kickChipPower",      3,  [0, 6.5], "Speed of the ball in m/s"],
-        ["angularControl",     1,  None, "NOT IMPLEMENTED IN ROBOT YET. 0 = angular velocity, 1 = absolute angle"],
-        ["feedback",           1,  None, "Ignore the packet. Just send feedback"],
+        ["dribbler",            3,  [0, 1], "Dribbler speed"],
+        ["kickChipPower",       3,  [0, 6.5], "Speed of the ball in m/s"],
+        ["angularControl",      1,  None, "NOT IMPLEMENTED IN ROBOT YET. 0 = angular velocity, 1 = absolute angle"],
+        ["feedback",            1,  None, "Ignore the packet. Just send feedback"],
     ],
     "REM_RobotFeedback" : [
-        ["header",             8,  None, "Header byte indicating the type of packet"],
-        ["remVersion",         4,  None, "Version of roboteam_embedded_messages"],
-        ["id",                 4,  None, "Id of the robot "],
-        ["messageId",          4,  None, "Id of the message"],
-        ["batteryLevel",       4,  None, "The voltage level of the battery"],
-        ["XsensCalibrated",    1,  None, "Indicates if the XSens IMU is calibrated"],
-        ["ballSensorWorking",  1,  None, "Indicates if the ballsensor is working"],
-        ["hasBall",            1,  None, "Indicates if the ball is somewhere in front of the ballsensor"],
-        ["capacitorCharged",   1,  None, "Indicates if the capacitor for kicking and chipping is charged"],
-        ["ballPos",            4,  [-0.5, 0.5],  "Indicates where in front of the ballsensor the ball is"],
+        ["header",              8,  None, "Header byte indicating the type of packet"],
+        ["remVersion",          4,  None, "Version of roboteam_embedded_messages"],
+        ["id",                  4,  None, "Id of the robot "],
+        ["messageId",           4,  None, "Id of the message"],
+        ["batteryLevel",        4,  None, "The voltage level of the battery"],
+        ["XsensCalibrated",     1,  None, "Indicates if the XSens IMU is calibrated"],
+        ["ballSensorWorking",   1,  None, "Indicates if the ballsensor is working"],
+        ["hasBall",             1,  None, "Indicates if the ball is somewhere in front of the ballsensor"],
+        ["capacitorCharged",    1,  None, "Indicates if the capacitor for kicking and chipping is charged"],
+        ["ballPos",             4,  [-0.5, 0.5],  "Indicates where in front of the ballsensor the ball is"],
         ["rho",                16, [0, 8],                 "The estimated magnitude of movement (m/s)"],
         ["theta",              16, [-math.pi, math.pi],    "The estimated direction of movement (rad)"],
         ["angle",              16, [-math.pi, math.pi],    "The estimated angle (rad)"],
-        ["wheelLocked",        4,  None, "Indicates if a wheel is locked. One bit per wheel"],
-        ["wheelBraking",       4,  None, "Indicates if a wheel is slipping. One bit per wheel"],
-        ["rssi",               8,  None, "Signal strength of the last packet received by the robot"]
+        ["wheelLocked",         4,  None, "Indicates if a wheel is locked. One bit per wheel"],
+        ["wheelBraking",        4,  None, "Indicates if a wheel is slipping. One bit per wheel"],
+        ["rssi",                8,  None, "Signal strength of the last packet received by the robot"]
     ],
     "REM_RobotStateInfo" : [
-        ["header",             8,  None, "Header byte indicating the type of packet"],
-        ["remVersion",         4,  None, "Version of roboteam_embedded_messages"],
-        ["id",                 4,  None, "Id of the robot "],
-        ["messageId",          4,  None, "Id of the message"],
-        ["xsensAcc1",         32, [-50000., 50000.], "xsensAcc1"],
-        ["xsensAcc2",         32, [-50000., 50000.], "xsensAcc2"],
-        ["xsensYaw",          32, [-50000., 50000.], "xsensYaw"],
-        ["rateOfTurn",        32, [-50000., 50000.], "rateOfTurn"],
-        ["wheelSpeed1",       32, [-50000., 50000.], "wheelSpeed1"],
-        ["wheelSpeed2",       32, [-50000., 50000.], "wheelSpeed2"],
-        ["wheelSpeed3",       32, [-50000., 50000.], "wheelSpeed3"],
-        ["wheelSpeed4",       32, [-50000., 50000.], "wheelSpeed4"],
+        ["header",              8,  None, "Header byte indicating the type of packet"],
+        ["remVersion",          4,  None, "Version of roboteam_embedded_messages"],
+        ["id",                  4,  None, "Id of the robot "],
+        ["messageId",           4,  None, "Id of the message"],
+        ["xsensAcc1",          32, [-50000., 50000.], "xsensAcc1"],
+        ["xsensAcc2",          32, [-50000., 50000.], "xsensAcc2"],
+        ["xsensYaw",           32, [-50000., 50000.], "xsensYaw"],
+        ["rateOfTurn",         32, [-50000., 50000.], "rateOfTurn"],
+        ["wheelSpeed1",        32, [-50000., 50000.], "wheelSpeed1"],
+        ["wheelSpeed2",        32, [-50000., 50000.], "wheelSpeed2"],
+        ["wheelSpeed3",        32, [-50000., 50000.], "wheelSpeed3"],
+        ["wheelSpeed4",        32, [-50000., 50000.], "wheelSpeed4"],
     ],
     "REM_RobotBuzzer" : [
-    	["header",             8,  None, "Header byte indicating the type of packet"],
-        ["remVersion",         4,  None, "Version of roboteam_embedded_messages"],
-        ["id",                 4,  None, "Id of the robot"],
-        ["messageId",          4,  None, "Id of the message"],
+    	["header",              8,  None, "Header byte indicating the type of packet"],
+        ["remVersion",          4,  None, "Version of roboteam_embedded_messages"],
+        ["id",                  4,  None, "Id of the robot"],
+        ["messageId",           4,  None, "Id of the message"],
     	["period",             12, None, "Sound that the buzzer makes."],
     	["duration",           16, [0., 5.], "Duration of the sound"]
     ],
-
     "REM_BasestationLog" : [
-        ["header",     8, None, "Header byte indicating the type of packet"]
+        ["header",              8, None, "Header byte indicating the type of packet"],
+        ["remVersion",          4, None, "Version of roboteam_embedded_messages"],
+        ["messageLength",       8, None, "Length of the following message"]
     ],
     "REM_RobotLog" : [
-        ["header",     8, None, "Header byte indicating the type of packet"],
-        ["remVersion", 4, None, "Version of roboteam_embedded_messages"],
-        ["id",         4, None, "Id of the robot"],
-        ["message_length", 8, None, "Length of the following message"]
+        ["header",              8, None, "Header byte indicating the type of packet"],
+        ["remVersion",          4, None, "Version of roboteam_embedded_messages"],
+        ["id",                  4, None, "Id of the robot"],
+        ["messageLength",       8, None, "Length of the following message"]
     ],
     "REM_BasestationGetConfiguration" : [
-        ["header",     8, None, "Header byte indicating the type of packet"]
+        ["header",              8, None, "Header byte indicating the type of packet"],
+        ["remVersion",          4, None, "Version of roboteam_embedded_messages"],
     ],
     "REM_BasestationConfiguration" : [
-        ["header",     8, None, "Header byte indicating the type of packet"],
-        ["remVersion", 4, None, "Version of roboteam_embedded_messages"],
-        ["channel",    1, None, "Channel on which the basestation and robots communicate"]
+        ["header",              8, None, "Header byte indicating the type of packet"],
+        ["remVersion",          4, None, "Version of roboteam_embedded_messages"],
+        ["channel",             1, None, "Channel on which the basestation and robots communicate"]
     ],
     "REM_BasestationSetConfiguration" : [
-        ["header",     8, None, "Header byte indicating the type of packet"],
-        ["remVersion", 4, None, "Version of roboteam_embedded_messages"],
-        ["channel",    1, None, "Channel on which the basestation and robots communicate"]
+        ["header",              8, None, "Header byte indicating the type of packet"],
+        ["remVersion",          4, None, "Version of roboteam_embedded_messages"],
+        ["channel",             1, None, "Channel on which the basestation and robots communicate"]
     ],
-
     "REM_RobotGetPIDGains" : [
-        ["header",     8, None, "Header byte indicating the type of packet"],
-        ["remVersion", 4, None, "Version of roboteam_embedded_messages"],
-        ["id",         4, None, "Id of the robot"]
+        ["header",              8, None, "Header byte indicating the type of packet"],
+        ["remVersion",          4, None, "Version of roboteam_embedded_messages"],
+        ["id",                  4, None, "Id of the robot"]
     ],
-
     "REM_RobotPIDGains" : [
-        ["header",             8,  None, "Header byte indicating the type of packet"],
-        ["remVersion",         4,  None, "Version of roboteam_embedded_messages"],
-        ["id",                 4,  None, "Id of the robot"],
+        ["header",              8,  None, "Header byte indicating the type of packet"],
+        ["remVersion",          4,  None, "Version of roboteam_embedded_messages"],
+        ["id",                  4,  None, "Id of the robot"],
         ["PbodyX",             16, [0.,40.], "Received P gain of the PID for body_x (x-direction)"],
         ["IbodyX",             16, [0.,20.], "Received I gain of the PID for body_x (x-direction)"],
         ["DbodyX",             16, [0.,10.], "Received D gain of the PID for body_x (x-direction)"],
@@ -106,26 +106,24 @@ packets = {
         ["Iwheels",            16, [0.,20.], "Received I gain of the PID for the wheels"],
         ["Dwheels",            16, [0.,10.], "Received D gain of the PID for the wheels"]
     ],
-
     "REM_RobotAssuredPacket" : [
-        ["header",         8, None, "Header byte indicating the type of packet"],
-        ["remVersion",     4, None, "Version of roboteam_embedded_messages"],
-        ["id",             4, None, "Id of the robot"],
-        ["sequenceNumber", 8, None, "Number to match this packet with AssuredAck"],
-        ["messageLength",  8, None, "Length of the following message"]
+        ["header",              8, None, "Header byte indicating the type of packet"],
+        ["remVersion",          4, None, "Version of roboteam_embedded_messages"],
+        ["id",                  4, None, "Id of the robot"],
+        ["sequenceNumber",      8, None, "Number to match this packet with AssuredAck"],
+        ["messageLength",       8, None, "Length of the following message"]
     ],
     "REM_RobotAssuredAck" : [
-        ["header",         8, None, "Header byte indicating the type of packet"],
-        ["remVersion",     4, None, "Version of roboteam_embedded_messages"],
-        ["id",             4, None, "Id of the robot"],
-        ["sequenceNumber", 8, None, "Number to match this packet with AssuredPacket"],
+        ["header",              8, None, "Header byte indicating the type of packet"],
+        ["remVersion",          4, None, "Version of roboteam_embedded_messages"],
+        ["id",                  4, None, "Id of the robot"],
+        ["sequenceNumber",      8, None, "Number to match this packet with AssuredPacket"],
     ],
-
     # https://media.digikey.com/pdf/Data%20Sheets/Semtech%20PDFs/SX1280-81_Rev3.2_Mar2020.pdf see page 124. 
     # Minimum payload length is 6 bytes
     "REM_SX1280Filler" : [
-        ["header",             8,  None, "Header byte indicating the type of packet"],
-        ["remVersion",         4,  None, "Version of roboteam_embedded_messages"],
+        ["header",              8,  None, "Header byte indicating the type of packet"],
+        ["remVersion",          4,  None, "Version of roboteam_embedded_messages"],
         ["fillerBits",         36,  None, "SX1280 requires a minimum of 6 bytes payload. See documentation page 124."]
     ]
 

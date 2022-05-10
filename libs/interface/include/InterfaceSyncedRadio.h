4e10e976a44d6d07e000620a58c0fdd6f3f5f4e7
Dawid <git@dawidkulikowski.pl>
2022-05-10 14:13:22 +0200

    Add missing mutex and disable Qt radio

--- libs/interface/include/InterfaceSyncedRadio.h
+++ libs/interface/include/InterfaceSyncedRadio.h
@@ -1,35 +1,35 @@
+////
+//// Created by Dawid Kulikowski on 04/10/2021.
+////
 //
-// Created by Dawid Kulikowski on 04/10/2021.
+//#ifndef RTT_INTERFACESYNCEDRADIO_H
+//#define RTT_INTERFACESYNCEDRADIO_H
+//#include <QRadioButton>
+//#include <QButtonGroup>
+//#include "roboteam_interface_utils/InterfaceDeclaration.h"
+//#include "InterfaceControllerClient.h"
+//#include "MainWindow.h"
 //
-
-#ifndef RTT_INTERFACESYNCEDRADIO_H
-#define RTT_INTERFACESYNCEDRADIO_H
-#include <QRadioButton>
-#include <QButtonGroup>
-#include "roboteam_interface_utils/InterfaceDeclaration.h"
-#include "InterfaceControllerClient.h"
-#include "MainWindow.h"
-
-namespace rtt::Interface {
-    class InterfaceSyncedRadio: public QButtonGroup {
-        Q_OBJECT
-    public:
-        InterfaceSyncedRadio(std::weak_ptr<InterfaceControllerClient>, std::string, QWidget* = nullptr);
-    private:
-        std::string identity;
-        int dpi;
-
-        std::weak_ptr<InterfaceControllerClient> ctrl;
-
-        void updateProps(const InterfaceDeclaration&);
-    protected slots:
-        void updateDeclaration();
-        void updateValue();
-
-        void notifyChangedValue(int id, bool enabled);
-
-    };
-}
-
-
-#endif  // RTT_INTERFACESYNCEDRADIO_H
+//namespace rtt::Interface {
+//    class InterfaceSyncedRadio: public QButtonGroup {
+//        Q_OBJECT
+//    public:
+//        InterfaceSyncedRadio(std::weak_ptr<InterfaceControllerClient>, std::string, QWidget* = nullptr);
+//    private:
+//        std::string identity;
+//        int dpi;
+//
+//        std::weak_ptr<InterfaceControllerClient> ctrl;
+//
+//        void updateProps(const InterfaceDeclaration&);
+//    protected slots:
+//        void updateDeclaration();
+//        void updateValue();
+//
+//        void notifyChangedValue(int id, bool enabled);
+//
+//    };
+//}
+//
+//
+//#endif  // RTT_INTERFACESYNCEDRADIO_H

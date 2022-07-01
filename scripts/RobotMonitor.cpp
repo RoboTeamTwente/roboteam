b04d1316cc305e963a2a4e4e05fe34fac2dcd0a0
Floris Heinen <Floris.heinen1@gmail.com>
2022-07-01 14:58:56 +0200

    Add dribbler status to monitor script
    Now, you can see both the status of the ball sensor and the dribbler

--- scripts/RobotMonitor.cpp
+++ scripts/RobotMonitor.cpp
@@ -7,33 +7,46 @@
 
 using namespace rtt;
 
-enum OwnBallType {
-    UNKNOWN, TRUE, FALSE
-};
-
 constexpr int MAX_AMOUNT_OF_ROBOTS = 16;
 constexpr int FORGET_TIME_MS = 1500; // After this time, we forget if a robot had that ball or not
 
-typedef std::array<std::pair<OwnBallType, std::chrono::steady_clock::time_point>, MAX_AMOUNT_OF_ROBOTS> RobotsWithBall;
+typedef std::array<std::pair<rtt::RobotFeedback, std::chrono::steady_clock::time_point>, MAX_AMOUNT_OF_ROBOTS> RobotStatuses;
 
 std::mutex yellowProtector;
-RobotsWithBall yellowBots;
+RobotStatuses yellowBots;
 std::mutex blueProtector;
-RobotsWithBall blueBots;
+RobotStatuses blueBots;
 
-auto lastPrintTime = std::chrono::steady_clock::now();
-bool updateHappened = false;
+constexpr char HAS_BAL_CHAR = 'O';
+constexpr char HAS_NOT_BAL_CHAR = 'X';
+constexpr char BROKEN_SENSOR_CHAR = '*';
 
-std::ostream& operator<<(std::ostream& os, const std::pair<OwnBallType, std::chrono::steady_clock::time_point>& data) {
-    switch (data.first) {
-        case OwnBallType::FALSE:
-            return os << "X";
-        case OwnBallType::TRUE:
-            return os << "O";
-        case OwnBallType::UNKNOWN: default:
-            return os << " ";
+auto lastTimePrintHappened = std::chrono::steady_clock::now();
+bool updateHappenedSincePrint = false;
+
+std::ostream& operator<<(std::ostream& os, const std::pair<rtt::RobotFeedback, std::chrono::steady_clock::time_point>& data) {
+    auto now = std::chrono::steady_clock::now();
+    auto timeSinceLastUpdate = std::chrono::duration_cast<std::chrono::milliseconds>(now - data.second).count();
+
+    if (timeSinceLastUpdate > FORGET_TIME_MS) {
+        // Just print an empty robot status
+        os << "   ";
+    } else {
+        if (data.first.ballSensorIsWorking) {
+            os << (data.first.ballSensorSeesBall ? HAS_BAL_CHAR : HAS_NOT_BAL_CHAR);
+        } else {
+            os << BROKEN_SENSOR_CHAR;
+        }
+
+        os << "/";
+
+        os << (data.first.dribblerSeesBall ? HAS_BAL_CHAR : HAS_NOT_BAL_CHAR);
     }
+
+
+    return os;
 }
+
 
 void printStatus() {
     const auto& y = yellowBots;
@@ -41,38 +54,18 @@ void printStatus() {
 
     std::scoped_lock<std::mutex, std::mutex> locks(yellowProtector, blueProtector);
 
-    std::cout << "┏━━━━━━━┫ Yellow ┣━━━━━━┳━━━━━━━━┫ Blue ┣━━━━━━━┓" << std::endl
-    << "┃ 0: "<<y[0]<<" 4: "<<y[4]<< "  8: "<< y[8]<<" 12: "<<y[12]<<" ┃ 0: "<<b[0]<<" 4: "<<b[4]<<"  8: "<< b[8]<<" 12: "<<b[12]<<" ┃"<<std::endl
-    << "┃ 1: "<<y[1]<<" 5: "<<y[5]<< "  9: "<< y[9]<<" 13: "<<y[13]<<" ┃ 1: "<<b[1]<<" 5: "<<b[5]<<"  9: "<< b[9]<<" 13: "<<b[13]<<" ┃"<<std::endl
-    << "┃ 2: "<<y[2]<<" 6: "<<y[6]<< " 10: "<<y[10]<<" 14: "<<y[14]<<" ┃ 2: "<<b[2]<<" 6: "<<b[6]<<" 10: "<<b[10]<<" 14: "<<b[14]<<" ┃"<<std::endl
-    << "┃ 3: "<<y[3]<<" 7: "<<y[7]<< " 11: "<<y[11]<<" 15: "<<y[15]<<" ┃ 3: "<<b[3]<<" 7: "<<b[7]<<" 11: "<<b[11]<<" 15: "<<b[15]<<" ┃"<<std::endl
-    << "┗━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━┛" << std::endl
-    << "O: Bot has ball X: Bot doesn't have ball ' ': Unknown" << std::endl;
+    std::cout << "┏━━━┫ Yellow ┣━━━┳━━━━┫ Blue ┣━━━━┓" << std::endl
+    << "┃ 0: "<<y[0]<<"  8: "<< y[8]<< " ┃ 0: "<< b[0]<<"  8: "<< b[8]<<" ┃ A/B:"<<std::endl
+    << "┃ 1: "<<y[1]<<"  9: "<< y[9]<< " ┃ 1: "<< b[1]<<"  9: "<< b[9]<<" ┃ A: BallSensor status"<<std::endl
+    << "┃ 2: "<<y[2]<<" 10: "<<y[10]<< " ┃ 2: "<< b[2]<<" 10: "<<b[10]<<" ┃ B: Dribbler status"<<std::endl
+    << "┃ 3: "<<y[3]<<" 11: "<<y[11]<< " ┃ 3: "<< b[3]<<" 11: "<<b[11]<<" ┃"<<std::endl
+    << "┃ 4: "<<y[4]<<" 12: "<<y[12]<< " ┃ 4: "<< b[4]<<" 12: "<<b[12]<<" ┃ O: Notices ball"<<std::endl
+    << "┃ 5: "<<y[5]<<" 13: "<<y[13]<< " ┃ 5: "<< b[5]<<" 13: "<<b[13]<<" ┃ X: Does not notice ball"<<std::endl
+    << "┃ 6: "<<y[6]<<" 14: "<<y[14]<< " ┃ 6: "<< b[6]<<" 14: "<<b[14]<<" ┃ *: Broken"<<std::endl
+    << "┃ 7: "<<y[7]<<" 15: "<<y[15]<< " ┃ 7: "<< b[7]<<" 15: "<<b[15]<<" ┃"<<std::endl
+    << "┗━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━┛" << std::endl;
 
-    lastPrintTime = std::chrono::steady_clock::now();
-}
-
-// Initializes the array to an empty state
-void clearArray(RobotsWithBall& arr) {
-    auto now = std::chrono::steady_clock::now();
-    for (int i = 0; i < MAX_AMOUNT_OF_ROBOTS; i++) {
-        arr[i] = { OwnBallType::UNKNOWN, now };
-    }
-}
-
-// Removes outdated data of the array
-int cleanUpArray(RobotsWithBall& arr) {
-    auto now = std::chrono::steady_clock::now();
-    int cleanCounter = 0;
-
-    for (auto &[type, time] : arr) {
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(now - time).count();
-        if (type != OwnBallType::UNKNOWN && duration > FORGET_TIME_MS) {
-            type = OwnBallType::UNKNOWN;
-            cleanCounter++;
-        }
-    }
-    return cleanCounter;
+    lastTimePrintHappened = std::chrono::steady_clock::now();
 }
 
 void onFeedback(const rtt::RobotsFeedback& feedbacks) {
@@ -83,33 +76,31 @@ void onFeedback(const rtt::RobotsFeedback& feedbacks) {
 
     auto now = std::chrono::steady_clock::now();
     for (const auto& feedback : feedbacks.feedback) {
-        if (feedback.id >= 0 && feedback.id < MAX_AMOUNT_OF_ROBOTS) {
-            auto type = feedback.hasBall ? OwnBallType::TRUE : OwnBallType::FALSE;
-
-            if (type != arr[feedback.id].first) updateCounter++;
-
-            arr[feedback.id] = { type, now };
+        auto id = feedback.id;
+        if (id >= 0 && id < MAX_AMOUNT_OF_ROBOTS) {
+            // If the feedback is different than what we previously had, update it
+            if (feedback != arr[id].first) {
+                arr[id].first = feedback;
+                updateCounter++;
+            }
+            // Always update the timer if we received data about this robot.
+            arr[feedback.id].second = now;
         }
     }
 
-    if (updateCounter > 0) updateHappened = true;
+    if (updateCounter > 0) updateHappenedSincePrint = true;
 }
 
 int main() {
-    clearArray(yellowBots);
-    clearArray(blueBots);
-
     auto feedbackSub = net::RobotFeedbackSubscriber(onFeedback);
 
     while (true) {
-        if (cleanUpArray(yellowBots) > 0 || cleanUpArray(blueBots) > 0) updateHappened = true;
-
         auto now = std::chrono::steady_clock::now();
-        auto timeSinceLastPrint = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastPrintTime).count();
+        auto timeSinceLastPrint = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastTimePrintHappened).count();
 
-        if (updateHappened || timeSinceLastPrint > 1000) {
+        if (updateHappenedSincePrint || timeSinceLastPrint > 1000) {
             printStatus();
-            updateHappened = false;
+            updateHappenedSincePrint = false;
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }

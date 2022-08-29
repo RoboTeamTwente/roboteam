4ceee3dab30913ec61e288eae75af78dc353897d
Floris Heinen <Floris.heinen1@gmail.com>
2022-08-29 05:37:40 +0200

    Remove unused CMake module
    We did not use the ibx websockets anymore, so I removed the dependency

--- cmake_modules/GetWebsocket.cmake
+++
@@ -1,8 +0,0 @@
-include(FetchContent)
-
-FetchContent_Declare(proj_ixwebsocket
-        GIT_REPOSITORY https://github.com/machinezone/IXWebSocket
-        GIT_TAG master
-        )
-
-FetchContent_MakeAvailable(proj_ixwebsocket)

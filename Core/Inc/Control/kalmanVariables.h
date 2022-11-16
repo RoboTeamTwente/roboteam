bb9d7b33e150c52567ccfc0cbc26e2be669a037c
Tom Meulenkamp <t.meulenkamp@roboteamtwente.nl>
2022-11-16 09:56:08 +0100

    Extend documentation for all control files

--- Core/Inc/Control/kalmanVariables.h
+++ Core/Inc/Control/kalmanVariables.h
@@ -25,6 +25,7 @@
 #define RAND_VAR 0.5F // variance in the random force
 
 ///////////////////////////////////////////////////// VARIABLES
+// TODO: Ask old team members to explain the variables and their use.
 
 //create arrays
 float aF[STATE*STATE] = {
@@ -32,31 +33,37 @@ float aF[STATE*STATE] = {
 		0, 1, 0, 0,
 		0, 0, 1, TIMESTEP,
 		0, 0, 0, 1};
+	
 float aH[OBSERVE*STATE] = {
 		1, 0, 0, 0,
 		0, 1, 0, 0,
 		0, 0, 1, 0,
 		0, 0, 0, 1};
+	
 float aR[OBSERVE*OBSERVE] = {
 		VEL_VAR, 0, 0, 0,
 		0, ACC_VAR, 0, 0,
 		0, 0, VEL_VAR, 0,
 		0, 0, 0, ACC_VAR};
+
 float aI[STATE*STATE] = {
 		1,0,0,0,
 		0,1,0,0,
 		0,0,1,0,
 		0,0,0,1};
+
 float aPold[STATE*STATE] = {
 		STATE_VAR,0,0,0,
 		0,STATE_VAR,0,0,
 		0,0,STATE_VAR,0,
 		0,0,0,STATE_VAR};
+		
 float aB[STATE*STATE] = {
 		1, 0, 0, 0,
 		0, 1, 0, 0,
 		0, 0, 1, 0,
 		0, 0, 0, 1};
+	
 float aQ[STATE*STATE] = {
 		TIMESTEP*TIMESTEP*RAND_VAR, TIMESTEP*RAND_VAR, 0, 0,
 		TIMESTEP*RAND_VAR, RAND_VAR, 0, 0,
@@ -64,25 +71,25 @@ float aQ[STATE*STATE] = {
 		0, 0, TIMESTEP*RAND_VAR, RAND_VAR};
 
 //empty arrays
-float aXold[STATE] = {0.0f};
-float az[OBSERVE] = {0.0f};
-float aXcurrent[STATE] = {0.0f};
-float aFX[STATE] = {0.0f};
+float aXold[STATE] = {0.0f};			// The old state
+float az[OBSERVE] = {0.0f};				
+float aXcurrent[STATE] = {0.0f};		// The current tate
+float aFX[STATE] = {0.0f};				// The state transition from this to the next state
 float aFt[STATE*STATE] = {0.0f};
 float aFP[STATE*STATE] = {0.0f};
-float aPcurrent[STATE*STATE] = {0.0f};
-float ayold[OBSERVE] = {0.0f};
+float aPcurrent[STATE*STATE] = {0.0f};	// The current prediction matrix
+float ayold[OBSERVE] = {0.0f};			
 float aHX[OBSERVE] = {0.0f};
 float aS[OBSERVE*OBSERVE] = {0.0f};
 float aHt[STATE*OBSERVE] = {0.0f};
 float aPHt[STATE*OBSERVE] = {0.0f};
 float aFPFt[STATE*STATE] = {0.0f};
 float aHPHt[OBSERVE*OBSERVE] = {0.0f};
-float aK[STATE*OBSERVE] = {0.0f};
+float aK[STATE*OBSERVE] = {0.0f};		// Kalman gain
 float aSi[OBSERVE*OBSERVE] = {0.0f};
-float aXnew[STATE] = {0.0f};
+float aXnew[STATE] = {0.0f};			// The next state
 float aKy[STATE] = {0.0f};
-float aPnew[STATE*STATE] = {0.0f};
+float aPnew[STATE*STATE] = {0.0f};		// The next prediction matrix
 float aKt[OBSERVE*STATE] = {0.0f};
 float aKR[STATE*OBSERVE] = {0.0f};
 float aKRKt[STATE*STATE] = {0.0f};
--- Core/Inc/Util/control_util.h
+++ Core/Inc/Util/control_util.h
@@ -29,7 +29,7 @@
 #define WIRELESS_RX_COUNT 4000  // count after which wireless should go to timeout after last packet. Multiply with period base (62.5 us) to get to the time in seconds.
 
 // Robot
-#define rad_robot 0.081F 	// robot radius (m) (from center to wheel contact point)
+#define rad_robot 0.081F	// robot radius (m) (from center to wheel contact point)
 #define rad_wheel 0.028F 	// wheel radius (m)
 
 #define FRONT_ANGLE 30		// angle of front wheels (deg)
@@ -40,13 +40,13 @@
 #define sinBack sin(BACK_ANGLE * M_PI/180)
 
 // Wheels
-#define PWM_CUTOFF 200.0F // arbitrary treshold to avoid motor shutdown
-#define WHEEL_GEAR_RATIO 2.65F // gear ratio between motor and wheel
-#define PWM_LIMIT MAX_PWM // should be equal to MAX_PWM by default
-float MAX_VOLTAGE; // [V] see datasheet
+#define PWM_CUTOFF 200.0F 		// arbitrary threshold to avoid motor shutdown
+#define WHEEL_GEAR_RATIO 2.65F 	// gear ratio between motor and wheel
+#define PWM_LIMIT MAX_PWM 		// should be equal to MAX_PWM by default
+float MAX_VOLTAGE; 				// [V] see datasheet
 #define MAX_VOLTAGE_30W 12.0
 #define MAX_VOLTAGE_50W 24.0
-float SPEED_CONSTANT; //[(rad/s)/V] see datasheet
+float SPEED_CONSTANT; 			//[(rad/s)/V] see datasheet
 #define SPEED_CONSTANT_30W 374.0
 #define SPEED_CONSTANT_50W 285.0
 #define WHEEL_PULSES_PER_ROTATION (float)4*1024 // number of pulses of the encoder per rotation of the motor (see datasheet)
@@ -55,21 +55,22 @@ float OMEGAtoPWM; // conversion factor from wheel speed [rad/s] to required PWM 
 #define WHEEL_ENCODER_TO_OMEGA (float)2*M_PI/(TIME_DIFF*WHEEL_GEAR_RATIO*WHEEL_PULSES_PER_ROTATION) // conversion factor from number of encoder pulses to wheel speed [rad/s]
 
 // Dribbler
+// TODO: Update these constants once the new dribblers and their encoders are installed.
 #define DRIBBLER_GEAR_RATIO 0.777777778F // gear ratio between motor and dribbler
 #define DRIBBLER_PULSES_PER_ROTATION 4.0 // number of pulses of the encoder per rotation of the motor (see datasheet)4
 #define DRIBBLER_TIME_DIFF 0.1
 #define DRIBBLER_ENCODER_TO_OMEGA ((float)2*M_PI/(DRIBBLER_TIME_DIFF*DRIBBLER_GEAR_RATIO*DRIBBLER_PULSES_PER_ROTATION)) // conversion factor from number of encoder pulses to dribbler speed [rad/s]
 
 // Control
-#define YAW_MARGIN (0.5F/180.0F)*(float)M_PI // margin at which the I-value of the PID is reset to 0
-float WHEEL_REF_LIMIT; // [rad/s] Limit the maximum wheel reference to leave room for the wheels PID
-#define WHEEL_REF_LIMIT_PWM 2200 // [pwm]
+#define YAW_MARGIN (0.5F/180.0F)*(float)M_PI 	// margin at which the I-value of the PID is reset to 0
+float WHEEL_REF_LIMIT; 							// [rad/s] Limit the maximum wheel reference to leave room for the wheels PID
+#define WHEEL_REF_LIMIT_PWM 2200 				// [pwm]
 
 // Shoot
-#define MIN_KICK_TIME 1.0 				// minimum time period of kicking
-#define MAX_KICK_TIME 25.0 				// maximum time period of kicking
-#define MIN_CHIP_TIME 10 				// minimum time period of chipping
-#define MAX_CHIP_TIME 100 				// maximum time period of chipping
+#define MIN_KICK_TIME 1.0 				// minimum time [ms] period of kicking
+#define MAX_KICK_TIME 25.0 				// maximum time [ms] period of kicking
+#define MIN_CHIP_TIME 10 				// minimum time [ms] period of chipping
+#define MAX_CHIP_TIME 100 				// maximum time [ms] period of chipping
 #define TIMER_FREQ 10000 			// frequency [Hz] of TIM6  (Clock frequency divided by prescaler)
 #define READY_CALLBACK_FREQ 1 		// frequency [Hz] of callback when shootState is Ready
 #define CHARGING_CALLBACK_FREQ 10 	// frequency [Hz] of callback when shootState is Charging
@@ -79,38 +80,38 @@ float WHEEL_REF_LIMIT; // [rad/s] Limit the maximum wheel reference to leave roo
 ///////////////////////////////////////////////////// STRUCTS
 
 typedef enum {
-	body_x,
-	body_y,
-	body_w,
-	body_yaw,
+	body_x,		// The velocity in the X direction
+	body_y,		// The velocity in the Y direction
+	body_w,		// The angular velocity
+	body_yaw,	// The angle
 }body_handles;
 
 typedef enum {
-	wheels_RF,
-	wheels_RB,
-	wheels_LB,
-	wheels_LF,
+	wheels_RF,	// The right front wheel
+	wheels_RB,	// The right back wheel
+	wheels_LB,	// The left back wheel
+	wheels_LF,	// The left front wheel
 }wheel_names;
 
 typedef enum {
-	off,
-	setup,
-	on,
-	turning,
-	idle
+	off,		// The PID controller is inactive
+	setup,		// Not used at this moment
+	on,			// The PID controller is active
+	turning,	// Not used at this moment
+	idle		// Not used at this moment
 }PID_states;// keeps track of the state of the system
 
 struct PIDstruct{
-	float kP;
-	float kI;
-	float kD;
-	float I;
-	float prev_e;
-	float timeDiff;
-	float minOutput;
-	float maxOutput;
-	float ramp;
-	float prev_PID;
+	float kP;			// The gain of the proportional action 
+	float kI;			// The gain of the integrating action
+	float kD;			// The gain fo the deriving action
+	float I;			// The integral of this PID
+	float prev_e;		// The previous error
+	float timeDiff;		// The time difference due to 100Hz frequency
+	float minOutput;	// Not being used at this moment. The minimal output of this PID
+	float maxOutput;	// Not being used at this moment. The maximal output of this PID
+	float ramp;			// Not being used at this moment. The maximal change of this PID value
+	float prev_PID;		// The previous PID value
 } static PIDdefault = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, TIME_DIFF, -1000000, 1000000, 1000000, 0};
 
 typedef struct PIDstruct PIDvariables;
@@ -121,7 +122,11 @@ typedef struct PIDstruct PIDvariables;
  */
 void control_util_Init();
 
-//Initializes the PID values
+/**
+ * Initializes the PID values.
+ * 
+ * Loads the constants into the struct and sets up the default PID parameters.
+ */
 void initPID(PIDvariables* PID, float kP, float kI, float kD);
 
 //clamps the input

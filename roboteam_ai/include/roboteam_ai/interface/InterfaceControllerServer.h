83bfb96dff458b683cb869a5d6a3bbee2b0b8333
Floris Heinen <floris.heinen1@gmail.com>
2022-12-14 04:25:57 +0100

    AI: Update interface controller server

--- roboteam_ai/include/roboteam_ai/interface/InterfaceControllerServer.h
+++ roboteam_ai/include/roboteam_ai/interface/InterfaceControllerServer.h
@@ -12,13 +12,19 @@
 
 namespace rtt::Interface {
 class InterfaceControllerServer : public InterfaceController<proto::ModuleState, proto::UiValues> {
-public:
-    InterfaceControllerServer() : InterfaceController<proto::ModuleState, proto::UiValues>(rtt::net::utils::ChannelType::AI_TO_INTERFACE_CHANNEL, rtt::net::utils::ChannelType::INTERFACE_TO_AI_CHANNEL, 1, 100) {}
-
-    bool hasPriorityData() const noexcept override;
+    InterfaceControllerServer()
+        : InterfaceController<proto::ModuleState, proto::UiValues>(rtt::net::utils::ChannelType::AI_TO_INTERFACE_CHANNEL, rtt::net::utils::ChannelType::INTERFACE_TO_AI_CHANNEL) {}
+    
+    void loop() override;
 
     void handleData(const proto::UiValues &state) override;
 
+    proto::ModuleState getDataForRemote() const noexcept override;
+   public:
+    void trigger_update();
+
+   private:
+    std::binary_semaphore update_marker{1};
     proto::ModuleState getDataForRemote(bool expired) const noexcept override;
 
     std::string getSelectedPlay();

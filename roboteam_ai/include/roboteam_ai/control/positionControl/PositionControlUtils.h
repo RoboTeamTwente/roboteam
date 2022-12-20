65ed84e20ce14b720d6bb38c8f7e55639e7d848f
JAndringa <andringajibbe@gmail.com>
2022-12-20 16:23:20 +0100

    Added documentation to PositionControlUtils.h

--- roboteam_ai/include/roboteam_ai/control/positionControl/PositionControlUtils.h
+++ roboteam_ai/include/roboteam_ai/control/positionControl/PositionControlUtils.h
@@ -10,32 +10,33 @@
 
 namespace rtt::ai::control {
 
+/**
+ * @brief Class containing useful functions used in position control
+ */
 class PositionControlUtils {
    private:
-    static constexpr double MAX_TARGET_DEVIATION = 0.05;
-
-    // minimum distance needed to consider the current target reached
-    static constexpr double MIN_DISTANCE_TARGET_REACHED = 2 * Constants::ROBOT_RADIUS();
+    static constexpr double MAX_TARGET_DEVIATION = 0.05; /**< Maximum distance a target can deviate before it is defined as a new position */
+    static constexpr double MIN_DISTANCE_TARGET_REACHED = 2 * Constants::ROBOT_RADIUS(); /**< minimum distance needed to consider the current target reached */
 
    public:
     /**
-     * If the distance between the old target and the new target > MAX_TARGET_DEVIATION
-     * @param targetPos
-     * @param oldTarget
-     * @return
+     * @brief Checks whether the distance between the old target and the new target is greater than MAX_TARGET_DEVIATION
+     * @param targetPos Target position to go to
+     * @param oldTarget Previous target position
+     * @return Boolean that tells whether the target position changed
      */
     static bool isTargetChanged(const Vector2 &targetPos, const Vector2 &oldTarget);
 
     /**
-     * The target is considered to be reached if the distance between target and currentPosition < MIN_DISTANCE
-     * @param targetPos
-     * @param currentPosition
-     * @return
+     * @brief checks whether the distance between target and currentPosition is greater than MIN_DISTANCE
+     * @param targetPos Target position to got to
+     * @param currentPosition Previous target positions
+     * @return Boolean that tells whether the target position has been reached
      */
     static bool isTargetReached(const Vector2 &targetPos, const Vector2 &currentPosition);
 
     /**
-     * Removes the first element in the path in the case the first point is reached
+     * @brief Removes the first element in the path in the case the first point is reached
      * @param path the vector of path points
      * @param currentPosition the current position of the robot
      */

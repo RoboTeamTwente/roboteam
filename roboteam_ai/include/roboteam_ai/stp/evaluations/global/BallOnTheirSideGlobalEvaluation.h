b48c33b5aeca5c526a0f30e4bb7f71d4020069be
JAndringa <andringajibbe@gmail.com>
2022-12-21 12:16:28 +0100

    Added documentation to BallOnTheirSideGlobalEvaluation.h

--- roboteam_ai/include/roboteam_ai/stp/evaluations/global/BallOnTheirSideGlobalEvaluation.h
+++ roboteam_ai/include/roboteam_ai/stp/evaluations/global/BallOnTheirSideGlobalEvaluation.h
@@ -8,10 +8,23 @@
 #include "stp/evaluations/BaseEvaluation.h"
 
 namespace rtt::ai::stp::evaluation {
+/**
+ * @brief Class that evaluates ball on their side
+ */
 class BallOnTheirSideGlobalEvaluation : public BaseEvaluation {
    public:
+    /**
+     * @brief Calculates the score for ball on their side
+     * @param world The current world
+     * @param field The current field
+     * @return The score of ball on their side
+     */
     [[nodiscard]] uint8_t metricCheck(const world::World* world, const Field* field) const noexcept override;
 
+    /**
+     * @brief Retrieves the name of the evaluation
+     * @return A string containing the name of the evaluation
+     */
     const char* getName() override { return "BallOnTheirSideGlobalEvaluation"; }
 };
 }  // namespace rtt::ai::stp::evaluation

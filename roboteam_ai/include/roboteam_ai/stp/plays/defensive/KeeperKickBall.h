11994e01e5bb3a51afda8c784a65f2383e9d4f47
JAndringa <andringajibbe@gmail.com>
2022-12-21 13:45:07 +0100

    Added documentation to KeeperKickBall.h

--- roboteam_ai/include/roboteam_ai/stp/plays/defensive/KeeperKickBall.h
+++ roboteam_ai/include/roboteam_ai/stp/plays/defensive/KeeperKickBall.h
@@ -11,73 +11,74 @@
 namespace rtt::ai::stp::play {
 
 /**
- * KeeperKickBall Play is executed when the ball is in our defense area and should be kicked out by our keeper
+ * @brief KeeperKickBall Play is executed when the ball is in our defense area and should be kicked out by our keeper
  */
 
 class KeeperKickBall : public Play {
    public:
     /**
-     * Constructor that initializes roles with roles that are necessary for this play
+     * @brief Constructor that initializes roles with roles that are necessary for this play
      */
     KeeperKickBall();
 
     /**
-     * Calculates the score of this play to determine which play is best in this situation
+     * @brief Calculates the score of this play to determine which play is best in this situation
      * @param field The current Field
      * @return The score of this play (0-255)
      */
     uint8_t score(const rtt::world::Field& field) noexcept override;
 
     /**
-     * Assigns robots to roles of this play
+     * @brief Assigns robots to roles of this play
+     * @return Map with assigned roles
      */
     Dealer::FlagMap decideRoleFlags() const noexcept override;
 
     /**
-     * Calculates info for the roles
+     * @brief Calculates info for the roles
      */
     void calculateInfoForRoles() noexcept override;
 
     /**
-     * Calculates info for the defenders
+     * @brief Calculates info for the defenders
      */
     void calculateInfoForDefenders() noexcept;
 
     /**
-     * Calculates info for the midfielders
+     * @brief Calculates info for the midfielders
      */
     void calculateInfoForMidfielders() noexcept;
 
     /**
-     * Calculates info for the attackers
+     * @brief Calculates info for the attackers
      */
     void calculateInfoForAttackers() noexcept;
 
     /**
-     * Calculate info for the roles that need to be calculated for scoring
+     * @brief Calculate info for the roles that need to be calculated for scoring
      */
     void calculateInfoForScoredRoles(world::World* world) noexcept override{};
 
     /**
-     * Gets the play name
+     * @brief Retrieve name of the play
+     * @return The name of the play as a string
      */
     const char* getName() override;
 
     /**
-     * Check if play should end. True if pass arrived, if the ball is not moving anymore after pass, or if there is a better pass available
+     * @brief Check if play should end. True if pass arrived, if the ball is not moving anymore after pass, or if there is a better pass available
+     * @return Boolean that tells whether the play should end
      */
     bool shouldEndPlay() noexcept override;
 
    private:
     /**
-     * Return true if passer is done with KickAtPos tactic
+     * @brief Check if the ball has been kicked. True if the kick tactic has ended, false if it is still going.
+     * @return Boolean that tells whether the ball has been kicked
      */
     bool ballKicked();
 
-    /**
-     * Struct containing info about the pass. Calculated once for each time this play is run
-     */
-    PassInfo passInfo;
+    PassInfo passInfo; /**< Struct containing info about the pass. Calculated once for each time this play is run */
 };
 }  // namespace rtt::ai::stp::play
 

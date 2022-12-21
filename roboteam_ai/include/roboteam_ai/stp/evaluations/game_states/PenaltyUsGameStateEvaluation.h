ec2096edb1f46b5105c3772125369e5f67851332
JAndringa <andringajibbe@gmail.com>
2022-12-21 11:40:38 +0100

    Added documentation to PenaltyUsGameStateEvaluation.h

--- roboteam_ai/include/roboteam_ai/stp/evaluations/game_states/PenaltyUsGameStateEvaluation.h
+++ roboteam_ai/include/roboteam_ai/stp/evaluations/game_states/PenaltyUsGameStateEvaluation.h
@@ -8,14 +8,23 @@
 #include "stp/evaluations/BaseEvaluation.h"
 
 namespace rtt::ai::stp::evaluation {
-
 /**
- * Invariant for the game state penalty us
+ * @brief Class that evaluates the penalty us prepare game state
  */
 class PenaltyUsGameStateEvaluation : public BaseEvaluation {
    public:
+    /**
+     * @brief Calculates the score for the penalty us prepare game state
+     * @param world The current world
+     * @param field The current field
+     * @return The score of the penalty us prepare game state
+     */
     [[nodiscard]] uint8_t metricCheck(const world::World* world, const world::Field* field) const noexcept override;
-
+    
+    /**
+     * @brief Retrieves the name of the evaluation
+     * @return A string containing the name of the evaluation
+     */
     const char* getName() override { return "gs::PenaltyUs"; }
 };
 }  // namespace rtt::ai::stp::evaluation

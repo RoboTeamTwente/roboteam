11df01ad075f227d58751f93e965df5ddd60828e
JAndringa <andringajibbe@gmail.com>
2022-12-21 11:04:45 +0100

    Added documentation to BallPlacementUsGameStateEvaluation.h

--- roboteam_ai/include/roboteam_ai/stp/evaluations/game_states/BallPlacementUsGameStateEvaluation.h
+++ roboteam_ai/include/roboteam_ai/stp/evaluations/game_states/BallPlacementUsGameStateEvaluation.h
@@ -8,10 +8,23 @@
 #include "stp/evaluations/BaseEvaluation.h"
 
 namespace rtt::ai::stp::evaluation {
+/**
+     * @brief Class that evaluates the ball placement us game state
+ */
 class BallPlacementUsGameStateEvaluation : public BaseEvaluation {
    public:
+    /**
+     * @brief Calculates the score for the ball placement us game state
+     * @param world The current world
+     * @param field The current field
+     * @return The score of the ball placement us game state
+     */
     [[nodiscard]] uint8_t metricCheck(const world::World* world, const world::Field* field) const noexcept override;
 
+    /**
+     * @brief Retrieves the name of the evaluation
+     * @return A string containing the name of the evaluation
+     */
     const char* getName() override { return "gs::BallPlacementUs"; }
 };
 }  // namespace rtt::ai::stp::evaluation

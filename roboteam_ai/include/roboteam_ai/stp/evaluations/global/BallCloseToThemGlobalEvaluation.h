685138cf40d06cf25514597976bd6ee8acee98f6
JAndringa <andringajibbe@gmail.com>
2022-12-21 11:54:49 +0100

    Added documentation to BallCloseToThemGlobalEvaluation.h

--- roboteam_ai/include/roboteam_ai/stp/evaluations/global/BallCloseToThemGlobalEvaluation.h
+++ roboteam_ai/include/roboteam_ai/stp/evaluations/global/BallCloseToThemGlobalEvaluation.h
@@ -10,27 +10,39 @@
 #include "stp/evaluations/BaseEvaluation.h"
 
 namespace rtt::ai::stp::evaluation {
-
+/**
+ * @brief Class that evaluates ball close to them
+ */
 class BallCloseToThemGlobalEvaluation : public BaseEvaluation {
    public:
+    /**
+     * @brief Constructor for the BallCloseToThemGlobalEvaluation class
+     */
     BallCloseToThemGlobalEvaluation() noexcept;
 
+    /**
+     * @brief Calculates the score for ball close to them
+     * @param world The current world
+     * @param field The current field
+     * @return The score of ball close to them
+     */
     [[nodiscard]] uint8_t metricCheck(const world::World* world, const world::Field* field) const noexcept override;
 
+    /**
+     * @brief Retrieves the name of the evaluation
+     * @return A string containing the name of the evaluation
+     */
     const char* getName() override { return "BallCloseToThem"; }
 
    private:
     /**
-     * Calculates the actual metric value using the piecewise linear function member
+     * @brief Calculates the actual metric value using the piecewise linear function member
      * @param x the x of the function
      * @return metric value between 0-255
      */
     [[nodiscard]] uint8_t calculateMetric(const double& x) const noexcept;
 
-    /**
-     * Unique pointer to the piecewise linear function that calculates the fuzzy value
-     */
-    std::unique_ptr<nativeformat::param::Param> piecewiseLinearFunction;
+    std::unique_ptr<nativeformat::param::Param> piecewiseLinearFunction; /**< Unique pointer to the piecewise linear function that calculates the fuzzy value */
 };
 
 }  // namespace rtt::ai::stp::evaluation

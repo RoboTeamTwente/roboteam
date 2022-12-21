f2cb1ed1e1a915222014273e2409e785210eb801
JAndringa <andringajibbe@gmail.com>
2022-12-21 11:36:01 +0100

    Added documentation to NormalPlayGameStateEvaluation.h

--- roboteam_ai/include/roboteam_ai/stp/evaluations/game_states/NormalPlayGameStateEvaluation.h
+++ roboteam_ai/include/roboteam_ai/stp/evaluations/game_states/NormalPlayGameStateEvaluation.h
@@ -8,14 +8,23 @@
 #include "stp/evaluations/BaseEvaluation.h"
 
 namespace rtt::ai::stp::evaluation {
-
 /**
- * Invariant for the game state normal play
+ * @brief Class that evaluates the normal play game state
  */
 class NormalPlayGameStateEvaluation : public BaseEvaluation {
    public:
+    /**
+     * @brief Calculates the score for the normal play game state
+     * @param world The current world
+     * @param field The current field
+     * @return The score of the normal play game state
+     */
     [[nodiscard]] uint8_t metricCheck(const world::World* world, const world::Field* field) const noexcept override;
 
+    /**
+     * @brief Retrieves the name of the evaluation
+     * @return A string containing the name of the evaluation
+     */
     const char* getName() override { return "gs::NormalPlay"; }
 };
 }  // namespace rtt::ai::stp::evaluation

d04a161d4f4d9ca481532169705616b4b0c10b4e
JAndringa <andringajibbe@gmail.com>
2022-12-21 09:53:36 +0100

    Added documentation to PositionScoring.h

--- roboteam_ai/include/roboteam_ai/stp/computations/PositionScoring.h
+++ roboteam_ai/include/roboteam_ai/stp/computations/PositionScoring.h
@@ -10,66 +10,70 @@
 #include "world/World.hpp"
 
 namespace rtt::ai::stp {
+
+/**
+ * @brief Class that manages the scoring for a position
+ */
 class PositionScoring {
    private:
     /**
-     * Determine score for the Open at given position
+     * @brief Determine score for the Open score at given position
      * @param point Position to calculate from
-     * @param field
-     * @param world
+     * @param field The current field
+     * @param world The current world
      * @param scores ref to struct linked to that pos
-     * @return score value
+     * @return Open score value
      */
     static double determineOpenScore(Vector2 &point, const rtt::Field &field, const world::World *world, gen::PositionScores &scores);
 
     /**
-     * Determine score for the Line of Sight to the ball at given position
+     * @brief Determine score for the Line of Sight to the ball at given position
      * @param point Position to calculate from
-     * @param world
+     * @param world The current world
      * @param scores ref to struct linked to that pos
-     * @return score value
+     * @return Line of Sight score value
      */
     static double determineLineOfSightScore(Vector2 &point, const world::World *world, gen::PositionScores &scores);
 
     /**
-     * Determine score for the Visibility of the goal at given position
+     * @brief Determine score for the Visibility of the goal at given position
      * @param point Position to calculate from
-     * @param field
-     * @param world
+     * @param field The current field
+     * @param world The current world
      * @param scores ref to struct linked to that pos
-     * @return score value
+     * @return Goal Shot score value
      */
     static double determineGoalShotScore(Vector2 &point, const Field &field, const world::World *world, gen::PositionScores &scores);
 
     /**
-     * Determine score for blocking potential of a position
+     * @brief Determine score for blocking potential of a position
      * @param point Position to calculate from
-     * @param world
+     * @param world The current world
      * @param scores ref to struct linked to that pos
-     * @return score value
+     * @return Blocking score value
      */
     static double determineBlockingScore(Vector2 &point, const world::World *world, gen::PositionScores &scores);
 
     /**
-     * Score a position using the given weights weights for a profile.
+     * @brief Score a position using the given weights weights for a profile.
      * Will check if the position already has a pre-calculated score (from this tick) then throws the weight over it
      * and sums the scores, resulting in a position scored a particular set of weights.
      * @param profile set of weights of the different factors that determine the score
      * @param position x,y coordinates
      * @param scores reference to scores of said position in map
-     * @param field
-     * @param world
+     * @param field The current field
+     * @param world The current world
      * @return score of position including the weights
      */
     static uint8_t getScoreOfPosition(const gen::ScoreProfile &profile, Vector2 position, gen::PositionScores &scores, const Field &field, const world::World *world);
 
    public:
     /**
-     * Get score of a position, used in getPosition
+     * @brief Get score of a position, used in getPosition
      * @param position Vector2 that needs to be scored
      * @param profile combination of weights for different factors that should be scored
-     * @param field
-     * @param world
+     * @param field The current field
+     * @param world The current world
      * @param bias value added to score
      * @return Position with score
      */

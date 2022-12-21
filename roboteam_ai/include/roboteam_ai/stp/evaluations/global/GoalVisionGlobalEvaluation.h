ca20ce9d2ebc547d9acfb9aa0272ffc99eacb6e3
JAndringa <andringajibbe@gmail.com>
2022-12-21 12:25:25 +0100

    Added documentation to GoalVisionGlobalEvaluation.h

--- roboteam_ai/include/roboteam_ai/stp/evaluations/global/GoalVisionGlobalEvaluation.h
+++ roboteam_ai/include/roboteam_ai/stp/evaluations/global/GoalVisionGlobalEvaluation.h
@@ -11,28 +11,38 @@
 
 namespace rtt::ai::stp::evaluation {
 /**
- * Calculates goal vision percentage based on the positions of all our robots
+ * @brief Class that evaluates goal vision
  */
 class GoalVisionGlobalEvaluation : public BaseEvaluation {
    public:
+    /**
+     * @brief Constructor for the GoalVisionGlobalEvaluation class
+     */
     GoalVisionGlobalEvaluation() noexcept;
 
+    /**
+     * @brief Calculates the score for goal vision
+     * @param world The current world
+     * @param field The current field
+     * @return The score of goal vision
+     */
     [[nodiscard]] uint8_t metricCheck(const world::World* world, const Field* field) const noexcept override;
 
+    /**
+     * @brief Retrieves the name of the evaluation
+     * @return A string containing the name of the evaluation
+     */
     const char* getName() override { return "GoalVisionGlobalEvaluation"; }
 
    private:
     /**
-     * Calculates the actual metric value using the piecewise linear function member
+     * @brief Calculates the actual metric value using the piecewise linear function member
      * @param x the x of the function
      * @return metric value between 0-255
      */
     [[nodiscard]] uint8_t calculateMetric(const double& x) const noexcept;
 
-    /**
-     * Unique pointer to the piecewise linear function that calculates the fuzzy value
-     */
-    std::unique_ptr<nativeformat::param::Param> piecewiseLinearFunction;
+    std::unique_ptr<nativeformat::param::Param> piecewiseLinearFunction; /**< Unique pointer to the piecewise linear function that calculates the fuzzy value */
 };
 }  // namespace rtt::ai::stp::evaluation
 

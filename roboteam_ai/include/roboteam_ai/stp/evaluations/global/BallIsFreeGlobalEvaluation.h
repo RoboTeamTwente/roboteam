0d60e10eb06b27e5819fdde3386b07b325b5ac7d
JAndringa <andringajibbe@gmail.com>
2022-12-21 12:11:09 +0100

    Added documentation to BallIsFreeGlobalEvaluation.h

--- roboteam_ai/include/roboteam_ai/stp/evaluations/global/BallIsFreeGlobalEvaluation.h
+++ roboteam_ai/include/roboteam_ai/stp/evaluations/global/BallIsFreeGlobalEvaluation.h
@@ -9,12 +9,22 @@
 
 namespace rtt::ai::stp::evaluation {
 /**
- * Check if the ball is currently not owned by any team
+ * @brief Class that evaluates ball is free
  */
 class BallIsFreeGlobalEvaluation : public BaseEvaluation {
    public:
+    /**
+     * @brief Calculates the score for ball is free
+     * @param world The current world
+     * @param field The current field
+     * @return The score of ball is free
+     */
     [[nodiscard]] uint8_t metricCheck(const world::World* world, const world::Field* field) const noexcept override;
 
+    /**
+     * @brief Retrieves the name of the evaluation
+     * @return A string containing the name of the evaluation
+     */
     const char* getName() override { return "BallIsFree"; }
 };
 }  // namespace rtt::ai::stp::evaluation

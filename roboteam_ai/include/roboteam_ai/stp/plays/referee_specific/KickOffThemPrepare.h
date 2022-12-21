245361b9b8ba948fb40f2df3392aeefa8e45ba0f
JAndringa <andringajibbe@gmail.com>
2022-12-21 15:47:02 +0100

    Added documentation to KickOffThemPrepare.h

--- roboteam_ai/include/roboteam_ai/stp/plays/referee_specific/KickOffThemPrepare.h
+++ roboteam_ai/include/roboteam_ai/stp/plays/referee_specific/KickOffThemPrepare.h
@@ -8,38 +8,42 @@
 #include "stp/Play.hpp"
 
 namespace rtt::ai::stp::play {
-
+/**
+ * @brief The kick off them prepare play is executed when the kick of them prepare game state is selected
+ */
 class KickOffThemPrepare : public Play {
    public:
     /**
-     * Constructor that initializes roles with roles that are necessary for this play
+     * @brief Constructor that initializes roles with roles that are necessary for this play
      */
     KickOffThemPrepare();
 
     /**
-     * Calculates the score of this play to determine which play is best in this situation
+     * @brief Calculates the score of this play to determine which play is best in this situation
      * @param field The current Field
      * @return The score of this play (0-255)
      */
     uint8_t score(const rtt::world::Field& field) noexcept override;
 
     /**
-     * Assigns robots to roles of this play
+     * @brief Assigns robots to roles of this play
+     * @return A map with assigned roles
      */
     Dealer::FlagMap decideRoleFlags() const noexcept override;
 
     /**
-     * Calculates info for the roles
+     * @brief Calculates info for the roles
      */
     void calculateInfoForRoles() noexcept override;
 
     /**
-     * Calculate info for the roles that need to be calculated for scoring
+     * @brief Calculate info for the roles that need to be calculated for scoring
      */
     void calculateInfoForScoredRoles(world::World*) noexcept override{};
 
     /**
-     * Gets the play name
+     * @brief Retrieves the name of the play
+     * @return The name of the play as a string
      */
     const char* getName() override;
 };

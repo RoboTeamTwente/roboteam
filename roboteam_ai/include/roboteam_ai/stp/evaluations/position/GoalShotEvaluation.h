3e6ddf2186f050ea9d0cd275b0066fa821e51ca2
JAndringa <andringajibbe@gmail.com>
2022-12-21 12:38:58 +0100

    Added documentation to GoalShotEvaluation.h

--- roboteam_ai/include/roboteam_ai/stp/evaluations/position/GoalShotEvaluation.h
+++ roboteam_ai/include/roboteam_ai/stp/evaluations/position/GoalShotEvaluation.h
@@ -8,11 +8,13 @@
 #include <cstdint>
 
 namespace rtt::ai::stp::evaluation {
-
+/**
+ * @brief Class that evaluates the Goal Shot score
+ */
 class GoalShotEvaluation {
    public:
     /**
-     * Score ability to make a goal from position based on how much of the goal is visible and the angular size of the goal
+     * @brief Score ability to make a goal from position based on how much of the goal is visible and the angular size of the goal
      * @param goalVisibility % visibility of the goal (taking in account other robots)
      * @param goalAngle angular size of the goal (relative to the goal angle from the penalty point)
      * @return uint8_t score

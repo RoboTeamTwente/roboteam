eaae6fe68215869ec08331e780954878773256d1
JAndringa <andringajibbe@gmail.com>
2022-12-21 12:28:44 +0100

    Added documentation to TheyDoNotHaveBallGlobalEvaluation.h

--- roboteam_ai/include/roboteam_ai/stp/evaluations/global/TheyDoNotHaveBallGlobalEvaluation.h
+++ roboteam_ai/include/roboteam_ai/stp/evaluations/global/TheyDoNotHaveBallGlobalEvaluation.h
@@ -8,10 +8,23 @@
 #include "stp/evaluations/BaseEvaluation.h"
 
 namespace rtt::ai::stp::evaluation {
+/**
+ * @brief Class that evaluates they do not have ball
+ */
 class TheyDoNotHaveBallGlobalEvaluation : public BaseEvaluation {
    public:
+    /**
+     * @brief Calculates the score for they do not have ball
+     * @param world The current world
+     * @param field The current field
+     * @return The score of they do not have ball
+     */
     [[nodiscard]] uint8_t metricCheck(const world::World* world, const world::Field* field) const noexcept override;
 
+    /**
+     * @brief Retrieves the name of the evaluation
+     * @return A string containing the name of the evaluation
+     */
     const char* getName() override { return "TheyDoNotHaveBall"; }
 };
 }  // namespace rtt::ai::stp::evaluation

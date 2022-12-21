d21a49a414b37207b9e8db902c423723392fed11
JAndringa <andringajibbe@gmail.com>
2022-12-21 16:00:21 +0100

    Added documentation to PenaltyUsPrepare.h

--- roboteam_ai/include/roboteam_ai/stp/plays/referee_specific/PenaltyUsPrepare.h
+++ roboteam_ai/include/roboteam_ai/stp/plays/referee_specific/PenaltyUsPrepare.h
@@ -8,38 +8,42 @@
 #include "stp/Play.hpp"
 
 namespace rtt::ai::stp::play {
-
+/**
+ * @brief The penalty us prepare play is executed when the penalty us prepare game state is selected
+ */
 class PenaltyUsPrepare : public Play {
    public:
     /**
-     * Constructor that initializes roles with roles that are necessary for this play
+     * @brief Constructor that initializes roles with roles that are necessary for this play
      */
     PenaltyUsPrepare();
 
     /**
-     * Calculates the score of this play to determine which play is best in this situation
+     * @brief Calculates the score of this play to determine which play is best in this situation
      * @param field The current Field
      * @return The score of this play (0-255)
      */
     uint8_t score(const rtt::world::Field& field) noexcept override;
 
     /**
-     * Assigns robots to roles of this play
+     * @brief Assigns robots to roles of this play
+     * @return A map with assigne roles
      */
     Dealer::FlagMap decideRoleFlags() const noexcept override;
 
     /**
-     * Calculates info for the roles
+     * @brief Calculates info for the roles
      */
     void calculateInfoForRoles() noexcept override;
 
     /**
-     * Info that should be calculated for scoring of the play
+     * @brief Info that should be calculated for scoring of the play
      */
     void calculateInfoForScoredRoles(world::World*) noexcept override{};
 
     /**
-     * Gets the play name
+     * @brief Retrieves the name of the play
+     * @return The name of the play as a string
      */
     const char* getName() override;
 };

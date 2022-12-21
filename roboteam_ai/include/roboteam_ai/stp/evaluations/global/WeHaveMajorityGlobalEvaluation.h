c43bed86c4b5880fbe688ae0a4573301d0a97672
JAndringa <andringajibbe@gmail.com>
2022-12-21 12:30:56 +0100

    Added documentation to WeHaveMajorityGlobalEvaluation.h

--- roboteam_ai/include/roboteam_ai/stp/evaluations/global/WeHaveMajorityGlobalEvaluation.h
+++ roboteam_ai/include/roboteam_ai/stp/evaluations/global/WeHaveMajorityGlobalEvaluation.h
@@ -8,10 +8,23 @@
 #include "stp/evaluations/BaseEvaluation.h"
 
 namespace rtt::ai::stp::evaluation {
+/**
+ * @brief Class that evaluates we have majority
+ */
 class WeHaveMajorityGlobalEvaluation : public BaseEvaluation {
    public:
+    /**
+     * @brief Calculates the score for we have majority
+     * @param world The current world
+     * @param field The current field
+     * @return The score of we have majority
+     */
     [[nodiscard]] uint8_t metricCheck(const world::World* world, const world::Field* field) const noexcept override;
 
+    /**
+     * @brief Retrieves the name of the evaluation
+     * @return A string containing the name of the evaluation
+     */
     const char* getName() override { return "WeHaveMajority"; }
 };
 }  // namespace rtt::ai::stp::evaluation

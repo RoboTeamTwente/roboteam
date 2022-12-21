dac99e5a45e251f45f7d47a89d170b3d9bd3e47b
JAndringa <andringajibbe@gmail.com>
2022-12-21 12:14:37 +0100

    Added documentation to BallNotInOurDefenseAreaAndStillGlobalEvaluation.h

--- roboteam_ai/include/roboteam_ai/stp/evaluations/global/BallNotInOurDefenseAreaAndStillGlobalEvaluation.h
+++ roboteam_ai/include/roboteam_ai/stp/evaluations/global/BallNotInOurDefenseAreaAndStillGlobalEvaluation.h
@@ -8,10 +8,23 @@
 #include "stp/evaluations/BaseEvaluation.h"
 
 namespace rtt::ai::stp::evaluation {
+/**
+ * @brief Class that evaluates ball not in our defense area and still
+ */
 class BallNotInOurDefenseAreaAndStillGlobalEvaluation : public BaseEvaluation {
    public:
+    /**
+     * @brief Calculates the score for ball not in our defense area and still
+     * @param world The current world
+     * @param field The current field
+     * @return The score of ball not in our defense area and still
+     */
     [[nodiscard]] uint8_t metricCheck(const world::World* world, const world::Field* field) const noexcept override;
 
+    /**
+     * @brief Retrieves the name of the evaluation
+     * @return A string containing the name of the evaluation
+     */
     const char* getName() override { return "BallNotInOurDefenseAreaAndStillGlobalEvaluation"; }
 };
 }  // namespace rtt::ai::stp::evaluation

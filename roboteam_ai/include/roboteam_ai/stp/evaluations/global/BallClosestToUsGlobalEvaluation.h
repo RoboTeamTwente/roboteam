d85ebc0b8a30c890323480f29373d9212cc2f1c9
JAndringa <andringajibbe@gmail.com>
2022-12-21 11:50:54 +0100

    Added documentation to BallClosestToUsGlobalEvaluation.h

--- roboteam_ai/include/roboteam_ai/stp/evaluations/global/BallClosestToUsGlobalEvaluation.h
+++ roboteam_ai/include/roboteam_ai/stp/evaluations/global/BallClosestToUsGlobalEvaluation.h
@@ -8,10 +8,23 @@
 #include "stp/evaluations/BaseEvaluation.h"
 
 namespace rtt::ai::stp::evaluation {
+/**
+ * @brief Class that evaluates ball closest to us
+ */
 class BallClosestToUsGlobalEvaluation : public BaseEvaluation {
    public:
+    /**
+     * @brief Calculates the score for ball closest to us
+     * @param world The current world
+     * @param field The current field
+     * @return The score of ball closest to us
+     */
     [[nodiscard]] uint8_t metricCheck(const world::World* world, const world::Field* field) const noexcept override;
 
+    /**
+     * @brief Retrieves the name of the evaluation
+     * @return A string containing the name of the evaluation
+     */
     const char* getName() override { return "BallClosestToUs"; }
 };
 }  // namespace rtt::ai::stp::evaluation

f800bc551f8fcc062d2d02aa2e0724ca7927c95d
JAndringa <andringajibbe@gmail.com>
2022-12-22 09:55:13 +0100

    Added documentation to Attacker.h

--- roboteam_ai/include/roboteam_ai/stp/roles/active/Attacker.h
+++ roboteam_ai/include/roboteam_ai/stp/roles/active/Attacker.h
@@ -8,12 +8,14 @@
 #include "stp/Role.hpp"
 
 namespace rtt::ai::stp::role {
-
+/**
+ * @brief Class that defines the attacker role. The attacker will try to shoot at the enemy goal.
+ */
 class Attacker : public Role {
    public:
     /**
-     * Ctor that sets the name of the role and creates a state machine of tactics
-     * @param name name of the role
+     * @brief Constructor that sets the name of the role and creates a state machine of tactics
+     * @param name The name of the role
      */
     Attacker(std::string name);
 };

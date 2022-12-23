ac62cbfc4d25efc0f46556af950f17fee451ba3e
JAndringa <andringajibbe@gmail.com>
2022-12-23 10:23:17 +0100

    Added documentation to Formation.h

--- roboteam_ai/include/roboteam_ai/stp/roles/passive/Formation.h
+++ roboteam_ai/include/roboteam_ai/stp/roles/passive/Formation.h
@@ -8,12 +8,14 @@
 #include "stp/Role.hpp"
 
 namespace rtt::ai::stp::role {
-
+/**
+ * @brief Class that defines the formation role. the formation will keep a given position until it is given a different role
+ */
 class Formation : public Role {
    public:
     /**
-     * Ctor that sets the name of the role and creates a state machine of tactics
-     * @param name name of the role
+     * @brief Constructor that sets the name of the role and creates a state machine of tactics
+     * @param name The name of the role
      */
     Formation(std::string name);
 };

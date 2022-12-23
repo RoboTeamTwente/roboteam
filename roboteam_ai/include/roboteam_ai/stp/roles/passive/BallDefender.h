93e365c784f7cfe9155a2347d7b8a92ef3deb453
JAndringa <andringajibbe@gmail.com>
2022-12-23 10:19:09 +0100

    Added documentation to BallDefender.h

--- roboteam_ai/include/roboteam_ai/stp/roles/passive/BallDefender.h
+++ roboteam_ai/include/roboteam_ai/stp/roles/passive/BallDefender.h
@@ -8,12 +8,14 @@
 #include "stp/Role.hpp"
 
 namespace rtt::ai::stp::role {
-
+/**
+ * @brief Class that defines the ball defender role. The ball defender will try to block the ball from begin shot to a given position.
+ */
 class BallDefender : public Role {
    public:
     /**
-     * Ctor that sets the name of the role and creates a state machine of tactics
-     * @param name name of the role
+     * @brief Constructor that sets the name of the role and creates a state machine of tactics
+     * @param name The name of the role
      */
     BallDefender(std::string name);
 };

a937a27f3f01a48003911b7ba7ae80178092d714
JAndringa <andringajibbe@gmail.com>
2023-01-18 15:16:40 +0100

    Added small script that starts the simulator and networking
    For now the main.py file is used for starting the simulator, sending the field data and then waiting for commands before updating the simulation.

--- roboteam_simulator/main.py
+++ roboteam_simulator/main.py
@@ -1,25 +1,14 @@
-import os
-
 from RTTSimEnv import RTTSimEnv
-from stable_baselines3 import A2C
-
-log_dir = "logs"
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
-
-models_dir = "models/A2C"
-
-if not os.path.exists(models_dir):
-    os.makedirs(models_dir)
+from networking import Publisher, Subscriber
+import threading
 
 env = RTTSimEnv()
-env.reset()
+pub = Publisher()
+sub = Subscriber()
 
-model = A2C('MlpPolicy', env, verbose=1, tensorboard_log=log_dir)
-
-TIME_STEPS = 100000
-iters = 0
-while True:
-    iters += 1
-    model.learn(total_timesteps=TIME_STEPS, reset_num_timesteps=False, tb_log_name=f"A2C_1")
-    model.save(f"{models_dir}/{TIME_STEPS * iters}")
+done = False
+while not done:
+    env.reset()
+    pub.send(env.field)
+    sub.receive()
+    obs, reward, done, info = env.step(env.action_space.sample())

576b58a9ecd48c0eb5705efbcad9d86cead40955
JAndringa <andringajibbe@gmail.com>
2023-01-23 15:47:00 +0100

    Added documentation to Rotate.h

--- roboteam_ai/include/roboteam_ai/stp/skills/Rotate.h
+++ roboteam_ai/include/roboteam_ai/stp/skills/Rotate.h
@@ -8,25 +8,25 @@
 #include "stp/Skill.h"
 
 namespace rtt::ai::stp::skill {
-
+/**
+ * @brief Class that defines the rotate skill. This is a very slow way of turning with the ball but is also very safe as we won't crash into things.
+ * This skill is useful when we don't have to be fast but we want to ensure that we can turn with the ball safely (for example during ball placement)
+ */
 class Rotate : public Skill {
     /**
-     * On update of this tactic
+     * @brief On update of this tactic
      * @param info StpInfo struct with all relevant info for this robot and this skill
      * @return A Status, either Running or Success
      */
     Status onUpdate(StpInfo const& info) noexcept override;
 
     /**
-     * Gets the skill name
+     * @brief Gets the skill name
      * @return The name of this skill
      */
     const char* getName() override;
 
-    /**
-     * Counts how long the robot is within the rotational error margin
-     */
-    int withinMarginCount = 0;
+    int withinMarginCount = 0; /**< Counts how long the robot is within the rotational error margin */
 };
 }  // namespace rtt::ai::stp::skill
 

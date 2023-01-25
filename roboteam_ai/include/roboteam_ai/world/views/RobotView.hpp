2cbde5e99f5dd251fa55daaf43bbacf614b500f9
JAndringa <andringajibbe@gmail.com>
2023-01-25 10:11:57 +0100

    Added documentation to RobotView.hpp

--- roboteam_ai/include/roboteam_ai/world/views/RobotView.hpp
+++ roboteam_ai/include/roboteam_ai/world/views/RobotView.hpp
@@ -13,67 +13,70 @@
 namespace rtt::world::view {
 
 /**
- * Class that offers a read-only memory view of the Robots
- *
+ * @brief Class that offers a read-only memory view of the Robots
  * Imagine RobotView being to Robot what string_view is to std::string,
  * except it provides more utility functions
  */
 class RobotView {
-    robot::Robot const *robotPtr;
+    robot::Robot const *robotPtr; /**< Pointer to a robot */
 
    public:
     /**
-     * Constructs a RobotView from a Robot
+     * @brief Constructs a RobotView from a Robot
      * @param _ptr Robot pointer to construct from
      * @ensures After contruction this->rbt == _ptr;
      */
     explicit RobotView(robot::Robot const *_ptr) noexcept;
 
     /**
+     * @brief Checks whether there is a robot in this robot view
      * @return get() != null;
      */
     explicit operator bool() const noexcept;
 
     /**
-     * Gets the internal pointer to the robot (std::unique_ptr style)
+     * @brief Gets the internal pointer to the robot (std::unique_ptr style)
      * @return Returns the internal pointer
      */
     [[nodiscard]] robot::Robot const *get() const noexcept;
 
     /**
-     * Overloads the dereference operator (*ptr) in std::unique_ptr style
+     * @brief Overloads the dereference operator (*ptr) in std::unique_ptr style
      * @return Returns a constant reference to the non-owned Robot
      */
     robot::Robot const &operator*() const noexcept;
 
     /**
-     * Gives std::unique_ptr (std::unique_ptr<Robot>()->...) style access into member functions
+     * @brief Gives std::unique_ptr (std::unique_ptr<Robot>()->...) style access into member functions
      * @return Returns get()
      */
     robot::Robot const *operator->() const noexcept;
 
     /**
-     * Copy assignment operator
+     * @brief Copy assignment operator
      * @return *this
      */
     RobotView &operator=(RobotView const &) = default;
 
     /**
-     * Move assignment operator
+     * @brief Move assignment operator
      * @return *this
      */
     RobotView &operator=(RobotView &&) = default;
 
     /**
-     * Copy constructor
+     * @brief Copy constructor of the RobotView class
      */
     RobotView(RobotView const &) = default;
 
     /**
-     * Move constructor, same as copy ctor
+     * @brief Move constructor, same as copy constructor
      */
     RobotView(RobotView &&) = default;
 
+    /**
+     * @brief Default destructor of the RobotView class
+     */
     ~RobotView() = default;
 };
 

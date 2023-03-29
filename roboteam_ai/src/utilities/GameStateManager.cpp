58abc8e3218df33b458ff073b2dda3c2d727b137
JAndringa <andringajibbe@gmail.com>
2023-03-29 14:14:51 +0200

    Fixed yellow robots moving during penalty_them.
    for some reason the penalty_them ref command used shootout_them as game state.

--- roboteam_ai/src/utilities/GameStateManager.cpp
+++ roboteam_ai/src/utilities/GameStateManager.cpp
@@ -45,7 +45,7 @@ void GameStateManager::setRefereeData(proto::SSL_Referee refMsg, const rtt::worl
                 cmd = RefCommand::PREPARE_PENALTY_US;
                 break;
             case proto::SSL_Referee_Command_PREPARE_PENALTY_BLUE:
-                cmd = RefCommand::PREPARE_SHOOTOUT_THEM;
+                cmd = RefCommand::PREPARE_PENALTY_THEM;
                 break;
             case proto::SSL_Referee_Command_DIRECT_FREE_YELLOW:
                 cmd = RefCommand::DIRECT_FREE_US;

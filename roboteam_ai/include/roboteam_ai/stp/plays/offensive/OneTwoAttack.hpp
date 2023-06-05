ab716ac01aaaf53f5cf0e12df7d0043512f09c36
JAndringa <andringajibbe@gmail.com>
2023-06-05 15:32:01 +0200

    add new play for better attacking

---
+++ roboteam_ai/include/roboteam_ai/stp/plays/offensive/OneTwoAttack.hpp
@@ -0,0 +1,59 @@
+//
+// Created by jibbe on 5-6-23.
+//
+
+#ifndef RTT_ONETWOATTACK_HPP
+#define RTT_ONETWOATTACK_HPP
+
+#include "stp/Play.hpp"
+
+namespace rtt::ai::stp::play {
+
+class OneTwoAttack : public Play {
+   public:
+    /**
+     * Constructor that initializes roles with roles that are necessary for this play
+     */
+    OneTwoAttack();
+
+    /**
+     * Calculates the score of this play to determine which play is best in this situation
+     * @param field The current Field
+     * @return The score of this play (0-255)
+     */
+    uint8_t score(const rtt::Field &field) noexcept override;
+
+    /**
+     * Assigns robots to roles of this play
+     */
+    Dealer::FlagMap decideRoleFlags() const noexcept override;
+
+    /**
+     * Calculates info for the roles
+     */
+    void calculateInfoForRoles() noexcept override;
+
+    /**
+     * Calculate info for the roles that need to be calculated for scoring
+     */
+    void calculateInfoForScoredRoles(world::World *) noexcept override{};
+
+    /**
+     * Gets the play name
+     */
+    const char *getName() override;
+
+    /**
+     * Optional function to save information for the next play
+     * @param info Map-Struct to save info in
+     */
+    void storePlayInfo(gen::PlayInfos &info) noexcept override;
+
+    /**
+     * Optional function to force end a play
+     * @return is play should end
+     */
+    bool shouldEndPlay() noexcept override;
+};
+}  // namespace rtt::ai::stp::play
+#endif  // RTT_ONETWOATTACK_HPP
---
+++ roboteam_ai/src/stp/plays/offensive/OneTwoAttack.cpp
@@ -0,0 +1,4 @@
+//
+// Created by jibbe on 5-6-23.
+//
+#include "stp/plays/offensive/OneTwoAttack.hpp"

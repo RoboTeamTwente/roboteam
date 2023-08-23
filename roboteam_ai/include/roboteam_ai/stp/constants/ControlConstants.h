eac4224c01f7cfcf355a1166f4c578e1670849c7
Emiel Steerneman <emielsteerneman@gmail.com>
2023-08-24 00:09:22 +0200

    Moved control constants to header file

--- roboteam_ai/include/roboteam_ai/stp/constants/ControlConstants.h
+++ roboteam_ai/include/roboteam_ai/stp/constants/ControlConstants.h
@@ -1,56 +1,77 @@
-/**
- * @file ControlConstants.h
- * @author jessevw
- * @brief All constants used for robot control within AI
- */
 #ifndef RTT_CONTROLCONSTANTS_H
 #define RTT_CONTROLCONSTANTS_H
+
+#include <math.h>
 
 #include <cstdint>
 
 namespace rtt::ai::stp::control_constants {
 
-extern const double MAX_KICK_POWER;                 /**< Maximum allowed kicking power */
-extern const double MIN_KICK_POWER;                 /**< Minimum allowed kicking power */
-extern const double MAX_CHIP_POWER;                 /**< Maximum allowed chipping power */
-extern const double MIN_CHIP_POWER;                 /**< Minimum allowed chipping power */
-extern const double MAX_POWER_KICK_DISTANCE;        /**< Distance at which the kicking power should be at maximum */
-extern const double MAX_POWER_CHIP_DISTANCE;        /**< Distance at which the chipping power should be at maximum */
-extern const double MAX_KICK_ATTEMPTS;              /**< Maximum allowed kicking attempts */
-extern const double MAX_CHIP_ATTEMPTS;              /**< Maximum allowed chipping attempts */
-extern const double ROBOT_RADIUS;                   /**< Radius of a robot */
-extern const double CENTER_TO_FRONT;                /**< Distance from center of the robot to the front of the robot */
-extern const double TURN_ON_DRIBBLER_DISTANCE;      /**< Distance from a robot to the ball at which the dribbler should turn on */
-inline constexpr uint8_t MAX_ROBOT_COUNT = 11;      /**< Maximum allowed number of robots */
-extern const double BALL_STILL_VEL;                 /**< Velocity of the ball at which it is considered still */
-extern const double BALL_STILL_VEL2;                /**< Squared velocity of the ball at which it is considered still */
-extern const double BALL_GOT_SHOT_LIMIT;            /**< Velocity of the ball at which it is considered shot */
-extern const double BALL_IS_MOVING_SLOW_LIMIT;      /**< Velocity of the ball at which it is considered moving slow */
-extern const double BALL_IS_CLOSE;                  /**< Distance from the ball to a robot at which the ball is considered close */
-extern const double BALL_RADIUS;                    /**< Radius of the ball */
-extern const double HAS_KICKED_ERROR_MARGIN;        /**< Velocity margin for detecting ball kicks */
-extern const double HAS_CHIPPED_ERROR_MARGIN;       /**< Velocity margin for detecting ball chips */
-extern const double ENEMY_CLOSE_TO_BALL_DISTANCE;   /**< Distance from the ball to an enemy robot at which the ball is considered close to the enemy */
-extern const double MAX_VEL_CMD;                    /**< Maximum allowed velocity that can be send to the robot */
-extern const double MAX_DRIBBLER_CMD;               /**< Maximum allowed dribbler velocity that can be send to the robot */
-extern const double ANGLE_RATE;                     /**< Maximum allowed angular velocity that can be send to the robot */
-extern const double MAX_VEL_WHEN_HAS_BALL;          /**< Maximum allowed velocity that can be send to the robot when that robot has the ball */
-extern const double HAS_BALL_ANGLE_ERROR_MARGIN;    /**< Angle error for detecting whether a robot has the ball */
-extern const double HAS_BALL_DISTANCE_ERROR_MARGIN; /**< Distance error for detecting whether a robot has the ball */
-extern const double GO_TO_POS_ERROR_MARGIN;         /**< Distance error for a robot to be considered to have reached a position */
-extern const double GO_TO_POS_ANGLE_ERROR_MARGIN;   /**< Angle error for a robot to be considered to have reached a position */
-extern const double BALL_PLACEMENT_MARGIN;          /**< Distance error for the ball to be considered to have reached the ball placement position*/
-extern const uint8_t FUZZY_TRUE;                    /**< Value at which the fuzzy logic is considered 100% true */
-extern const uint8_t FUZZY_FALSE;                   /**< Value at which the fuzzy logic is considered 0% true */
-extern const double FUZZY_MARGIN;                   /**< Error margin of the fuzzy logic */
-extern const double FUZZY_DEFAULT_CUTOFF;           /**< Value at which the fuzzy logic is considered 50% true */
-extern const double DISTANCE_TO_ROBOT_CLOSE;        /**< Distance from the robot to another robot at which the robot is considered close to that other robot */
-extern const double DISTANCE_TO_ROBOT_FAR;          /**< Distance from the robot to another robot at which the robot is considered far from that other robot */
-extern const double ROBOT_CLOSE_TO_POINT;           /**< Distance from the robot to a position at which the robot is considered close to that position */
-extern const double DISTANCE_TO_ROBOT_NEAR;         /**< Distance from the robot to another robot at which the robot is considered near that other robot */
-extern const double DEFENSE_AREA_AVOIDANCE_MARGIN;  /**< Distance error for avoiding the defense area */
-extern const double DISTANCE_FROM_GOAL_CLOSE;       /**< Distance from the keeper to the goal at which the keeper is considered close to that goal */
-extern const double AVOID_BALL_DISTANCE;            /**< Minimum distance all robots should keep when avoiding the ball */
+// Kick and chip constants
+constexpr double MAX_KICK_POWER = 6.5;        /**< Maximum allowed kicking power */
+constexpr double MIN_KICK_POWER = 3.0;        /**< Minimum allowed kicking power */
+constexpr double MAX_CHIP_POWER = 6.5;        /**< Maximum allowed chipping power */
+constexpr double MIN_CHIP_POWER = 4.5;        /**< Minimum allowed chipping power */
+constexpr double MAX_POWER_KICK_DISTANCE = 8; /**< Distance at which the kicking power should be at maximum */
+constexpr double MAX_POWER_CHIP_DISTANCE = 9; /**< Distance at which the chipping power should be at maximum */
+// Max attempts before the force_kick_chip is set to true
+constexpr double MAX_KICK_ATTEMPTS = 25; /**< Maximum allowed kicking attempts */
+constexpr double MAX_CHIP_ATTEMPTS = 25; /**< Maximum allowed chipping attempts */
+
+/// Robot physical constants
+constexpr double ROBOT_RADIUS = 0.088;   /**< Radius of a robot */
+constexpr double CENTER_TO_FRONT = 0.05; /**< Distance from center of the robot to the front of the robot */
+
+/// Dribbler constants
+// The distance from robot to ball at which the dribbler should turn on
+constexpr double TURN_ON_DRIBBLER_DISTANCE = 5 * ROBOT_RADIUS; /**< Distance from a robot to the ball at which the dribbler should turn on */
+
+constexpr uint8_t MAX_ROBOT_COUNT = 11; /**< Maximum allowed number of robots */
+
+/// Ball constants
+constexpr double BALL_STILL_VEL = 0.1;                              /**< Velocity of the ball at which it is considered still */
+constexpr double BALL_STILL_VEL2 = BALL_STILL_VEL * BALL_STILL_VEL; /**< Squared velocity of the ball at which it is considered still */
+constexpr double BALL_GOT_SHOT_LIMIT = 1.3;                         /**< Velocity of the ball at which it is considered shot */
+constexpr double BALL_IS_MOVING_SLOW_LIMIT = 1;                     /**< Velocity of the ball at which it is considered moving slow */
+constexpr double BALL_IS_CLOSE = 0.5;                               /**< Distance from the ball to a robot at which the ball is considered close */
+constexpr double BALL_RADIUS = 0.0215;                              /**< Radius of the ball */
+constexpr double HAS_KICKED_ERROR_MARGIN = 1;                       /**< Velocity margin for detecting ball kicks */
+constexpr double HAS_CHIPPED_ERROR_MARGIN = 0.4;                    /**< Velocity margin for detecting ball chips */
+constexpr double ENEMY_CLOSE_TO_BALL_DISTANCE = 1.0;                /**< Distance from the ball to an enemy robot at which the ball is considered close to the enemy */
+
+/// RobotCommand limits
+constexpr double MAX_VEL_CMD = 8;      /**< Maximum allowed velocity that can be send to the robot */
+constexpr double MAX_DRIBBLER_CMD = 1; /**< Maximum allowed dribbler velocity that can be send to the robot */
+// Angle increment per tick
+constexpr double ANGLE_RATE = 0.1 * M_PI;     /**< Maximum allowed angular velocity that can be send to the robot */
+constexpr double MAX_VEL_WHEN_HAS_BALL = 3.0; /**< Maximum allowed velocity that can be send to the robot when that robot has the ball */
+
+/// GoToPos Constants
+// Distance margin for 'goToPos'. If the robot is within this margin, goToPos is successful
+constexpr double GO_TO_POS_ERROR_MARGIN = 0.06; /**< Distance error for a robot to be considered to have reached a position */
+// Angle margin for 'goToPos'. If the robot is within this margin, goToPos is successful
+constexpr double GO_TO_POS_ANGLE_ERROR_MARGIN = 0.04; /**< Angle error for a robot to be considered to have reached a position */
+// Maximum inaccuracy during ballplacement
+constexpr double BALL_PLACEMENT_MARGIN = 0.15; /**< Distance error for the ball to be considered to have reached the ball placement position*/
+
+/// Invariant constants
+constexpr uint8_t FUZZY_TRUE = 255;          /**< Value at which the fuzzy logic is considered 100% true */
+constexpr uint8_t FUZZY_FALSE = 0;           /**< Value at which the fuzzy logic is considered 0% true */
+constexpr double FUZZY_MARGIN = 0.1;         /**< Error margin of the fuzzy logic */
+constexpr double FUZZY_DEFAULT_CUTOFF = 127; /**< Value at which the fuzzy logic is considered 50% true */
+
+/// Distance constants
+constexpr double DISTANCE_TO_ROBOT_CLOSE = ROBOT_RADIUS;      /**< Distance from the robot to another robot at which the robot is considered close to that other robot */
+constexpr double DISTANCE_TO_ROBOT_FAR = 5 * ROBOT_RADIUS;    /**< Distance from the robot to another robot at which the robot is considered far from that other robot */
+constexpr double ROBOT_CLOSE_TO_POINT = 0.2;                  /**< Distance from the robot to a position at which the robot is considered close to that position */
+constexpr double DISTANCE_TO_ROBOT_NEAR = 2.2 * ROBOT_RADIUS; /**< Distance from the robot to another robot at which the robot is considered near that other robot */
+constexpr double DEFENSE_AREA_AVOIDANCE_MARGIN = 0.1;         /**< Distance error for avoiding the defense area */
+
+/// Keeper constants
+constexpr double DISTANCE_FROM_GOAL_CLOSE = 2 * ROBOT_RADIUS; /**< Distance from the keeper to the goal at which the keeper is considered close to that goal */
+
+/// GameState constants
+constexpr double AVOID_BALL_DISTANCE = 0.8; /**< Minimum distance all robots should keep when avoiding the ball */
 
 }  // namespace rtt::ai::stp::control_constants
 

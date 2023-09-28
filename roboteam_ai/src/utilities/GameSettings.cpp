1ce390254eedf35369d4a7e5e026678f25a33aa9
emiel steerneman <emielsteerneman@gmail.com>
2023-09-28 23:26:54 +0200

    ran format.sh

--- roboteam_ai/src/utilities/GameSettings.cpp
+++ roboteam_ai/src/utilities/GameSettings.cpp
@@ -1,10 +1,11 @@
+#include "utilities/GameSettings.h"
+
 #include <roboteam_utils/Print.h>
 #include <utilities/IOManager.h>
 
 #include <string_view>
 
 #include "RobotHubMode.h"
-#include "utilities/GameSettings.h"
 
 namespace rtt {
 
@@ -14,7 +15,7 @@ std::atomic<bool> GameSettings::yellow = false;
 std::atomic<bool> GameSettings::left = false;
 std::atomic<net::RobotHubMode> GameSettings::robothub_mode = net::RobotHubMode::SIMULATOR;
 
-bool GameSettings::hasChanges(){
+bool GameSettings::hasChanges() {
     bool has_changes_copy = has_changes;
     has_changes = false;
     return has_changes_copy;
@@ -28,8 +29,8 @@ void GameSettings::copyFrom(const proto::GameSettings& settings) {
 
 bool GameSettings::isPrimaryAI() { return primary_AI; }
 
-void GameSettings::setPrimaryAI(bool is_primary_AI) { 
-    if(primary_AI != is_primary_AI){
+void GameSettings::setPrimaryAI(bool is_primary_AI) {
+    if (primary_AI != is_primary_AI) {
         has_changes = true;
         primary_AI = is_primary_AI;
         RTT_INFO("Setting primary AI to ", primary_AI ? "true" : "false");
@@ -57,7 +58,7 @@ bool GameSettings::setYellow(bool is_yellow) {
 bool GameSettings::isLeft() { return left; }
 
 void GameSettings::setLeft(bool is_left) {
-    if(left != is_left){
+    if (left != is_left) {
         has_changes = true;
         left = is_left;
         RTT_INFO("Setting left to ", left ? "left" : "right");
@@ -67,7 +68,7 @@ void GameSettings::setLeft(bool is_left) {
 net::RobotHubMode GameSettings::getRobotHubMode() { return robothub_mode; }
 
 void GameSettings::setRobotHubMode(net::RobotHubMode mode) {
-    if(robothub_mode != mode){
+    if (robothub_mode != mode) {
         has_changes = true;
         robothub_mode = mode;
         RTT_INFO("Setting robot hub mode to ", robotHubModeToString(mode));

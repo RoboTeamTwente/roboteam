5e7c5e0dabe8f7a7a77022990f013bd3c82f1aa9
Jorn <j.dejong@roboteamtwente.nl>
2023-11-08 09:41:50 +0100

    Add option to set position for BlockBall tactic

--- roboteam_ai/src/stp/roles/passive/BallAvoider.cpp
+++ roboteam_ai/src/stp/roles/passive/BallAvoider.cpp
@@ -5,12 +5,11 @@
 #include "stp/roles/passive/BallAvoider.h"
 
 #include "stp/tactics/passive/AvoidBall.h"
-#include "stp/tactics/passive/Formation.h"
 
 namespace rtt::ai::stp::role {
 
 BallAvoider::BallAvoider(std::string name) : Role(std::move(name)) {
     // create state machine and initializes the first state
-    robotTactics = collections::state_machine<Tactic, Status, StpInfo>{tactic::AvoidBall(), tactic::Formation()};
+    robotTactics = collections::state_machine<Tactic, Status, StpInfo>{tactic::AvoidBall()};
 }
 }  // namespace rtt::ai::stp::role

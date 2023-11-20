34a516830252097144b788dad7984eb336f89295
Simone Tollardo <simonetollardo@gmail.com>
2023-11-20 22:02:09 +0100

    Feature: catch errors on 'build.sh'

--- build.sh
+++ build.sh
@@ -1,6 +1,19 @@
 #! /bin/bash
 
-set -e
+# Exit on errors
+# Note: do not concat commands with && otheriwise won't be catched
+set -eo pipefail
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+RESET='\033[0m'
+
+error() {
+    echo -e "${RED}Fatal error on line $1${RESET}" >&2
+}
+
+trap 'error $LINENO' ERR
 
 if [ "$1" == "-y" ];
 then
@@ -13,40 +26,45 @@ if [ -f /.dockerenv ] || [ "$OK" = "1" ];
 then
     if [ "$0" == "./build.sh" ];
     then
-        echo "Checking submodules"
+        echo -e "${GREEN}Checking submodules${RESET}"
         git submodule update --init --recursive
         echo "Done"
         #rm -rf build
         mkdir -p build
         pushd build
-            echo "Building RoboTeamTwente software"
-            cmake .. -DCMAKE_BUILD_TYPE=Release && make -j$(nproc) roboteam_observer roboteam_ai roboteam_robothub
+            echo -e "${GREEN}Building RoboTeamTwente software${RESET}"
+            cmake .. -DCMAKE_BUILD_TYPE=Release
+            make -j$(nproc) roboteam_observer roboteam_ai roboteam_robothub
         popd
-            echo "Installing interface"
+            echo -e "${GREEN}Installing interface${RESET}"
         pushd roboteam_interface
             yarn install
         popd
         pushd external
-            echo "Building external"
+            echo -e "${GREEN}Building external${RESET}"
             pushd framework
-                echo "Building external/framework"
-                mkdir -p build && pushd build
-                cmake .. && make simulator-cli -j$(nproc)
+                echo -e "${GREEN}Building external/framework${RESET}"
+                mkdir -p build
+                pushd build
+                cmake ..
+                make simulator-cli -j$(nproc)
                 popd
             popd
             pushd autoref
-                echo "Building external/autoref"
-                mkdir -p build && pushd build
-                cmake .. && make autoref-cli -j$(nproc)
+                echo -e "${GREEN}Building external/autoref${RESET}"
+                mkdir -p build
+                pushd build
+                cmake ..
+                make autoref-cli -j$(nproc)
                 popd
             popd
         popd
-        echo "Done, exiting builder.."
+        echo -e "${GREEN}Done, exiting builder..${RESET}"
     else
-        echo "E: The script must be called from the root folder"
+        echo -e "${RED}E: The script must be called from the root folder${RESET}"
         exit 1
     fi
 else
-    echo "W: If you really want to build outside the container call the script with -y argument."
+    echo -e "${ORANGE}W: If you really want to build outside the container call the script with -y argument.${RESET}"
     exit 1
 fi

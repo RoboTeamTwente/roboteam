65a7cf92edc735a0e53ad53157b80495c34c7d29
JJGlezTorres <jj.gonzaleztorres@ibertrans.es>
2023-11-29 11:28:36 +0100

    Ratio added

--- roboteam_interface/src/modules/components/panels/performance-widget.vue
+++ roboteam_interface/src/modules/components/panels/performance-widget.vue
@@ -7,15 +7,22 @@ const stpData = useSTPDataStore()
 </script>
 
 <template>
-    Performance
-    <div>
-      <font-awesome-icon icon="fa-triangle-exclamation" />
+    <p style="font-size: 20px; font-weight: bold;">Ratio Offensive/Defensive</p>
+    <div style="margin-top: 10px;">
       Current play: 
       {{stpData.latest?.currentPlay?.playName}}
     </div>
     <div>
-      <font-awesome-icon icon="fa-triangle-exclamation" />
-      {{possession}} %
+      Ratio:
+      {{possession.toFixed(2)}} %
+    </div>
+    <div>
+      Offensive counter:
+      {{offensive_counter}}
+    </div>
+    <div>
+      Defensive counter:
+      {{defensive_counter}}
     </div>
 </template>
 
@@ -26,45 +33,49 @@ import { defineComponent, ref, watch } from 'vue';
 export default defineComponent({
   data() {
     return {
-      stpData: {
-        latest: {
-          currentPlay: {
-            playName: 'Halt' // Supongamos que es A al inicio
-            // Puedes inicializar playName con cualquier valor A, B, C o D
-          }
-        }
-      },
+      stpData: useSTPDataStore(),
       offensive_plays: ['Attack', 'AttackingPass', 'KeeperKickBall'],
-      defensive_plays: ['DefendShot', 'DefendPass', 'DefensiveStopFormation', 'AggressiveStopFormation'],
-      neutral_plays: ['Halt', 'BallPlacementUs', 'BallPlacementThem', 'PenaltyThemPrepare', 'PenaltyUsPrepare', 'PenaltyThem', 
+      defensive_plays: ['Defend Shot', 'Defend Pass', 'Keeper Kick Ball'],
+      neutral_plays: ['Halt', 'BallPlacementUs', 'BallPlacementThem', 'PenaltyThemPrepare', 'PenaltyUsPrepare', 'PenaltyThem', 'DefensiveStopFormation', 'AggressiveStopFormation',
       'PenaltyUs', 'KickOffUsPrepare', 'KickOffThemPrepare', 'FreeKickThem', 'FreeKickUsAtGoal', 'FreeKickUsPass', 'KickOffUs', 'KickOffThem'],
       offensive_counter: 0,
       defensive_counter: 0, 
-      nuetral_counter: 0,
-      possession: 0
+      neutral_counter: 0,
+      possession: 0, 
+      last_time: 0, 
+      interval_time: 0
     };
   },
   methods: {
     increment_counters() {
-      const playName = this.stpData.latest.currentPlay.playName;
+
+      const playName = this.stpData.latest?.currentPlay?.playName;
       
-      if (this.offensive_plays.includes(playName)) {
-        this.offensive_counter++;
-      } else if (this.defensive_plays.includes(playName)) {
+      if (this.defensive_plays.includes(String(playName))) {
         this.defensive_counter++;
-      } else if (this.neutral_plays.includes(playName)) {
-        this.nuetral_counter++;
+      } else if (this.offensive_plays.includes(String(playName))) {
+        this.offensive_counter++;
+      } else if (this.neutral_plays.includes(String(playName))) {
+        this.neutral_counter++;
       } 
 
       this.possession = this.offensive_counter / (this.offensive_counter + this.defensive_counter) * 100
+
     }
+
   },
   mounted() {
     this.increment_counters();
   },
   watch: {
-    'stpData.latest.currentPlay.playName'() { // When a change is produced in this variable, execute increment_counters
+    'stpData.latest.currentPlay.playName'() { 
       this.increment_counters();
+      if (this.last_time === 0) {
+        this.last_time = Date.now();
+      } else {
+        this.interval_time = Date.now() - this.last_time;
+        this.last_time = Date.now();
+      }
     }
   }
 });

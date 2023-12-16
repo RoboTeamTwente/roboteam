6e066601c56fb7d710d3efb05d17ffb8fd8c679c
Jorn <j.dejong@roboteamtwente.nl>
2023-12-16 19:28:59 +0100

    Update format.sh script to exclude submodules (autoref+REM)

--- format.sh
+++ format.sh
@@ -1,1 +1,1 @@
-find -L | grep -E "^./roboteam.*?\.(c|cpp|h|hpp)$" --color=never | xargs clang-format -i -style="{BasedOnStyle : Google, IndentWidth : 4, ColumnLimit : 180}"
+find -L . -not -path "./roboteam_autoref/*" -not -path "./roboteam/roboteam_robothub/roboteam_embedded_messages/*" | grep -E "^./roboteam.*?\.(c|cpp|h|hpp)$" --color=never | xargs clang-format -i -style="{BasedOnStyle : Google, IndentWidth : 4, ColumnLimit : 180}"

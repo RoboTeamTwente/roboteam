0f5979468e93a66c029acf78eef09783a5168d83
Jorn <j.dejong@roboteamtwente.nl>
2024-04-30 19:10:47 +0200

    Fix 'Robot kicks when in stop state and ball is placed close to him by human ref

--- roboteam_ai/src/stp/roles/PenaltyKeeper.cpp
+++ roboteam_ai/src/stp/roles/PenaltyKeeper.cpp
@@ -4,7 +4,6 @@
 
 #include "stp/tactics/KeeperBlockBall.h"
 #include "stp/tactics/active/GetBall.h"
-#include "stp/tactics/active/KickAtPos.h"
 #include "stp/tactics/passive/Formation.h"
 #include "world/FieldComputations.h"
 

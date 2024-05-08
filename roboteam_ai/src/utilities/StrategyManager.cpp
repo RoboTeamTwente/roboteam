f675cc9da5d81f14eb62bd265294dbeffc43eb67
Luuk <l.winters@roboteamtwente.nl>
2024-05-08 15:32:33 +0200

    Fix Defender to close to kickpoint foul

--- roboteam_ai/src/utilities/StrategyManager.cpp
+++ roboteam_ai/src/utilities/StrategyManager.cpp
@@ -10,7 +10,7 @@ void StrategyManager::setCurrentGameState(RefCommand command, RefCommand nextCom
         lastCommand = command;
         return;
     }
-
+    
     if (command == RefCommand::STOP && (nextCommand == RefCommand::PREPARE_KICKOFF_THEM || nextCommand == RefCommand::PREPARE_KICKOFF_US ||
                                         nextCommand == RefCommand::PREPARE_PENALTY_THEM || nextCommand == RefCommand::PREPARE_PENALTY_US)) {
         command = nextCommand;
@@ -35,7 +35,7 @@ void StrategyManager::setCurrentGameState(RefCommand command, RefCommand nextCom
 
     if ((currentGameState.commandId == RefCommand::DIRECT_FREE_THEM || currentGameState.commandId == RefCommand::DIRECT_FREE_US ||
          currentGameState.commandId == RefCommand::KICKOFF_US || currentGameState.commandId == RefCommand::KICKOFF_THEM) &&
-        ballOpt.has_value() && (ballOpt.value()->velocity.length() > stp::control_constants::BALL_STILL_VEL || currentGameState.timeLeft < 0)) {
+        ballOpt.has_value() && (ballOpt.value()->velocity.length() > stp::control_constants::BALL_GOT_SHOT_LIMIT || currentGameState.timeLeft < -0.5)) {
         currentGameState = getGameStateForRefCommand(RefCommand::NORMAL_START);
         lastCommand = command;
         return;

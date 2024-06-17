1fd0adeeccd1519e4783e481b8bb5e32c52f877b
Luuk <l.winters@roboteamtwente.nl>
2024-06-17 11:22:15 +0200

    More defenders, less attackers

--- roboteam_ai/src/stp/plays/offensive/Attack.cpp
+++ roboteam_ai/src/stp/plays/offensive/Attack.cpp
@@ -32,13 +32,13 @@ Attack::Attack() : Play() {
         std::make_unique<role::Defender>("defender_0"),
         std::make_unique<role::Formation>("attacker_0"),
         std::make_unique<role::Defender>("defender_1"),
-        std::make_unique<role::Formation>("attacker_1"),
+        std::make_unique<role::Defender>("defender_2"),
         // Additional roles if we play 11v11
         std::make_unique<role::Formation>("waller_0"),
         std::make_unique<role::Formation>("waller_1"),
+        std::make_unique<role::Formation>("attacker_1"),
+        std::make_unique<role::Defender>("defender_3"),
         std::make_unique<role::Formation>("attacker_2"),
-        std::make_unique<role::Defender>("defender_2"),
-        std::make_unique<role::Formation>("attacker_3"),
     };
 }
 
@@ -60,10 +60,10 @@ Dealer::FlagMap Attack::decideRoleFlags() const noexcept {
     flagMap.insert({"defender_0", {DealerFlagPriority::MEDIUM_PRIORITY, {}}});
     flagMap.insert({"defender_1", {DealerFlagPriority::MEDIUM_PRIORITY, {}}});
     flagMap.insert({"defender_2", {DealerFlagPriority::MEDIUM_PRIORITY, {}}});
+    flagMap.insert({"defender_3", {DealerFlagPriority::MEDIUM_PRIORITY, {}}});
     flagMap.insert({"attacker_0", {DealerFlagPriority::LOW_PRIORITY, {}}});
     flagMap.insert({"attacker_1", {DealerFlagPriority::LOW_PRIORITY, {}}});
     flagMap.insert({"attacker_2", {DealerFlagPriority::LOW_PRIORITY, {}}});
-    flagMap.insert({"attacker_3", {DealerFlagPriority::LOW_PRIORITY, {}}});
 
     return flagMap;
 }

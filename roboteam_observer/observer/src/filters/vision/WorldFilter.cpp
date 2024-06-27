202cb4f4c640c50b970e1d4942b05821498453cf
Jorn <j.dejong@roboteamtwente.nl>
2024-06-27 09:34:20 +0200

    Avoid copying balls

--- roboteam_observer/observer/src/filters/vision/WorldFilter.cpp
+++ roboteam_observer/observer/src/filters/vision/WorldFilter.cpp
@@ -210,13 +210,18 @@ void WorldFilter::processBalls(const DetectionFrame &frame) {
     }
 }
 void WorldFilter::addBallPredictionsToMessage(proto::World &world, Time time) const {
-    std::vector<BallFilter> filteredBalls;
-    std::copy_if(balls.begin(), balls.end(), std::back_inserter(filteredBalls), [](const BallFilter &filter) { return filter.getNumObservations() > 3; });
-
-    if (!filteredBalls.empty()) {
-        auto bestFilter =
-            std::max_element(filteredBalls.begin(), filteredBalls.end(), [](const BallFilter &best, const BallFilter &filter) { return best.getHealth() < filter.getHealth(); });
-
+    const BallFilter *bestFilter = nullptr;
+    double bestHealth = -1.0;
+    for (const auto &filter : balls) {
+        if (filter.getNumObservations() > 3) {
+            double currentHealth = filter.getHealth();
+            if (!bestFilter || currentHealth > bestHealth) {
+                bestFilter = &filter;
+                bestHealth = currentHealth;
+            }
+        }
+    }
+    if (bestFilter) {
         FilteredBall bestBall = bestFilter->mergeBalls(time);
         world.mutable_ball()->CopyFrom(bestBall.asWorldBall());
     }

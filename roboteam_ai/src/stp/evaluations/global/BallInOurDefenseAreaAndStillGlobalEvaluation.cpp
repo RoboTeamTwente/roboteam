4ea31b993df7866821cb73874ff836d79106139a
Luuk <l.winters@roboteamtwente.nl>
2024-07-01 10:39:45 +0200

    Let keeper drive towards the goal with the ball if near the edge of the defense area, to prevent kicking against a robot

--- roboteam_ai/src/stp/evaluations/global/BallInOurDefenseAreaAndStillGlobalEvaluation.cpp
+++ roboteam_ai/src/stp/evaluations/global/BallInOurDefenseAreaAndStillGlobalEvaluation.cpp
@@ -5,8 +5,10 @@
 
 namespace rtt::ai::stp::evaluation {
 uint8_t BallInOurDefenseAreaAndStillGlobalEvaluation::metricCheck(const world::World* world, const Field* field) const noexcept {
-    return (field->leftDefenseArea.contains(world->getWorld()->getBall()->get()->position) &&
-            field->leftDefenseArea.contains(world->getWorld()->getBall()->get()->expectedEndPosition))
+    double ballRadius = constants::BALL_RADIUS / 2;
+    auto ourDefenseArea = FieldComputations::getDefenseArea(*field, true, ballRadius, 0);
+    return (ourDefenseArea.contains(world->getWorld()->getBall()->get()->position) &&
+            ourDefenseArea.contains(world->getWorld()->getBall()->get()->expectedEndPosition))
                ? constants::FUZZY_TRUE
                : constants::FUZZY_FALSE;
 }
--- roboteam_ai/src/stp/roles/active/KeeperPasser.cpp
+++ roboteam_ai/src/stp/roles/active/KeeperPasser.cpp
@@ -4,10 +4,56 @@
 #include "stp/tactics/active/GetBall.h"
 #include "stp/tactics/active/GetBehindBallInDirection.h"
 #include "stp/tactics/active/InstantKick.h"
+#include "stp/tactics/active/DriveWithBall.h"
+#include "gui/Out.h"
 
 namespace rtt::ai::stp::role {
 
 KeeperPasser::KeeperPasser(std::string name) : Role(std::move(name)) {
-    robotTactics = collections::state_machine<Tactic, Status, StpInfo>{tactic::GetBehindBallInDirection(), tactic::GetBall(), tactic::InstantKick(), tactic::KeeperBlockBall()};
+    robotTactics = collections::state_machine<Tactic, Status, StpInfo>{tactic::GetBehindBallInDirection(), tactic::GetBall(), tactic::DriveWithBall(), tactic::InstantKick(), tactic::KeeperBlockBall()};
+}
+
+Status KeeperPasser::update(StpInfo const& info) noexcept {
+    StpInfo skillStpInfo = info;
+    if (info.getRobot() && info.getField() && info.getBall()) {
+        auto ourDefenseArea = FieldComputations::getDefenseArea(*info.getField(), true, -0.4, 0.0);
+        // Stop Formation tactic when ball is moving, start blocking, getting the ball and pass (normal keeper behavior)
+        if (robotTactics.current_num() == 2 && ourDefenseArea.contains(info.getBall().value()->position)) {
+            forceNextTactic();
+        }
+        else if (robotTactics.current_num() == 2) {
+            auto intersections = ourDefenseArea.intersections(LineSegment(info.getField().value().leftGoalArea.leftLine().center(), info.getRobot().value()->getPos()));
+            std::array<rtt::Vector2, 4> intersectionsTotal = {ourDefenseArea[0], ourDefenseArea[1], ourDefenseArea[2], ourDefenseArea[3]};
+                rtt::ai::gui::Out::draw(
+                {
+                    .label = "Penalty op2",
+                    .color = proto::Drawing::BLACK,
+                    .method = proto::Drawing::LINES_CONNECTED,
+                    .category = proto::Drawing::MARGINS,
+                    .size = 12,
+                    .thickness = 4,
+                },
+                intersectionsTotal);
+
+
+            if (intersections.size() == 2) {
+                Vector2 intersection_one = intersections[0];
+                std::array<rtt::Vector2, 2> intersectionOne = {intersections[0], intersections[-1]};
+                rtt::ai::gui::Out::draw(
+                {
+                    .label = "Penalty op",
+                    .color = proto::Drawing::BLACK,
+                    .method = proto::Drawing::CROSSES,
+                    .category = proto::Drawing::MARGINS,
+                    .size = 12,
+                    .thickness = 4,
+                },
+                intersectionOne);
+                skillStpInfo.setPositionToMoveTo(intersection_one);
+            }
+        }
+    }
+
+    return Role::update(skillStpInfo);
 }
 }  // namespace rtt::ai::stp::role

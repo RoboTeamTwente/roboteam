1482cf4403f3810b6c16df2dc56e4bd9c8256109
Luuk <l.winters@roboteamtwente.nl>
2024-07-01 10:42:04 +0200

    Let keeper drive towards the goal with the ball if near the edge of the defense area, to prevent kicking against a robot

--- roboteam_ai/src/stp/evaluations/global/BallInOurDefenseAreaAndStillGlobalEvaluation.cpp
+++ roboteam_ai/src/stp/evaluations/global/BallInOurDefenseAreaAndStillGlobalEvaluation.cpp
@@ -7,8 +7,7 @@ namespace rtt::ai::stp::evaluation {
 uint8_t BallInOurDefenseAreaAndStillGlobalEvaluation::metricCheck(const world::World* world, const Field* field) const noexcept {
     double ballRadius = constants::BALL_RADIUS / 2;
     auto ourDefenseArea = FieldComputations::getDefenseArea(*field, true, ballRadius, 0);
-    return (ourDefenseArea.contains(world->getWorld()->getBall()->get()->position) &&
-            ourDefenseArea.contains(world->getWorld()->getBall()->get()->expectedEndPosition))
+    return (ourDefenseArea.contains(world->getWorld()->getBall()->get()->position) && ourDefenseArea.contains(world->getWorld()->getBall()->get()->expectedEndPosition))
                ? constants::FUZZY_TRUE
                : constants::FUZZY_FALSE;
 }
